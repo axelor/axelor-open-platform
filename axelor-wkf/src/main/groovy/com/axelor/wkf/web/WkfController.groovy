fefeb78717bca8a7039d4ca019107ee67c289c9f
pbe-axelor <p.belloy@axelor.com>
2012-12-19 14:10:36 +0100

    Add link to ORYX.

--- axelor-wkf/src/main/groovy/com/axelor/wkf/web/WkfController.groovy
+++ axelor-wkf/src/main/groovy/com/axelor/wkf/web/WkfController.groovy
@@ -2,7 +2,7 @@ package com.axelor.wkf.web
 
 import com.axelor.rpc.ActionRequest
 import com.axelor.rpc.ActionResponse
-import com.axelor.wkf.WkfRunner;
+import com.axelor.wkf.WkfRunner
 import com.axelor.wkf.db.Instance
 import com.axelor.wkf.db.Workflow
 
@@ -24,7 +24,7 @@ class WkfController {
 			domain: "self.workflow.id = ${context.id}"
 		]
 		
-   }
+	}
 
 	/**
 	 * Open all instances of the wkf.
@@ -38,6 +38,25 @@ class WkfController {
 		
 		WkfRunner.runClass(Class.forName(context.metaModel.fullName));
 		
-   }
+	}
+	
+	/**
+	 * Open ORYX editor
+	 * 
+	 * @param request
+	 * @param response
+	 */
+	def showEditor(ActionRequest request, ActionResponse response) {
+	   
+		def context = request.context
+		
+		if (context?.id){
+			response.view = [title: "Oryx : ${context.name}", resource: "http://188.165.204.152:8080/oryx/editor;bpmn2.0#/model/${context.id}", viewType: "html"]
+		}
+		else {
+			response.view = [title: "Oryx", resource: "http://188.165.204.152:8080/oryx/editor;bpmn2.0#", viewType: "html"]
+		}
+	
+	}
 	
 }
