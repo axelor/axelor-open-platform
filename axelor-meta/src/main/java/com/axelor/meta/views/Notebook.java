4fe767ff50724da4cbdd9d4eb46efaac35225f33
pbe-axelor <p.belloy@axelor.com>
2012-12-26 20:37:00 +0100

    Support for `height` attribute to Notebook.

--- axelor-meta/src/main/java/com/axelor/meta/views/Notebook.java
+++ axelor-meta/src/main/java/com/axelor/meta/views/Notebook.java
@@ -2,6 +2,7 @@ package com.axelor.meta.views;
 
 import java.util.List;
 
+import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlElements;
 import javax.xml.bind.annotation.XmlType;
@@ -12,11 +13,22 @@ import com.fasterxml.jackson.annotation.JsonTypeName;
 @JsonTypeName("notebook")
 public class Notebook extends AbstractContainer {
 
+	@XmlAttribute
+	private String height;
+	
 	@XmlElements({
 		@XmlElement(name = "page", type = Page.class, required = true)
 	})
 	private List<Page> pages;
 	
+	public String getHeight() {
+		return height;
+	}
+	
+	public void setHeight(String height) {
+		this.height = height;
+	}
+
 	public List<Page> getPages() {
 		return pages;
 	}
