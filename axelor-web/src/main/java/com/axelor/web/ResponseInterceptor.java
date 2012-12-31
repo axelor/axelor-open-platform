241d43c228829c607396aedaca802f21c7fda7c5
pbe-axelor <p.belloy@axelor.com>
2012-12-31 12:15:21 +0100

    Improved ResponseInterceptor.

--- axelor-web/src/main/java/com/axelor/web/ResponseInterceptor.java
+++ axelor-web/src/main/java/com/axelor/web/ResponseInterceptor.java
@@ -5,6 +5,8 @@ import javax.persistence.EntityTransaction;
 
 import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.axelor.db.JPA;
 import com.axelor.rpc.Response;
@@ -12,27 +14,57 @@ import com.google.inject.persist.Transactional;
 
 public class ResponseInterceptor implements MethodInterceptor {
 	
+	private final Logger log = LoggerFactory.getLogger(ResponseInterceptor.class);
+	
 	private final ThreadLocal<Boolean> txnStarted = new ThreadLocal<Boolean>();
 	
 	@Override
 	public Object invoke(final MethodInvocation invocation) throws Throwable {
 		
-		final Transactional transactional = invocation.getMethod().getAnnotation(Transactional.class);
+		Response response = null;
+		Transactional transactional = invocation.getMethod().getAnnotation(Transactional.class);
 		if (transactional != null || txnStarted.get() == Boolean.TRUE) {
-			return invocation.proceed();
+			log.debug("Transactional: {}", invocation.getMethod());
+			try {
+				response = (Response) invocation.proceed();
+				log.debug("Success");
+			} catch (Exception e) {
+				response = new Response();
+				response.setException(e);
+				log.error("Error: {}", e, e);
+				if (JPA.em().getTransaction().isActive()) {
+					JPA.em().getTransaction().rollback();
+				}
+			}
+		}
+
+		if (response != null) {
+			return response;
 		}
 		
 		final EntityManager em = JPA.em();
 		final EntityTransaction txn = em.getTransaction();
 		
 		if (txn.isActive()) {
-			return invocation.proceed();
+			log.debug("In transaction: {}", invocation.getMethod());
+			try {
+				response = (Response) invocation.proceed();
+				log.debug("Success");
+			} catch (Exception e) {
+				response = new Response();
+				response.setException(e);
+				log.error("Error: {}", e, e);
+				if (JPA.em().getTransaction().isActive()) {
+					JPA.em().getTransaction().rollback();
+				}
+			}
 		}
 		
+		log.debug("Transaction begin: {}", invocation.getMethod());
+
 		txn.begin();
 		txnStarted.set(true);
-		
-		Response response;
+
 		try {
 			response = (Response) invocation.proceed();
 			if (txn.isActive() && !txn.getRollbackOnly()) {
@@ -41,10 +73,12 @@ public class ResponseInterceptor implements MethodInterceptor {
 		} catch (Throwable e) {
 			response = new Response();
 			response.setException(e);
+			log.error("Error: {}", e, e);
 		} finally {
 			if (txn.isActive()) {
 				txn.rollback();
 			}
+			log.debug("Transaction complete: {}", invocation.getMethod());
 			txnStarted.remove();
 		}
 		return response;
