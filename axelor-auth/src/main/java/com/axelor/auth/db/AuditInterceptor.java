f570dfbbf3135b48c9a2967ac4adbbbef93c48b7
pbe-axelor <p.belloy@axelor.com>
2013-01-17 00:44:31 +0100

    Support for auditable model & interceptor.

---
+++ axelor-auth/src/main/java/com/axelor/auth/db/AuditInterceptor.java
@@ -0,0 +1,61 @@
+package com.axelor.auth.db;
+
+import java.io.Serializable;
+
+import org.hibernate.EmptyInterceptor;
+import org.hibernate.Transaction;
+import org.hibernate.type.Type;
+import org.joda.time.LocalDateTime;
+
+import com.axelor.auth.AuthUtils;
+
+@SuppressWarnings("serial")
+public class AuditInterceptor extends EmptyInterceptor {
+	
+	private ThreadLocal<User> currentUser = new ThreadLocal<User>();
+	
+	@Override
+	public void afterTransactionBegin(Transaction tx) {
+		currentUser.set(AuthUtils.getUser());
+	}
+
+	@Override
+	public void afterTransactionCompletion(Transaction tx) {
+		currentUser.remove();
+	}
+
+	@Override
+	public boolean onFlushDirty(Object entity, Serializable id,
+			Object[] currentState, Object[] previousState,
+			String[] propertyNames, Type[] types) {
+		if (!(entity instanceof AuditableModel)) {
+			return false;
+		}
+		for (int i = 0; i < propertyNames.length; i++) {
+			if ("updatedOn".equals(propertyNames[i])) {
+				currentState[i] = new LocalDateTime();
+			}
+			if ("updatedBy".equals(propertyNames[i])) {
+				currentState[i] = currentUser.get();
+			}
+		}
+		return true;
+	}
+	
+	@Override
+	public boolean onSave(Object entity, Serializable id, Object[] state,
+			String[] propertyNames, Type[] types) {
+		if (!(entity instanceof AuditableModel)) {
+			return false;
+		}
+		for (int i = 0; i < propertyNames.length; i++) {
+			if ("createdOn".equals(propertyNames[i])) {
+				state[i] = new LocalDateTime();
+			}
+			if ("createdBy".equals(propertyNames[i])) {
+				state[i] = currentUser.get();
+			}
+		}
+		return true;
+	}
+}
---
+++ axelor-auth/src/main/java/com/axelor/auth/db/AuditableModel.java
@@ -0,0 +1,78 @@
+package com.axelor.auth.db;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.CascadeType;
+import javax.persistence.FetchType;
+import javax.persistence.ManyToOne;
+import javax.persistence.MappedSuperclass;
+
+import org.hibernate.annotations.Type;
+import org.joda.time.LocalDateTime;
+
+import com.axelor.db.Model;
+import com.axelor.db.Widget;
+
+/**
+ * The base abstract class with update logging feature.
+ * 
+ * The model instance logs the creation date, last modified date,
+ * the authorized user who created the record and the user who
+ * updated the record last time.
+ * 
+ */
+@MappedSuperclass
+public abstract class AuditableModel extends Model {
+
+	@Widget(readonly = true)
+	@Type(type = "org.joda.time.contrib.hibernate.PersistentLocalDateTime")
+	private LocalDateTime createdOn;
+
+	@Widget(readonly = true)
+	@Type(type = "org.joda.time.contrib.hibernate.PersistentLocalDateTime")
+	private LocalDateTime updatedOn;
+
+	@Widget(readonly = true)
+	@ManyToOne(fetch = FetchType.LAZY, cascade = { CascadeType.PERSIST, CascadeType.MERGE })
+	private User createdBy;
+
+	@Widget(readonly = true)
+	@ManyToOne(fetch = FetchType.LAZY, cascade = { CascadeType.PERSIST, CascadeType.MERGE })
+	private User updatedBy;
+	
+	public LocalDateTime getCreatedOn() {
+		return createdOn;
+	}
+
+	@Access(AccessType.FIELD)
+	private void setCreatedOn(LocalDateTime createdOn) {
+		this.createdOn = createdOn;
+	}
+
+	public LocalDateTime getUpdatedOn() {
+		return updatedOn;
+	}
+
+	@Access(AccessType.FIELD)
+	private void setUpdatedOn(LocalDateTime updatedOn) {
+		this.updatedOn = updatedOn;
+	}
+
+	public User getCreatedBy() {
+		return createdBy;
+	}
+
+	@Access(AccessType.FIELD)
+	private void setCreatedBy(User createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	public User getUpdatedBy() {
+		return updatedBy;
+	}
+
+	@Access(AccessType.FIELD)
+	private void setUpdatedBy(User updatedBy) {
+		this.updatedBy = updatedBy;
+	}
+}
