c7d6f4587a6c273c3524cc994702c4e248fe9bf1
Pierre Belloy <p.belloy@axelor.com>
2013-01-20 09:38:09 +0100

    Fixed cyclic dependency issue.

--- axelor-tools/src/test/java/com/axelor/tool/TestModule.java
+++ axelor-tools/src/test/java/com/axelor/tool/TestModule.java
@@ -1,12 +1,11 @@
 package com.axelor.tool;
 
-import com.axelor.db.JpaModule;
 import com.google.inject.AbstractModule;
 
 public class TestModule extends AbstractModule {
 
 	@Override
 	protected void configure() {
-		install(new JpaModule("testUnit", false, true));
+
 	}
 }
