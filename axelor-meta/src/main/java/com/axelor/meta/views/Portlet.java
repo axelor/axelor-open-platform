d9c2a1ba36e7defadb78a41fe7bb0dff44b52d83
Pierre Belloy <p.belloy@axelor.com>
2013-01-21 23:39:13 +0100

    Re-defined portal view.

--- axelor-meta/src/main/java/com/axelor/meta/views/Portlet.java
+++ axelor-meta/src/main/java/com/axelor/meta/views/Portlet.java
@@ -1,55 +1,22 @@
 package com.axelor.meta.views;
 
-import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlType;
 
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
-import com.fasterxml.jackson.annotation.JsonTypeInfo.Id;
+import com.fasterxml.jackson.annotation.JsonTypeName;
 
 @XmlType
-@JsonTypeInfo(use = Id.NONE)
+@JsonTypeName("portlet")
 public class Portlet extends AbstractContainer {
 
-	private String resource;
-
-	private String domain;
+	@XmlAttribute
+	private String action;
 	
-	@XmlElement(name = "view-name")
-	private String viewName;
-	
-	@XmlElement(name = "view-type")
-	private String viewType;
-
-	public String getResource() {
-		return resource;
+	public String getAction() {
+		return action;
 	}
 	
-	public void setResource(String resource) {
-		this.resource = resource;
+	public void setAction(String action) {
+		this.action = action;
 	}
-	
-	public String getDomain() {
-		return domain;
-	}
-
-	public void setDomain(String domain) {
-		this.domain = domain;
-	}
-	
-	public String getViewName() {
-		return viewName;
-	}
-	
-	public void setViewName(String viewName) {
-		this.viewName = viewName;
-	}
-	
-	public String getViewType() {
-		return viewType;
-	}
-	
-	public void setViewType(String viewType) {
-		this.viewType = viewType;
-	}
-
 }
