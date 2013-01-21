9a5748a204ac14d98b7c7e91cc3db7fb5658769d
pbe-axelor <p.belloy@axelor.com>
2013-01-21 23:39:13 +0100

    Re-defined portal view.

--- axelor-meta/src/main/java/com/axelor/meta/views/Portal.java
+++ axelor-meta/src/main/java/com/axelor/meta/views/Portal.java
@@ -2,6 +2,7 @@ package com.axelor.meta.views;
 
 import java.util.List;
 
+import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlElements;
 import javax.xml.bind.annotation.XmlType;
@@ -11,11 +12,22 @@ import com.fasterxml.jackson.annotation.JsonTypeName;
 @XmlType
 @JsonTypeName("portal")
 public class Portal extends AbstractView {
+	
+	@XmlAttribute
+	private Integer cols;
 
 	@XmlElements({
 		@XmlElement(name = "portlet", type = Portlet.class)
 	})
 	private List<Portlet> items;
+
+	public Integer getCols() {
+		return cols;
+	}
+	
+	public void setCols(Integer cols) {
+		this.cols = cols;
+	}
 	
 	public List<Portlet> getItems() {
 		return items;
