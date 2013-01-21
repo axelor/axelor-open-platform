fae5120b68fc8bc191c12f7117fc36a3100e6a0d
pbe-axelor <p.belloy@axelor.com>
2013-01-21 23:44:52 +0100

    Refactored ViewService.

--- axelor-web/src/main/java/com/axelor/web/service/ViewService.java
+++ axelor-web/src/main/java/com/axelor/web/service/ViewService.java
@@ -6,7 +6,9 @@ import java.util.Map;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
@@ -29,96 +31,85 @@ import com.google.inject.servlet.RequestScoped;
 @RequestScoped
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
-@Path("/meta/{model}")
-public class ViewService extends ResourceService {
+@Path("/meta")
+public class ViewService extends AbstractService {
 
 	@Inject
 	private MetaService service;
-	
-	@RequestScoped
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.APPLICATION_JSON)
-	@Path("/meta/models")
-	public static class ModelService {
-		
-		@GET
-		public Response models() {
-			return Resource.models(new Request());
+
+	private Class<?> findClass(String name) {
+		try {
+			return Class.forName(name);
+		} catch (Exception e) {
 		}
+		return null;
 	}
 	
-	@GET @SuppressWarnings("all")
-	public Response get() {
-		Response r1 = this.fields();
-		Response r2 = this.views();
-		
-		Map<String, Object> data = Maps.newHashMap();
-		data.putAll((Map) r1.getData());
-		data.put("views", r2.getData());
+	@GET
+	@Path("models")
+	public Response models() {
+		return Resource.models(new Request());
+	}
 
-		Response response = new Response();
-		response.setData(data);
+	@GET
+	@Path("fields/{model}")
+	public Response fields(@PathParam("model") String model) {
+
+		final Response response = new Response();
+		final Map<String, Object> meta = Maps.newHashMap();
+		final Class<?> modelClass = findClass(model);
+		final List<Object> fields = Lists.newArrayList();
+
+		for (Property p : Mapper.of(modelClass).getProperties()) {
+			fields.add(p.toMap());
+		}
+
+		meta.put("model", model);
+		meta.put("fields", fields);
+
+		response.setData(meta);
+		response.setStatus(Response.STATUS_SUCCESS);
+
 		return response;
 	}
 	
 	@GET
-	@Path("fields")
-	public Response fields() {
-		return getResource().fields();
-	}
-	
-	@GET
-	@Path("views")
-	public Response views() {
-		MultivaluedMap<String, String> params = getUriInfo().getQueryParameters(true);
-		Map<String, String> views = Maps.newHashMap();
-		for (String mode : params.keySet())
+	@Path("views/{model}")
+	public Response views(@PathParam("model") String model) {
+		final MultivaluedMap<String, String> params = getUriInfo().getQueryParameters(true);
+		final Map<String, String> views = Maps.newHashMap();
+		for (String mode : params.keySet()) {
 			views.put(mode, params.getFirst(mode));
-		return service.findViews(entityClass(), views);
+		}
+		return service.findViews(findClass(model), views);
 	}
 	
 	@GET
 	@Path("view")
-	public Response getView(@QueryParam("type") String type, @QueryParam("name") String name) {
-		Map<String, String> view = Maps.newHashMap();
-		view.put(type, name);
-		return service.findViews(entityClass(), view);
-	}
-	
-	private Property findField(String name) {
-		Mapper mapper = Mapper.of(entityClass());
-		Iterator<String> iter = Splitter.on(".").split(name).iterator();
-		Property p = mapper.getProperty(iter.next());
-		while(iter.hasNext()) {
-			mapper = Mapper.of(p.getTarget());
-			p = mapper.getProperty(iter.next());
-		}
-		return p;
-	}
-	
-	private List<Object> findSelection(Property property) {
-		if (property.getSelection() == null)
-			return null;
-		List<MetaSelect> items = MetaSelect.all().filter("self.key = ?", property.getSelection()).fetch();
-		List<Object> all = Lists.newArrayList();
-		for(MetaSelect ms : items) {
-			all.add(ImmutableMap.of("value", ms.getValue(), "title", ms.getTitle()));
-		}
-		return all;
+	public Response view(
+			@QueryParam("model") String model,
+			@QueryParam("name") String name,
+			@QueryParam("type") String type) {
+		return service.findView(model, name, type);
 	}
 
-	@GET
-	@Path("view_fields")
-	@SuppressWarnings("all")
-	public Response getViewFields(@QueryParam("items") String items) {
-		Response response = new Response();
-		List<Object> fields = Lists.newArrayList();
-		Iterable<String> names = Splitter.on(",").trimResults().split(items);
+	@POST
+	@Path("view/fields")
+	public Response viewFields(Request request) {
 		
+		final String model = request.getModel();
+		final List<String> names = request.getFields();
+
+		final Class<?> modelClass = findClass(model);
+		final Mapper mapper = Mapper.of(modelClass);
+
+		final Response response = new Response();
+		final List<Object> fields = Lists.newArrayList();
+
 		for(String name : names) {
-			Property p = findField(name);
+			Property p = findField(mapper, name);
 			if (p != null) {
-				Map map = (Map) p.toMap();
+				Map<String, Object> map = p.toMap();
 				map.put("name", name);
 				if (p.getSelection() != null && !"".equals(p.getSelection().trim())) {
 					map.put("selection", findSelection(p));
@@ -126,8 +117,31 @@ public class ViewService extends ResourceService {
 				fields.add(map);
 			}
 		}
-
 		response.setData(fields);
+		
 		return response;
+	}
+	
+	private Property findField(final Mapper mapper, String name) {
+		final Iterator<String> iter = Splitter.on(".").split(name).iterator();
+		Mapper current = mapper;
+		Property property = current.getProperty(iter.next());
+		while(iter.hasNext()) {
+			current = Mapper.of(property.getTarget());
+			property = current.getProperty(iter.next());
+		}
+		return property;
+	}
+
+	private List<Object> findSelection(Property property) {
+		if (property.getSelection() == null) {
+			return null;
+		}
+		final List<MetaSelect> items = MetaSelect.all().filter("self.key = ?", property.getSelection()).fetch();
+		final List<Object> all = Lists.newArrayList();
+		for(MetaSelect ms : items) {
+			all.add(ImmutableMap.of("value", ms.getValue(), "title", ms.getTitle()));
+		}
+		return all;
 	}
 }
