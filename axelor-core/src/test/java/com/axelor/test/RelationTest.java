24b85b844994e15b90e4d972fc7a319374af9921
pbe-axelor <p.belloy@axelor.com>
2013-02-07 18:12:01 +0100

    Added some more tests.

--- axelor-core/src/test/java/com/axelor/test/RelationTest.java
+++ axelor-core/src/test/java/com/axelor/test/RelationTest.java
@@ -2,6 +2,8 @@ package com.axelor.test;
 
 import java.math.BigDecimal;
 
+import javax.persistence.PersistenceException;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -67,10 +69,65 @@ public class RelationTest {
 		
 		Assert.assertSame(line, move.getMoveLines().get(0));
 	}
+	
+	@Transactional
+	protected void testRemoveCollection() {
+		Move move = Move.all().fetchOne();
+		
+		// the moveLines is exclusive o2m field, so clear would delete all
+		// the move lines but one of move line is referenced outside so the
+		// entity manager will throws an exception
+		move.getMoveLines().clear();
+	}
+	
+	@Transactional
+	protected void testRemoveCollectionImproper() {
+		Move move = Move.all().fetchOne();
+		Invoice invoice = Invoice.all().fetchOne();
+		
+		// before trying to clear moveLines, we remove invoice itself that
+		// refers one of the move line but the invoice again is referenced
+		// in a move
+		invoice.remove();
+		
+		// invoice is removed but still referenced in move so on transaction
+		//completion entity manager will throw an exception
+		move.getMoveLines().clear();
+	}
 
+	@Transactional
+	protected void testRemoveCollectionProper() {
+		Move move = Move.all().fetchOne();
+		Invoice invoice = Invoice.all().fetchOne();
+
+		// clear the reference
+		invoice.setRejectMoveLine(null);
+		
+		// then clear the collection
+		move.getMoveLines().clear();
+		
+		// or
+		
+		// invoice.remove();
+		// move.setInvoice(null);
+		// move.getMoveLines().clear();
+	}
+	
 	@Test
 	public void test() {
 		testErrors();
 		testRelations();
+		
+		try {
+			testRemoveCollection();
+			Assert.fail();
+		} catch (PersistenceException e) {}
+		
+		try {
+			testRemoveCollectionImproper();
+			Assert.fail();
+		} catch (PersistenceException e) {}
+		
+		testRemoveCollectionProper();
 	}
 }
