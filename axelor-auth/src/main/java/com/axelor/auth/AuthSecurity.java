b554e344be07d8db1db5d5be35a2a4a22664ebce
pbe-axelor <p.belloy@axelor.com>
2013-03-03 14:00:12 +0100

    Improved security management
    - dotted fields on __user__
    - restricted access check

--- axelor-auth/src/main/java/com/axelor/auth/AuthSecurity.java
+++ axelor-auth/src/main/java/com/axelor/auth/AuthSecurity.java
@@ -1,5 +1,8 @@
 package com.axelor.auth;
 
+import groovy.lang.Binding;
+import groovy.lang.GroovyShell;
+
 import java.util.List;
 
 import javax.inject.Provider;
@@ -18,11 +21,14 @@ import com.axelor.rpc.filter.Filter;
 import com.axelor.rpc.filter.JPQLFilter;
 import com.google.common.base.Objects;
 import com.google.common.base.Strings;
+import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Lists;
 
 @Singleton
 class AuthSecurity implements JpaSecurity, Provider<JpaSecurity> {
-	
+
+	private static final String MESSAGE = "You are not authorized to access this resource.";
+
 	private User getUser() {
 		User user = AuthUtils.getUser();
 		if (user == null || "admin".equals(user.getCode())
@@ -34,37 +40,47 @@ class AuthSecurity implements JpaSecurity, Provider<JpaSecurity> {
 		return user;
 	}
 	
+	private Object eval(Object bean, String prefix, String expr) {
+		if (bean == null) {
+			return null;
+		}
+		GroovyShell shell = new GroovyShell(new Binding(ImmutableMap.of(prefix, bean)));
+		return shell.evaluate(expr);
+	}
+
 	@Override
 	public Filter getFilter(String action, Class<? extends Model> model, Object... ids) {
 
-		User user = getUser();
+		final User user = getUser();
 		if (user == null) {
 			return null;
 		}
 
-		Mapper mapper = Mapper.of(Permission.class);
-		List<Filter> filters = Lists.newArrayList();
+		final Mapper mapper = Mapper.of(Permission.class);
+		final List<Filter> filters = Lists.newArrayList();
 
-		LocalDate date = new LocalDate();
-		LocalDateTime dateTime = new LocalDateTime();
+		final LocalDate date = new LocalDate();
+		final LocalDateTime dateTime = new LocalDateTime();
 
-		for(Permission permission : user.getGroup().getPermissions()) {
-			if (Strings.isNullOrEmpty(permission.getCondition()) ||
-				!Objects.equal(model.getName(), permission.getObjectName())) {
+		for(final Permission permission : user.getGroup().getPermissions()) {
+			if (!Objects.equal(model.getName(), permission.getObjectName())
+					|| Strings.isNullOrEmpty(permission.getCondition())) {
 				continue;
 			}
-			Boolean permitted = (Boolean) mapper.get(permission, action);
+			final Boolean permitted = (Boolean) mapper.get(permission, action);
 			if (permitted != Boolean.TRUE) {
 				continue;
 			}
 
-			String params = permission.getParams();
-			List<Object> args = Lists.newArrayList();
-			
+			final String params = permission.getParams();
+			final List<Object> args = Lists.newArrayList();
+
 			for(String param : (params == null ? "" : params).split(",")) {
 				param = param.trim();
 				if ("__user__".equals(param)) {
 					args.add(user);
+				} else if (param.startsWith("__user__.")) {
+					args.add(this.eval(user, "__user__", param));
 				} else if ("__date__".equals(param)) {
 					args.add(date);
 				} else if ("__time__".equals(param)) {
@@ -88,23 +104,28 @@ class AuthSecurity implements JpaSecurity, Provider<JpaSecurity> {
 
 	@Override
 	public void check(String action, Class<? extends Model> model) {
-		User user = getUser();
+		final User user = getUser();
 		if (user == null) {
 			return;
 		}
-		
-		Mapper mapper = Mapper.of(Permission.class);
-		
+
+		final Mapper mapper = Mapper.of(Permission.class);
+		boolean checkRestricted = true;
+
 		for(Permission permission : user.getGroup().getPermissions()) {
-			if (!Strings.isNullOrEmpty(permission.getCondition()) ||
-				!Objects.equal(model.getName(), permission.getObjectName())) {
+			if (!Objects.equal(model.getName(), permission.getObjectName())
+					|| !Strings.isNullOrEmpty(permission.getCondition())) {
 				continue;
 			}
 			Boolean permitted = (Boolean) mapper.get(permission, action);
 			if (permitted == null || permitted == Boolean.FALSE) {
-				throw new UnauthorizedException(
-						"You are not authorized to access this resource");
+				throw new UnauthorizedException(MESSAGE);
 			}
+			checkRestricted = false;
+		}
+
+		if (checkRestricted && user.getGroup().getRestricted() == Boolean.TRUE) {
+			throw new UnauthorizedException(MESSAGE);
 		}
 	}
 
@@ -124,7 +145,7 @@ class AuthSecurity implements JpaSecurity, Provider<JpaSecurity> {
 		
 		Model found = filter.build(model).fetchOne();
 		if (found == null || !Objects.equal(found.getId(), id)) {
-			throw new UnauthorizedException("You are not authorized to access this resource.");
+			throw new UnauthorizedException(MESSAGE);
 		}
 	}
 
