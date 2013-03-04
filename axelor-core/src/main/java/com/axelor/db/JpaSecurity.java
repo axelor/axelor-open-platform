3628a6d396aa8d446aa714ad7af1c944809e6cb8
Pierre Belloy <p.belloy@axelor.com>
2013-03-04 22:42:12 +0100

    Refactored security implementation.

--- axelor-core/src/main/java/com/axelor/db/JpaSecurity.java
+++ axelor-core/src/main/java/com/axelor/db/JpaSecurity.java
@@ -4,11 +4,34 @@ import com.axelor.rpc.filter.Filter;
 
 public interface JpaSecurity {
 	
-	Filter getFilter(String action, Class<? extends Model> model, Object... ids);
-	
-	void check(String action, Class<? extends Model> model);
+	static enum AccessType {
+		
+		READ	("You are not authorized to read this resource."),
+		WRITE	("You are not authorized to update this resource."),
+		CREATE	("You are not authorized to create this resource."),
+		REMOVE	("You are not authrorized to remove this resource.");
 
-	void check(String action, Class<? extends Model> model, Long id);
+		private String message;
+		private AccessType(String message) {
+			this.message = message;
+		}
+
+		@Override
+		public String toString() {
+			return this.message;
+		}
+	}
+
+	public static final AccessType CAN_READ = AccessType.READ;
+	public static final AccessType CAN_WRITE = AccessType.WRITE;
+	public static final AccessType CAN_CREATE = AccessType.CREATE;
+	public static final AccessType CAN_REMOVE = AccessType.REMOVE;
+
+	Filter getFilter(AccessType type, Class<? extends Model> model, Object... ids);
+
+	void check(AccessType type, Class<? extends Model> model);
+
+	void check(AccessType type, Class<? extends Model> model, Long id);
 	
-	void check(String action, Model entity);
+	void check(AccessType type, Model entity);
 }
