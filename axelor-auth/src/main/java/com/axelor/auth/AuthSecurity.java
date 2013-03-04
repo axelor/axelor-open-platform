057c0b69e3266df89a83b11949f7a2418f9dab24
pbe-axelor <p.belloy@axelor.com>
2013-03-04 22:48:09 +0100

    Refactored security implementation.
    - single instance for table & row level rules
    - added validation to check unique rule per group

--- axelor-auth/src/main/java/com/axelor/auth/AuthSecurity.java
+++ axelor-auth/src/main/java/com/axelor/auth/AuthSecurity.java
@@ -9,25 +9,59 @@ import javax.inject.Provider;
 import javax.inject.Singleton;
 
 import org.apache.shiro.authz.UnauthorizedException;
-import org.joda.time.LocalDate;
-import org.joda.time.LocalDateTime;
 
 import com.axelor.auth.db.Permission;
 import com.axelor.auth.db.User;
 import com.axelor.db.JpaSecurity;
 import com.axelor.db.Model;
-import com.axelor.db.mapper.Mapper;
 import com.axelor.rpc.filter.Filter;
 import com.axelor.rpc.filter.JPQLFilter;
 import com.google.common.base.Objects;
-import com.google.common.base.Strings;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Lists;
 
 @Singleton
 class AuthSecurity implements JpaSecurity, Provider<JpaSecurity> {
 
-	private static final String MESSAGE = "You are not authorized to access this resource.";
+	private static class Condition {
+		
+		private Filter filter;
+
+		public Condition(User user, String condition, String params) {
+			
+			final List<Object> args = Lists.newArrayList();
+			
+			for(String param : (params == null ? "" : params).split(",")) {
+				param = param.trim();
+				if ("__user__".equals(param)) {
+					args.add(user);
+				} else if (param.startsWith("__user__.")) {
+					args.add(this.eval(user, "__user__", param));
+				} else {
+					args.add(param);
+				}
+			}
+
+			this.filter = new JPQLFilter(condition, args.toArray());
+		}
+
+		private Object eval(Object bean, String prefix, String expr) {
+			if (bean == null) {
+				return null;
+			}
+			GroovyShell shell = new GroovyShell(new Binding(ImmutableMap.of(prefix, bean)));
+			return shell.evaluate(expr);
+		}
+		
+		public Filter getFilter() {
+			return filter;
+		}
+
+		@Override
+		public String toString() {
+			return filter.getQuery();
+		}
+	}
 
 	private User getUser() {
 		User user = AuthUtils.getUser();
@@ -40,58 +74,71 @@ class AuthSecurity implements JpaSecurity, Provider<JpaSecurity> {
 		return user;
 	}
 	
-	private Object eval(Object bean, String prefix, String expr) {
-		if (bean == null) {
+	private Condition getCondition(User user, Permission permission, AccessType accessType) {
+		final String condition;
+		final String params;
+		switch (accessType) {
+		case READ:
+			condition = permission.getReadCondition();
+			params = permission.getReadParams();
+			break;
+		case WRITE:
+			condition = permission.getWriteCondition();
+			params = permission.getWriteParams();
+			break;
+		case CREATE:
+			condition = permission.getCreateCondition();
+			params = permission.getCreateParams();
+			break;
+		case REMOVE:
+			condition = permission.getRemoveCondition();
+			params = permission.getRemoveParams();
+			break;
+		default:
 			return null;
 		}
-		GroovyShell shell = new GroovyShell(new Binding(ImmutableMap.of(prefix, bean)));
-		return shell.evaluate(expr);
+
+		if (condition == null || "".equals(condition.trim())) {
+			return null;
+		}
+		return new Condition(user, condition, params);
+	}
+
+	private boolean hasAccess(Permission permission, AccessType accessType) {
+		switch(accessType) {
+		case READ:
+			return permission.getCanRead() == Boolean.TRUE;
+		case WRITE:
+			return permission.getCanWrite() == Boolean.TRUE;
+		case CREATE:
+			return permission.getCanCreate() == Boolean.TRUE;
+		case REMOVE:
+			return permission.getCanRemove() == Boolean.TRUE;
+		default:
+			return false;
+		}
 	}
 
 	@Override
-	public Filter getFilter(String action, Class<? extends Model> model, Object... ids) {
+	public Filter getFilter(AccessType type, Class<? extends Model> model, Object... ids) {
 
 		final User user = getUser();
 		if (user == null) {
 			return null;
 		}
 
-		final Mapper mapper = Mapper.of(Permission.class);
 		final List<Filter> filters = Lists.newArrayList();
 
-		final LocalDate date = new LocalDate();
-		final LocalDateTime dateTime = new LocalDateTime();
-
 		for(final Permission permission : user.getGroup().getPermissions()) {
-			if (!Objects.equal(model.getName(), permission.getObjectName())
-					|| Strings.isNullOrEmpty(permission.getCondition())) {
+			if (!Objects.equal(model.getName(), permission.getObject())) {
 				continue;
 			}
-			final Boolean permitted = (Boolean) mapper.get(permission, action);
-			if (permitted != Boolean.TRUE) {
-				continue;
+			Condition condition = this.getCondition(user, permission, type);
+			if (condition != null) {
+				filters.add(condition.getFilter());
 			}
-
-			final String params = permission.getParams();
-			final List<Object> args = Lists.newArrayList();
-
-			for(String param : (params == null ? "" : params).split(",")) {
-				param = param.trim();
-				if ("__user__".equals(param)) {
-					args.add(user);
-				} else if (param.startsWith("__user__.")) {
-					args.add(this.eval(user, "__user__", param));
-				} else if ("__date__".equals(param)) {
-					args.add(date);
-				} else if ("__time__".equals(param)) {
-					args.add(dateTime);
-				} else {
-					args.add(param);
-				}
-			}
-			filters.add(new JPQLFilter(permission.getCondition(), args.toArray()));
 		}
-		
+
 		if (filters.isEmpty()) {
 			return null;
 		}
@@ -103,59 +150,57 @@ class AuthSecurity implements JpaSecurity, Provider<JpaSecurity> {
 	}
 
 	@Override
-	public void check(String action, Class<? extends Model> model) {
+	public void check(AccessType type, Class<? extends Model> model) {
 		final User user = getUser();
 		if (user == null) {
 			return;
 		}
 
-		final Mapper mapper = Mapper.of(Permission.class);
 		boolean checkRestricted = true;
-
+		
 		for(Permission permission : user.getGroup().getPermissions()) {
-			if (!Objects.equal(model.getName(), permission.getObjectName())
-					|| !Strings.isNullOrEmpty(permission.getCondition())) {
+			if (!Objects.equal(model.getName(), permission.getObject())) {
 				continue;
 			}
-			Boolean permitted = (Boolean) mapper.get(permission, action);
-			if (permitted == null || permitted == Boolean.FALSE) {
-				throw new UnauthorizedException(MESSAGE);
+			boolean permitted = hasAccess(permission, type);
+			if (!permitted) {
+				throw new UnauthorizedException(type.toString());
 			}
 			checkRestricted = false;
 		}
 
 		if (checkRestricted && user.getGroup().getRestricted() == Boolean.TRUE) {
-			throw new UnauthorizedException(MESSAGE);
+			throw new UnauthorizedException(type.toString());
 		}
 	}
 
 	@Override
-	public void check(String action, Class<? extends Model> model, Long id) {
+	public void check(AccessType type, Class<? extends Model> model, Long id) {
 		User user = getUser();
 		if (user == null) {
 			return;
 		}
-		
-		check(action, model);
 
-		Filter filter = this.getFilter(action, model, id);
+		check(type, model);
+
+		Filter filter = this.getFilter(type, model, id);
 		if (filter == null) {
 			return;
 		}
-		
+
 		Model found = filter.build(model).fetchOne();
 		if (found == null || !Objects.equal(found.getId(), id)) {
-			throw new UnauthorizedException(MESSAGE);
+			throw new UnauthorizedException(type.toString());
 		}
 	}
 
 	@Override
-	public void check(String action, Model entity) {
+	public void check(AccessType type, Model entity) {
 		User user = getUser();
 		if (user == null || entity == null) {
 			return;
 		}
-		check(action, entity.getClass(), entity.getId());
+		check(type, entity.getClass(), entity.getId());
 	}
 
 	@Override
