cfc54cbd199eab0d97f085d6aa953bc7f304777f
Pierre Belloy <p.belloy@axelor.com>
2013-03-05 10:58:45 +0100

    Update tree.

--- axelor-wkf/src/main/groovy/com/axelor/wkf/web/WkfController.groovy
+++ axelor-wkf/src/main/groovy/com/axelor/wkf/web/WkfController.groovy
@@ -1,12 +1,18 @@
 package com.axelor.wkf.web
 
+import javax.inject.Inject
+
 import com.axelor.rpc.ActionRequest
 import com.axelor.rpc.ActionResponse
-import com.axelor.wkf.WkfRunner
+import com.axelor.wkf.WkfSettings;
 import com.axelor.wkf.db.Instance
 import com.axelor.wkf.db.Workflow
+import com.axelor.wkf.workflow.WorkFlowEngine
 
 class WkfController {
+	
+	@Inject
+	WorkFlowEngine workFlowEngine
 
 	/**
 	 * Open all instances of the wkf.
@@ -36,7 +42,7 @@ class WkfController {
 	   
 		def context = request.context as Workflow
 		
-		WkfRunner.runClass(Class.forName(context.metaModel.fullName));
+		workFlowEngine.run(Class.forName(context.metaModel.fullName));
 		
 	}
 	
@@ -51,10 +57,10 @@ class WkfController {
 		def context = request.context
 				
 		if (context?.id){
-			response.view = [title: "Oryx : ${context.name}", resource: "http://188.165.204.152:8080/oryx/editor;bpmn2.0#/model/${context.id}", viewType: "html"]
+			response.view = [title: "Oryx : ${context.name}", resource: "${WkfSettings.get().get('wkf.oryx.url', '')}/model/${context.id}", viewType: "html"]
 		}
 		else {
-			response.view = [title: "Oryx", resource: "http://188.165.204.152:8080/oryx/editor;bpmn2.0#", viewType: "html"]
+			response.view = [title: "Oryx", resource: "${WkfSettings.get().get('wkf.oryx.url', '')}", viewType: "html"]
 		}
 	
 	}
