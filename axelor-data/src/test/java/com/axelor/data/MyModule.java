385a70cf23b3f8e5b77054e7a757b8d43ec5f85a
pbe-axelor <p.belloy@axelor.com>
2013-03-19 08:36:29 +0100

    Fixed unit tests.

--- axelor-data/src/test/java/com/axelor/data/MyModule.java
+++ axelor-data/src/test/java/com/axelor/data/MyModule.java
@@ -1,5 +1,7 @@
 package com.axelor.data;
 
+import net.sf.ehcache.CacheManager;
+
 import com.axelor.db.JpaModule;
 import com.google.inject.AbstractModule;
 
@@ -7,6 +9,12 @@ public class MyModule extends AbstractModule {
 	
 	@Override
 	protected void configure() {
+		
+		// shutdown the cache manager if running
+		if (CacheManager.ALL_CACHE_MANAGERS.size() > 0) {
+			CacheManager.getInstance().shutdown();
+		}
+
 		install(new JpaModule("testUnit", true, true));
 		configureImport();
 	}
