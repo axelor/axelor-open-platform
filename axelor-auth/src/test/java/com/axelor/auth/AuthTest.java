413515dea4e28b791c545a815fb9b88174a7d9e3
pbe-axelor <p.belloy@axelor.com>
2013-03-20 13:48:28 +0100

    Added unit tests.

--- axelor-auth/src/test/java/com/axelor/auth/AuthTest.java
+++ axelor-auth/src/test/java/com/axelor/auth/AuthTest.java
@@ -1,17 +1,67 @@
 package com.axelor.auth;
 
+import junit.framework.Assert;
+
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import com.axelor.auth.db.User;
+import com.axelor.db.JPA;
 import com.axelor.test.GuiceModules;
 import com.axelor.test.GuiceRunner;
+import com.google.inject.persist.Transactional;
 
 @RunWith(GuiceRunner.class)
 @GuiceModules({ TestModule.class })
 public class AuthTest {
+	
+	@Before
+	@Transactional
+	public void setUp() {
+		if (User.all().filter("self.code = ?", "demo").count() > 0) {
+			return;
+		}
+		
+		User user = new User();
+		user.setCode("demo");
+		user.setName("Demo");
+		user.setPassword("demo");
+		
+		user.save();
+		
+		Subject subject = AuthUtils.getSubject();
+		UsernamePasswordToken token = new UsernamePasswordToken("demo", "demo");
 
+		subject.login(token);
+	}
+	
 	@Test
-	public void test() {
+	@Transactional
+	public void test () {
+
+		User user = User.all().filter("self.code = ?", "demo").fetchOne();
+		
+		Assert.assertNotNull(user);
+		
+		User current = AuthUtils.getUser();
+		
+		Assert.assertTrue(JPA.em().contains(current));
+
+		JPA.clear();
+
+		Assert.assertFalse(JPA.em().contains(current));
+
+		User user2 = new User();
+		user2.setCode("demo2");
+		user2.setName("Demo2");
+		user2.setPassword("demo2");
+		
+		user2.save();
+		
+		Assert.assertNotNull(user2.getCreatedBy());
 		
 	}
 }
--- axelor-auth/src/test/java/com/axelor/auth/TestModule.java
+++ axelor-auth/src/test/java/com/axelor/auth/TestModule.java
@@ -1,5 +1,7 @@
 package com.axelor.auth;
 
+import java.util.Properties;
+
 import com.axelor.db.JpaModule;
 import com.google.inject.AbstractModule;
 
@@ -7,6 +9,11 @@ public class TestModule extends AbstractModule {
 
 	@Override
 	protected void configure() {
-		install(new JpaModule("testUnit"));
+
+		Properties properties = new Properties();
+		properties.put("hibernate.ejb.interceptor", "com.axelor.auth.db.AuditInterceptor");
+		
+		install(new AuthModule.Simple());
+		install(new JpaModule("testUnit").properties(properties));
 	}
 }
