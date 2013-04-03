069420e2fb62ba50619dfc401b534721f6cb4daf
pbe-axelor <p.belloy@axelor.com>
2013-04-03 12:57:45 +0100

    Fixed groups validation.

--- axelor-auth/src/main/java/com/axelor/auth/web/AuthValidator.java
+++ axelor-auth/src/main/java/com/axelor/auth/web/AuthValidator.java
@@ -1,9 +1,13 @@
 package com.axelor.auth.web;
 
+import java.math.BigInteger;
 import java.util.Set;
+
+import javax.persistence.Query;
 
 import com.axelor.auth.db.Group;
 import com.axelor.auth.db.Permission;
+import com.axelor.db.JPA;
 import com.axelor.rpc.ActionRequest;
 import com.axelor.rpc.ActionResponse;
 import com.google.common.base.Joiner;
@@ -21,20 +25,37 @@ public class AuthValidator {
 		Set<Permission> all = Sets.newHashSet();
 		Set<String> unique = Sets.newHashSet();
 		Set<String> duplicates = Sets.newHashSet();
+		Set<String> notValid = Sets.newHashSet();
 		
 		for(Permission permission : group.getPermissions()) {
 			String object = permission.getObject();
 			if (unique.contains(object)) {
 				duplicates.add(object);
+				continue;
 			} else {
-				all.add(permission);
 				unique.add(object);
 			}
+			
+			Query query = JPA.em().createNativeQuery("SELECT count(*) from meta_meta_model where CONCAT(package_name, '.', name) = ?1");
+			query.setParameter(1, object);
+			
+			BigInteger requestResult = (BigInteger) query.getSingleResult();
+			if(requestResult.equals(BigInteger.ZERO)){
+				notValid.add(object);
+				continue;
+			}
+			
+			all.add(permission);
 		}
 		
 		if (duplicates.size() > 0) {
-			response.setFlash("Only one permission per object is required.<br><br>"+
-							  "Duplicates found:<br>" + Joiner.on("<br>").join(duplicates));
+			response.setFlash(JPA.translate("Only one permission per object is required.")+"<br><br>"+
+					JPA.translate("Duplicates found:<br>") + Joiner.on("<br>").join(duplicates));
+			response.setValues(ImmutableMap.of("permissions", all));
+		}
+		else if(notValid.size() >0){
+			response.setFlash(JPA.translate("Not a valid object.")+"<br><br>"+
+					JPA.translate("Object found:")+"<br>" + Joiner.on("<br>").join(notValid));
 			response.setValues(ImmutableMap.of("permissions", all));
 		}
 	}
