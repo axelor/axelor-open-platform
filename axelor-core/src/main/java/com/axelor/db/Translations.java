072fdd5fa32cba69b4b6a5b8acc52a60cdcd9513
Pierre Belloy <p.belloy@axelor.com>
2013-04-05 09:08:38 +0200

    Added dummy implementation of Translations interface.

--- axelor-core/src/main/java/com/axelor/db/Translations.java
+++ axelor-core/src/main/java/com/axelor/db/Translations.java
@@ -1,11 +1,38 @@
 package com.axelor.db;
 
+import javax.inject.Provider;
+
+import com.google.inject.ImplementedBy;
+
+@ImplementedBy(Translations.Dummy.class)
 public interface Translations {
-	
+
+	static class Dummy implements Translations, Provider<Translations> {
+
+		@Override
+		public Translations get() {
+			return new Dummy();
+		}
+
+		@Override
+		public String get(String key) {
+			return key;
+		}
+		
+		@Override
+		public String get(String key, String defaultValue) {
+			return key;
+		}
+
+		@Override
+		public String get(String key, String domain, String defaultValue) {
+			return key;
+		}
+	}
+
 	String get(String key);
 	
 	String get(String key, String defaultValue);
 	
 	String get(String key, String domain, String defaultValue);
-
 }
