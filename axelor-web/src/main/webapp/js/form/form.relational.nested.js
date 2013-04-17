3246facae3947fc71a5f3e2e80870fa8e29a83bb
pbe-axelor <p.belloy@axelor.com>
2013-04-17 10:41:53 +0100

    Split form.relational.js into several files.
    Conflicts:
    	src/main/webapp/js/form/form.relational.js

---
+++ axelor-web/src/main/webapp/js/form/form.relational.nested.js
@@ -0,0 +1,235 @@
+(function() {
+
+var ui = angular.module("axelor.ui");
+
+var NestedForm = {
+	scope: true,
+	controller: [ '$scope', '$element', function($scope, $element) {
+		
+		FormViewCtrl.call(this, $scope, $element);
+		
+		$scope.onShow = function(viewPromise) {
+			
+		};
+		
+		$scope.registerNested($scope);
+		$scope.show();
+	}],
+	link: function(scope, element, attrs, ctrl) {
+
+	},
+	template: '<div ui-view-form x-handler="this"></div>'
+};
+
+ui.EmbeddedEditorCtrl = EmbeddedEditorCtrl;
+ui.EmbeddedEditorCtrl.$inject = ['$scope', '$element', 'DataSource', 'ViewService'];
+
+function EmbeddedEditorCtrl($scope, $element, DataSource, ViewService) {
+	
+	var params = angular.copy($scope._viewParams);
+	
+	params.views = _.compact([params.summaryView]);
+	$scope._viewParams = params;
+
+	ViewCtrl($scope, DataSource, ViewService);
+	FormViewCtrl.call(this, $scope, $element);
+
+	$scope.onShow = function() {
+		
+	};
+	
+	var originalEdit = $scope.edit;
+	
+	function doEdit(record) {
+		if (record && record.id > 0 && !record.$fetched) {
+			$scope.doRead(record.id).success(function(record){
+				originalEdit(record);
+			});
+		} else {
+			originalEdit(record);
+		}
+	};
+	
+	function doClose() {
+		if ($scope.isDetailView) {
+			$scope.edit($scope.getSelectedRecord());
+			return;
+		}
+		$element.hide();
+		$element.data('$rel').show();
+	};
+	
+	$scope.edit = function(record) {
+		doEdit(record);
+	};
+
+	$scope.onClose = function() {
+		doClose();
+	};
+	
+	$scope.onOK = function() {
+		if (!$scope.isValid()) {
+			return;
+		}
+		var record = $scope.record;
+		if (record) record.$fetched = true;
+		$scope.select(record);
+		setTimeout(doClose);
+	};
+	
+	$scope.onClear = function() {
+		$scope.record = {};
+	};
+	
+	$scope.$on('grid:changed', function(event) {
+		var record = $scope.getSelectedRecord();
+		if ($scope.isDetailView) {
+			$scope.edit(record);
+		}
+	});
+	
+	$scope.show();
+}
+
+var EmbeddedEditor = {
+	restrict: 'EA',
+	css: 'nested-editor',
+	scope: true,
+	controller: EmbeddedEditorCtrl,
+	template:
+		'<fieldset class="form-item-group bordered-box">'+
+			'<div ui-view-form x-handler="this"></div>'+
+			'<div class="btn-toolbar pull-right">'+
+				'<button type="button" class="btn btn-danger" ng-click="onClose()">Cancel</button> '+
+				'<button type="button" class="btn btn-primary" ng-click="onOK()">OK</button>'+
+			'</div>'+
+		'</fieldset>'
+};
+
+ui.NestedEditorCtrl = NestedEditorCtrl;
+ui.NestedEditorCtrl.$inject = ['$scope', '$element', 'DataSource', 'ViewService'];
+
+function NestedEditorCtrl($scope, $element, DataSource, ViewService) {
+	
+	var params = $scope._viewParams;
+	
+	params.views = _.compact([params.summaryView]);
+	$scope._viewParams = params;
+
+	ui.ManyToOneCtrl.call(this, $scope, $element, DataSource, ViewService);
+	
+	$scope.nested = null;
+	$scope.registerNested = function(scope) {
+		$scope.nested = scope;
+	};
+}
+
+var NestedEditor = {
+	restrict: 'EA',
+	css: 'nested-editor',
+	require: '?ngModel',
+	scope: true,
+	controller: NestedEditorCtrl,
+	link: function(scope, element, attrs, model) {
+		
+		var configured = false,
+			updateFlag = true;
+		
+		function setValidity(nested, valid) {
+			setTimeout(function(){
+				model.$setValidity('valid', nested.isValid());
+				if (scope.setValidity) {
+					scope.setValidity('valid', nested.isValid());
+				}
+				scope.$apply();
+			});
+		}
+		
+		function configure(nested) {
+			
+			//FIX: select on M2O doesn't apply to nested editor
+			scope.$watch(attrs.ngModel + '.id', function(){
+				setTimeout(function(){
+					nested.$apply();
+				});
+			});
+
+			nested.$watch('form.$valid', function(valid){
+				setValidity(nested, valid);
+			});
+			nested.$watch('record', function(rec, old){
+				if (updateFlag && rec != old) {
+					if (_.isEmpty(rec)) {
+						rec = null;
+					} else {
+						rec.$dirty = true;
+					}
+					if (rec) {
+						model.$setViewValue(rec);
+					}
+				}
+				updateFlag = true;
+				setValidity(nested, nested.isValid());
+			}, true);
+		}
+		
+		scope.ngModel = model;
+		
+		scope.onClear = function() {
+			model.$setViewValue(null);
+			model.$render();
+		};
+
+		scope.onClose = function() {
+			element.hide();
+		};
+
+		attrs.$observe('title', function(title){
+			scope.title = title;
+		});
+		
+		model.$render = function() {
+			var nested = scope.nested,
+				promise = nested._viewPromise,
+				value = model.$viewValue;
+
+			if (nested == null)
+				return;
+			
+			if (!configured) {
+				configured = true;
+				promise.then(function(){
+					configure(nested);
+				});
+			}
+			if (value == null || !value.id || value.$dirty) {
+				return nested.edit(value);
+			}
+			
+			promise.then(function(){
+				nested.doRead(value.id).success(function(record){
+					updateFlag = false;
+					nested.edit(record);
+				});
+			});
+		};
+	},
+	template:
+	'<fieldset class="form-item-group bordered-box">'+
+		'<legend>'+
+			'<span ng-bind-html-unsafe="title"></span> '+
+			'<span class="legend-toolbar">'+
+				'<a href="" tabindex="-1" ng-click="onClear()" title="{{\'Clear\' | t}}"><i class="icon-ban-circle"></i></a> '+
+				'<a href="" tabindex="-1" ng-click="onSelect()" title="{{\'Select\' | t}}"><i class="icon-search"></i></a> '+
+				'<a href="" tabindex="-1" ng-click="onClose()" title="{{\'Close\' | t}}"><i class="icon-remove-sign"></i></a>'+
+			'</span>'+
+		'</legend>'+
+		'<div ui-nested-form></div>'+
+	'</fieldset>'
+};
+
+ui.formDirective('uiNestedEditor', NestedEditor);
+ui.formDirective('uiEmbeddedEditor', EmbeddedEditor);
+ui.formDirective('uiNestedForm', NestedForm);
+	
+}).call(this);
---
+++ axelor-web/src/main/webapp/js/form/form.relational.single.js
@@ -0,0 +1,292 @@
+(function(){
+
+var ui = angular.module("axelor.ui");
+
+ui.ManyToOneCtrl = ManyToOneCtrl;
+ui.ManyToOneCtrl.$inject = ['$scope', '$element', 'DataSource', 'ViewService'];
+
+function ManyToOneCtrl($scope, $element, DataSource, ViewService) {
+
+	ui.RefFieldCtrl.call(this, $scope, $element, DataSource, ViewService);
+
+	var ds = $scope._dataSource,
+		field = $scope.getViewDef($element),
+		nameField = field.targetName || field.nameField || 'id';
+
+	$scope.createNestedEditor = function() {
+		
+		var embedded = $('<div ui-nested-editor></div>')
+			.attr('ng-model', $element.attr('ng-model'))
+			.attr('name', $element.attr('name'))
+			.attr('x-title', $element.attr('x-title'))
+			.attr('x-path', $element.attr('x-path'));
+
+		embedded = ViewService.compile(embedded)($scope);
+		embedded.hide();
+		
+		var colspan = $element.parents("form.dynamic-form:first").attr('x-cols') || 4,
+			cell = $('<td class="form-item"></td>').attr('colspan', colspan).append(embedded),
+			row = $('<tr></tr>').append(cell);
+		
+		row.insertAfter($element.parents("tr:first"));
+		
+		return embedded;
+	};
+	
+	$scope.select = function(value) {
+		
+		if (_.isArray(value)) {
+			value = _.first(value);
+		}
+		
+		var record = value;
+
+		// fetch '.' names
+		var path = $element.attr('x-path');
+		var relatives = $element.parents().find('[x-field][x-path^="'+path+'."]').map(
+				function(){
+					return $(this).attr('x-path').replace(path+'.','');
+				}).get();
+		
+		relatives = _.unique(relatives);
+		if (relatives.length > 0 && value && value.id) {
+			return ds.read(value.id, {
+				fields: relatives
+			}).success(function(rec){
+				var record = { 'id' : value.id };
+				record[nameField] = rec[nameField];
+				_.each(relatives, function(name) {
+					var prefix = name.split('.')[0];
+					record[prefix] = rec[prefix];
+				});
+				$scope.setValue(record, true);
+			});
+		}
+		// end fetch '.' names
+
+		if (value && value.id) {
+			record = { 'id' : value.id };
+			record[nameField] = value[nameField];
+			if (nameField && _.isUndefined(value[nameField])) {
+				return ds.details(value.id).success(function(rec){
+					$scope.setValue(rec, true);
+				});
+			}
+		}
+		
+		$scope.setValue(record, true);
+	};
+	
+	$scope.onEdit = function() {
+		var record = $scope.getValue();
+		$scope.showEditor(record);
+	};
+	
+	$scope.onSummary = function() {
+		var record = $scope.getValue();
+		if (record && record.id) {
+			return ds.read(record.id).success(function(record){
+				$scope.showNestedEditor(record);
+			});
+		}
+		$scope.showNestedEditor(record);
+	};
+}
+
+var ManyToOneItem = {
+	css	: 'many2one-item',
+	require: '?ngModel',
+	scope: true,
+	controller: ManyToOneCtrl,
+	link: function(scope, element, attrs, model) {
+		
+		scope.ngModel = model;
+		
+		var field = scope.getViewDef(element),
+			input = element.children('input:first'),
+			nameField = field.targetName || field.nameField || 'id';
+		
+		scope.formPath = scope.formPath ? scope.formPath + "." + field.name : field.name;
+		
+		input.keydown(function(e){
+			var handled = false;
+			if (e.keyCode == 113) { // F2
+				if (e.shiftKey) {
+					scope.onNew();
+				} else {
+					scope.onEdit();
+				}
+				handled = true;
+			}
+			if (e.keyCode == 114) { // F3
+				scope.onSelect();
+				handled = true;
+			}
+			if (e.keyCode == 46) { // DELETE
+				scope.select(null);
+				handled = true;
+			}
+			if (!handled) {
+				return;
+			}
+			setTimeout(function(){
+				scope.$apply();
+			});
+			e.preventDefault();
+			e.stopPropagation();
+			return false;
+		});
+		
+		model.$render = function() {
+			var value = model.$viewValue;
+			if (value) {
+				value = value[nameField];
+			}
+			input.val(value);
+		};
+
+		var embedded = null;
+		var readonly = false;
+		if (field.widget == 'NestedEditor') {
+			setTimeout(function(){
+				embedded = scope.showNestedEditor();
+				if (readonly) {
+					scope.setReadonly(embedded, readonly);
+				}
+			});
+		}
+
+		attrs.$observe('readonly', function(value) {
+			readonly = value;
+			if (embedded) {
+				scope.setReadonly(embedded, readonly);
+			}
+		});
+		
+		scope.setValidity = function(key, value) {
+			model.$setValidity(key, value);
+		};
+		
+		function search(request, response) {
+			var fields = field.targetSearch || [],
+				filter = {}, ds = scope._dataSource;
+
+			fields = ["id", nameField].concat(fields);
+			fields = _.chain(fields).compact().unique().value();
+
+			_.each(fields, function(name){
+				if (name !== "id" && request.term) {
+					filter[name] = request.term;
+				}
+			});
+			
+			var domain = scope._domain,
+				context = scope._context;
+
+			if (domain && scope.getContext) {
+				context = _.extend({}, context, scope.getContext());
+			}
+
+			var params = {
+				filter: filter,
+				fields: fields,
+				archived: true,
+				limit: 10
+			};
+			
+			if (domain) {
+				params.domain = domain;
+				params.context = context;
+			}
+
+			ds.search(params).success(function(records, page){
+				if(_.isEmpty(records)){
+		            onSelectFired = false;
+			    }
+				response(records);
+			});
+		}
+		
+		var onSelectFired = false;
+		input.autocomplete({
+			source: function(request, response) {
+				var onSelect = element.data('$onSelect');
+				if (onSelect && !onSelectFired) {
+					onSelect.handle().then(function(){
+						search(request, response);
+					});
+					onSelectFired = true;
+				}
+				else search(request, response);
+			},
+			close: function(event, ui) {
+				onSelectFired = false;
+			},
+			focus: function(event, ui) {
+				return false;
+			},
+			select: function(event, ui) {
+				scope.select(ui.item);
+				onSelectFired = false;
+				return false;
+			}
+		}).data("autocomplete")._renderItem = function( ul, item ) {
+			var label = item[nameField] || item.name || item.code || item.id;
+			return $("<li><a>" + label  + "</a></li>")
+				.data("item.autocomplete", item)
+				.appendTo(ul);
+		};
+		
+		var canSelect = _.isUndefined(field.canSelect) ? true : field.canSelect;
+		setTimeout(function(){
+			if (!canSelect) scope.setHidden(element);
+		});
+		
+		if ((scope._viewParams || {}).summaryView) {
+			element.removeClass('picker-icons-3').addClass('picker-icons-4');
+		}
+
+		scope.isDisabled = function() {
+			return scope.isReadonly(element);
+		};
+	},
+	template:
+	'<div class="picker-input picker-icons-3">'+
+		'<input type="text" autocomplete="off">'+
+		'<span class="picker-icons">'+
+			'<i class="icon-eye-open" ng-click="onSummary()" ng-show="hasPermission(\'read\') && _viewParams.summaryView"></i>'+
+			'<i class="icon-pencil" ng-click="onEdit()" ng-show="hasPermission(\'read\')" title="{{\'Edit\' | t}}"></i>'+
+			'<i class="icon-plus" ng-click="onNew()" ng-show="hasPermission(\'write\') && !isDisabled()" title="{{\'New\' | t}}"></i>'+
+			'<i class="icon-search" ng-click="onSelect()" ng-show="hasPermission(\'read\') && !isDisabled()" title="{{\'Select\' | t}}"></i>'+
+		'</span>'+
+   '</div>'
+};
+
+var SuggestBox = _.extend({}, ManyToOneItem, {
+	_linkOrig : ManyToOneItem.link,
+	link: function(scope, element, attrs, model) {
+		this._linkOrig(scope, element, attrs, model);
+		var input = element.children(':input:first');
+		input.autocomplete("option" , {
+			minLength: 0
+		});
+		scope.showSelection = function() {
+			if (scope.isReadonly(element)) {
+				return;
+			}
+			input.autocomplete("search" , '');
+		};
+	},
+	template:
+	'<span class="picker-input">'+
+		'<input type="text" autocomplete="off">'+
+		'<span class="picker-icons">'+
+			'<i class="icon-caret-down" ng-click="showSelection()"></i>'+
+		'</span>'+
+   '</span>'
+});
+
+ui.formDirective('uiManyToOne', ManyToOneItem);
+ui.formDirective('uiSuggestBox', SuggestBox);
+
+}).call(this);
