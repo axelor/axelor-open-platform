fdac42917c21c2367151957752d9e098120e27d0
pbe-axelor <p.belloy@axelor.com>
2013-05-08 13:28:42 +0100

    Intrgrated recent SlickGrid fixes.

--- axelor-web/src/main/webapp/lib/slickgrid/slick.grid.js
+++ axelor-web/src/main/webapp/lib/slickgrid/slick.grid.js
@@ -975,8 +975,8 @@ if (typeof Slick === "undefined") {
         unregisterPlugin(plugins[i]);
       }
 
-      if (options.enableColumnReorder && $headers.sortable) {
-        $headers.sortable("destroy");
+      if (options.enableColumnReorder) {
+          $headers.filter(":ui-sortable").sortable("destroy");
       }
 
       unbindAncestorScrollEvents();
@@ -1332,7 +1332,7 @@ if (typeof Slick === "undefined") {
       if (value == null) {
         return "";
       } else {
-        return value.toString().replace(/&/g,"&amp;").replace(/</g,"&lt;").replace(/>/g,"&gt;");
+        return (value + "").replace(/&/g,"&amp;").replace(/</g,"&lt;").replace(/>/g,"&gt;");
       }
     }
 
