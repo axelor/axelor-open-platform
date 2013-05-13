f427145dabccce8a5b23b1b16aad4e7323bc20b3
pbe-axelor <p.belloy@axelor.com>
2013-05-13 23:12:12 +0100

    Re-implemented javascript minimize script.

--- axelor-web/src/main/webapp/minimize.sh
+++ axelor-web/src/main/webapp/minimize.sh
@@ -1,54 +1,15 @@
 #!/bin/bash
+UGLIFY=$(which uglifyjs)
+OUTPUT="js/application-all.js"
 
-NODE=$(which node)
-OUTPUT="js/application-all.min.js"
-
-if [ ! -f "$NODE" ] ; then
+if [ ! -f "$UGLIFY" ] ; then
 	echo "Install NodeJS + UglifyJS to combine all javascript resources."
 	exit 0
 fi
 
-[ -z "$1" ] || OUTPUT=$1
+files=$(cat js/application.js | grep -P "\s*(\'|\")(.*?.js)('|\")\s*(,)?" | sed s/"'\|\""/''/g | sed s/",$"/' '/g)
 
-mkdir -p `dirname $OUTPUT`
+echo "Compressing all javascript sources..."
+$UGLIFY $files -o $OUTPUT $@
+echo "Generated '$OUTPUT'"
 
-cat << EOF | $NODE
-
-var fs = require('fs')
-
-var jsp = require("uglify-js").parser;
-var pro = require("uglify-js").uglify;
-
-// uglify non-lib code
-function uglify(source) {
-	var ast = jsp.parse(source);
-	ast = pro.ast_mangle(ast);
-	ast = pro.ast_squeeze(ast);
-	return pro.gen_code(ast);
-}
-
-function minimizeJS() {
-	var code = "";
-	
-	console.log("Combine: js/application.js");
-	fs.readFileSync('js/application.js').toString()
-	.split('\n').forEach(function (line) {
-		var mat = /\s*(\'|\")(.*?.js)('|\")\s*(,)?/.exec(line)
-		if (mat) {
-			var file = mat[2]
-			console.log('Processing:', file);
-			var source = fs.readFileSync(file).toString();
-			source = source.replace("\"use strict;\"", "")
-			if (/([-_.])min\.js/.test(file)) {
-				return code += source + ";\n"
-			}
-			code += uglify(source) + ";\n"
-		}
-	})
-	fs.writeFileSync("$OUTPUT", code)
-	console.log("Created: $OUTPUT");
-}
-
-minimizeJS();
-
-EOF
