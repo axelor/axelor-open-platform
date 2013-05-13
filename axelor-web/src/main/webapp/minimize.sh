94101d57c438f46af3b5fe0cdd2b2b28e6cc22fd
Pierre Belloy <p.belloy@axelor.com>
2013-05-14 00:53:32 +0200

    Fixed javascript minimize issue.

--- axelor-web/src/main/webapp/minimize.sh
+++ axelor-web/src/main/webapp/minimize.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 UGLIFY=$(which uglifyjs)
-OUTPUT="js/application-all.js"
+OUTPUT="js/application-all.min.js"
+
+ARGS=""
+while test "$1" ; do
+	case $1 in
+		-o) OUTPUT=$2 ; shift ;;
+		*) ARGS="$ARGS $1" ;;
+	esac
+	shift
+done
 
 if [ ! -f "$UGLIFY" ] ; then
 	echo "Install NodeJS + UglifyJS to combine all javascript resources."
@@ -9,7 +18,10 @@ fi
 
 files=$(cat js/application.js | grep -P "\s*(\'|\")(.*?.js)('|\")\s*(,)?" | sed s/"'\|\""/''/g | sed s/",$"/' '/g)
 
+OUTDIR=$(dirname $OUTPUT)
+mkdir -p $OUTDIR
+
 echo "Compressing all javascript sources..."
-$UGLIFY $files -o $OUTPUT $@
+$UGLIFY $files -o $OUTPUT $ARGS
 echo "Generated '$OUTPUT'"
 
