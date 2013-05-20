27c74db2a59499fe1b3b70641d953e991bd64d45
pbe-axelor <p.belloy@axelor.com>
2013-05-20 15:19:38 +0200

    Manage Action; Update model; Fix views;

--- axelor-wkf/src/main/java/com/axelor/wkf/workflow/WorkflowService.java
+++ axelor-wkf/src/main/java/com/axelor/wkf/workflow/WorkflowService.java
@@ -9,19 +9,14 @@ import java.util.Set;
 
 import javax.inject.Inject;
 
-import org.hibernate.proxy.HibernateProxy;
 import org.joda.time.DateTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.axelor.auth.AuthUtils;
 import com.axelor.auth.db.User;
-import com.axelor.db.JPA;
 import com.axelor.db.Model;
-import com.axelor.db.mapper.Mapper;
-import com.axelor.meta.service.MetaModelService;
-import com.axelor.rpc.ActionRequest;
-import com.axelor.rpc.ActionResponse;
+import com.axelor.meta.ActionHandler;
 import com.axelor.wkf.IWorkflow;
 import com.axelor.wkf.action.Action;
 import com.axelor.wkf.db.Instance;
@@ -32,17 +27,14 @@ import com.axelor.wkf.db.Transition;
 import com.axelor.wkf.db.WaitingNode;
 import com.axelor.wkf.db.Workflow;
 import com.google.common.base.Preconditions;
-import com.google.common.collect.Maps;
 import com.google.inject.persist.Transactional;
 
 public class WorkflowService implements IWorkflow {
 	
-	protected static final Logger LOG = LoggerFactory.getLogger(WorkflowService.class);
+	protected Logger log = LoggerFactory.getLogger(WorkflowService.class);
 	
 	private Action actionWorkflow;
-	
-	protected ActionRequest actionRequest;
-	protected ActionResponse actionResponse;
+	private ActionHandler actionHandler;
 	
 	protected int maxNodeCounter;
 	protected Map<Node, Integer> nodeCounters;
@@ -74,45 +66,20 @@ public class WorkflowService implements IWorkflow {
 	 * 
 	 * 
 	 */
-	protected WorkflowService init (Workflow wkf, ActionRequest actionRequest) {
+	protected WorkflowService init (Workflow wkf, ActionHandler actionHandler) {
+
+		Preconditions.checkNotNull(wkf);
 		
-		LOG.debug("INIT Wkf engine context ::: {}", actionRequest.getContext());
-		
-		this.actionRequest = actionRequest;
+		log.debug("INIT Wkf engine context ::: {}", actionHandler.getContext());
+
+		this.actionHandler = actionHandler;
 		this.maxNodeCounter = wkf.getMaxNodeCounter();
-		this.instance = getInstance(wkf, (Long)actionRequest.getContext().get("id"));
+		this.instance = getInstance(wkf, (Long)actionHandler.getContext().get("id"));
 		
 		loadWaitingNodes( instance );
 		loadExecutedNodes( instance );
 		
 		return this;
-		
-	}
-	
-	/**
-	 * Init the service from one instance.
-	 * 
-	 * 
-	 */
-	protected WorkflowService init (Workflow wkf, Model model) {
-
-		LOG.debug("INIT Wkf engine bean () ::: {}", model);
-		ActionRequest request = new ActionRequest();
-
-		Map<String, Object> data = Maps.newHashMap();
-		data.put("context", Mapper.toMap( model ));
-		
-		request.setData( data );
-		request.setModel( persistentClassName( model ) );
-		
-		return init(wkf, request);
-		
-	}
-	
-	private String persistentClassName( Model model ){
-		
-		if ( model instanceof HibernateProxy ) { return ((HibernateProxy) model).getHibernateLazyInitializer().getPersistentClass().getName(); }
-		else { return model.getClass().getName(); }
 		
 	}
 
@@ -157,11 +124,10 @@ public class WorkflowService implements IWorkflow {
 	 * @see Model
 	 */
 	@Override
-	public Workflow getWorkflow(Class<?> klass){
+	public Workflow getWorkflow(String name){
 		
 		List<Workflow> workflows = Workflow.all()
-				.filter("self.metaModel = ?1 AND self.active = true", MetaModelService.getMetaModel(klass))
-				.order("self.sequence")
+				.filter("self.name = ?1 AND self.active = true", name)
 				.fetch();
 		
 		for (Workflow workflow : workflows){
@@ -173,91 +139,12 @@ public class WorkflowService implements IWorkflow {
 	}
 
 // RUN WKF
-	
-	/**
-	 * Run workflow from specific class for all records from this class.
-	 * 
-	 * @param klass
-	 * 		Target class.
-	 */
-	@Override
-	public ActionResponse run( Class<Model> klass ){
-		
-		Workflow wkf = getWorkflow( klass );
-		
-		if (wkf != null){
-			
-			LOG.debug("Run workflow {}", wkf.getName());
-			
-			for (Model model : JPA.all(klass).fetch()) { run( wkf, model ); }
-			
-			
-		}
-		else {
-			
-			LOG.debug("No workflow for entity ::: {}", klass);
-			
-		}
-		
-		return this.actionResponse;
-	}
-	
-	/**
-	 * Run workflow from specific class for one specific record.
-	 * 
-	 * @param bean
-	 * @param id
-	 */
-	@Override
-	public ActionResponse run(Model model) {
-		
-		Workflow wkf = getWorkflow( model.getClass() );
-		
-		if (wkf != null){ 
-			LOG.debug("Run workflow {}", wkf.getName());
-			run(wkf, model); 
-		}
-		else { LOG.debug("No workflow for entity ::: {}", model.getClass()); }
-		
-		return this.actionResponse;
-		
-	}
-	
-	/**
-	 * Run workflow from specific class for one specific wkf and one specific record.
-	 * 
-	 * @param wkf
-	 * @param bean
-	 */
-	@Override
-	public ActionResponse run(Workflow wkf, Model model){
-		
-		updateInstance( init( wkf, model ).playNodes( instance.getNodes() ) );
-		
-		return this.actionResponse;
-		
-	}
 
 	@Override
-	public ActionResponse run( ActionRequest actionRequest ) {
+	public Map<String, String> run( String wkf, ActionHandler handler ){
 		
-		Workflow wkf = getWorkflow( actionRequest.getBeanClass() );
-		
-		if (wkf != null){ 
-			LOG.debug("Run workflow {}", wkf.getName());
-			run(wkf, actionRequest);
-		}
-		else { LOG.debug("No workflow for entity ::: {}", actionRequest.getBeanClass()); }
-		
-		return this.actionResponse;
-		
-	}
-
-	@Override
-	public ActionResponse run( Workflow wkf, ActionRequest actionRequest ){
-		
-		updateInstance( init( wkf, actionRequest ).playNodes( instance.getNodes() ) );
-		return this.actionResponse;
+		updateInstance( init( getWorkflow(wkf), handler ).playNodes( instance.getNodes() ) );
+		return actionWorkflow.getData();
 		
 	}
 
@@ -295,7 +182,7 @@ public class WorkflowService implements IWorkflow {
 	 */
 	protected Set<Node> playNode(Node node){
 
-		LOG.debug("Play node {}", node.getName());
+		log.debug("Play node {}", node.getName());
 		
 		testMaxPassedNode(node);
 		
@@ -305,7 +192,7 @@ public class WorkflowService implements IWorkflow {
 			
 			if ( isPlayable(node) ){
 
-				if ( node.getAction() != null ){ actionResponse = actionWorkflow.execute( node.getAction().getName(), actionRequest ); }
+				if ( node.getAction() != null ){ actionWorkflow.execute( node.getAction().getName(), actionHandler ); }
 				nodes.addAll( playTransitions( node.getEndTransitions() ) );
 				counterAdd( instance, node );
 				addExecutedNodes( node );
@@ -314,7 +201,7 @@ public class WorkflowService implements IWorkflow {
 		}
 		else {
 
-			if ( node.getAction() != null ){ actionResponse = actionWorkflow.execute( node.getAction().getName(), actionRequest ); }
+			if ( node.getAction() != null ){ actionWorkflow.execute( node.getAction().getName(), actionHandler ); }
 			nodes.add( node );
 			
 		}
@@ -359,9 +246,7 @@ public class WorkflowService implements IWorkflow {
 	 */
 	protected Set<Node> playTransition(Transition transition) {
 
-		Preconditions.checkArgument( transition.getStartNode().getWorkflow().getMetaModel().equals( transition.getNextNode().getWorkflow().getMetaModel() ) );
-
-		LOG.debug("Play transition {}", transition.getName());
+		log.debug("Play transition {}", transition.getName());
 		
 		Set<Node> nodes = new HashSet<Node>();
 		
@@ -369,9 +254,9 @@ public class WorkflowService implements IWorkflow {
 		
 		if (transition.getCondition() != null){
 
-			actionResponse = actionWorkflow.execute( transition.getCondition().getName(), actionRequest );
+			actionWorkflow.execute( transition.getCondition().getName(), actionHandler );
 			
-			if ( !actionWorkflow.isInError(actionResponse) ){
+			if ( !actionWorkflow.isInError( ) ){
 				
 				addWaitingNodes( transition );
 				nodes.addAll( playNode( transition.getNextNode() ) );
@@ -409,7 +294,7 @@ public class WorkflowService implements IWorkflow {
 
 		nodeCounters.put(node, counter);
 
-		LOG.debug( "compteur {} ::: max {}", counter, this.maxNodeCounter );
+		log.debug( "compteur {} ::: max {}", counter, this.maxNodeCounter );
 		
 		if (counter > this.maxNodeCounter) {
 			throw new Error( String.format( "We passed by the node %s %d time", node.getName(), counter ) );
