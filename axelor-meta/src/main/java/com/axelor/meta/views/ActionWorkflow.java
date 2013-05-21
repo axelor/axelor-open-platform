8ddf2c9a809fdd09a97d96b93b9903a0355d87d8
pbe-axelor <p.belloy@axelor.com>
2013-05-21 21:59:28 +0200

    Update action-workflow.

--- axelor-meta/src/main/java/com/axelor/meta/views/ActionWorkflow.java
+++ axelor-meta/src/main/java/com/axelor/meta/views/ActionWorkflow.java
@@ -1,11 +1,9 @@
 package com.axelor.meta.views;
 
 import java.lang.reflect.Method;
-import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
 
-import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlType;
 
 import com.axelor.meta.ActionHandler;
@@ -16,12 +14,16 @@ public class ActionWorkflow extends Action {
 	
 	public static final String className = "com.axelor.wkf.workflow.WorkflowService",
 			method = "run";
-
-	@XmlElement(name = "workflow")
-	private List<WorkflowItem> workflows;
 	
-	public List<WorkflowItem> getWorkflows() {
-		return workflows;
+	@XmlAttribute
+	private String model;
+	
+	public String getModel() {
+		return model;
+	}
+	
+	public void setModel(String model) {
+		this.model = model;
 	}
 	
 	@SuppressWarnings({ "rawtypes", "unchecked" })
@@ -29,7 +31,6 @@ public class ActionWorkflow extends Action {
 	public Object evaluate(ActionHandler handler) {
 
 		Map<String, String> result = Maps.newHashMap();
-		Iterator<WorkflowItem> iter = workflows.iterator();
 		
 		if (getName() != null) {
 			log.debug("action-workflow: {}", getName());
@@ -40,14 +41,7 @@ public class ActionWorkflow extends Action {
 			Class<?> klass = Class.forName( className );
 			Method m = klass.getMethod( method, String.class, ActionHandler.class );
 			Object obj = handler.getInjector().getInstance(klass);
-			
-			while(iter.hasNext()) {
-				Act act = iter.next();
-				if (act.test(handler)) {
-					String name = act.getName().trim();
-					result.putAll( (Map) m.invoke(obj, name, handler) );
-				}
-			}
+			result.putAll( (Map) m.invoke(obj, model.trim(), handler) );
 			
 		} catch (Exception e) { 
 			log.error( "{}", e);
@@ -60,10 +54,6 @@ public class ActionWorkflow extends Action {
 	@Override
 	public Object wrap(ActionHandler handler) {
 		return evaluate(handler);
-	}
-	
-	public static class WorkflowItem extends Act {
-		
 	}
 
 }
