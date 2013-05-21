13aec94afa8b8977da96095ec4d1776d8f154260
pbe-axelor <p.belloy@axelor.com>
2013-05-21 11:31:15 +0100

    Prevent tabstop on axelor logo.

--- axelor-web/src/main/webapp/js/widget/widget.navtabs.js
+++ axelor-web/src/main/webapp/js/widget/widget.navtabs.js
@@ -10,7 +10,7 @@ angular.module('axelor.ui').directive('navTabs', function() {
 			setTimeout(function() {
 				var wrap = elem.children('.nav-tabs-wrap').css('margin-right', 100);
 				var logo = $('<img src="img/axelor.png" width="100" height="32">');
-				$('<a href="http://axelor.com" target="_blank"></a>').append(logo)
+				$('<a href="http://axelor.com" tabindex="-1" target="_blank"></a>').append(logo)
 					.css("position", "absolute")
 					.css("right", 0).css("top", 0)
 					.css("border-bottom", wrap.find('ul.nav-tabs').css('border-bottom'))
