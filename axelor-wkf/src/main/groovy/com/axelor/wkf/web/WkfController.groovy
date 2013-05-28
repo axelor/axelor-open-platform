b0c5b8c4b84b9fa8246c68c0dc238055dfa5ec6a
pbe-axelor <p.belloy@axelor.com>
2013-05-28 21:04:41 +0200

    Implement signavio interface.

--- axelor-wkf/src/main/groovy/com/axelor/wkf/web/WkfController.groovy
+++ axelor-wkf/src/main/groovy/com/axelor/wkf/web/WkfController.groovy
@@ -1,30 +1,33 @@
 package com.axelor.wkf.web
 
+import groovy.util.logging.Slf4j;
+
 import javax.inject.Inject
 
+import com.axelor.auth.AuthUtils
 import com.axelor.rpc.ActionRequest
 import com.axelor.rpc.ActionResponse
 import com.axelor.wkf.WkfSettings
 import com.axelor.wkf.db.Instance
 import com.axelor.wkf.db.Workflow
-import com.axelor.wkf.workflow.WorkflowImporter;
+import com.axelor.wkf.workflow.WorkflowImporter
 import com.axelor.wkf.workflow.WorkflowService
 
+@Slf4j
 class WkfController {
-	
+
 	@Inject
 	WorkflowService workflowService
-	
+
 	@Inject
 	WorkflowImporter workflowImporter
-	
+
 	def importWorkflow(ActionRequest request, ActionResponse response){
-		
+
 		def context = request.context as Workflow
-		
+
 		workflowImporter.run(context.bpmn)
 		response.reload = true
-		
 	}
 
 	/**
@@ -34,34 +37,33 @@ class WkfController {
 	 * @param response
 	 */
 	def openInstances (ActionRequest request, ActionResponse response) {
-	   
+
 		def context = request.context as Workflow
-				
+
 		response.view = [
 			title: "${context.name}",
 			resource: Instance.class.name,
 			domain: "self.workflow.id = ${context.id}"
 		]
-		
 	}
-	
+
 	/**
-	 * Open ORYX editor
+	 * Open editor
 	 * 
 	 * @param request
 	 * @param response
 	 */
-	def showEditor(ActionRequest request, ActionResponse response) {
-	   
-		def context = request.context
-				
-		if (context?.id){
-			response.view = [title: "Oryx : ${context.name}", resource: "${WkfSettings.get().get('wkf.oryx.url', '')}/model/${context.id}", viewType: "html"]
-		}
-		else {
-			response.view = [title: "Oryx", resource: "${WkfSettings.get().get('wkf.oryx.url', '')}", viewType: "html"]
-		}
-	
+	def openEditor( ActionRequest request, ActionResponse response ) {
+
+		def context = request.context as Workflow
+		def resource = "${WkfSettings.get().get('wkf.editor.url', '')}/p/editor?id=${context.id}&model=${context.metaModel.fullName}&url=${WkfSettings.get().get('application.url', '')}&lang=${WkfSettings.get().get('wkf.editor.lang', '')}&sessionId=${AuthUtils.subject.session.id.toString()}"
+		
+		log.debug("SIGNAVIO URL : ${resource}")
+		
+		response.view = [
+			title : "Editor : ${context.name}",
+			resource : resource,
+			viewType : "html"
+		]
 	}
-	
 }
