b3a901c082bbe98f40bf7947e1bf86c83871f791
pbe-axelor <p.belloy@axelor.com>
2013-06-08 19:43:16 +0100

    Improved ActionResponse
    - added setValue
    - added setSignal
    - made public setAttr
    - docs

--- axelor-core/src/main/java/com/axelor/rpc/ActionResponse.java
+++ axelor-core/src/main/java/com/axelor/rpc/ActionResponse.java
@@ -7,16 +7,19 @@ import java.util.Map;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
 
-import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 
+/**
+ * An implementation of {@link Response} to be used with controllers.
+ * 
+ */
 @XmlType
 @XmlRootElement(name = "response")
 public class ActionResponse extends Response {
 
-	Map<String, Object> _data = Maps.newHashMap();
-	
+	final Map<String, Object> _data = Maps.newHashMap();
+
 	@SuppressWarnings("all")
 	private void set(String name, Object value) {
 		if (getData() == null) {
@@ -26,63 +29,200 @@ public class ActionResponse extends Response {
 		}
 		_data.put(name, value);
 	}
-	
+
+	/**
+	 * Set the <i>reload</i> flag.
+	 * <p>
+	 * The client user <i>reload</i> flag to refresh the view.
+	 * </p>
+	 * 
+	 * @param reload
+	 *            whether to reload client view
+	 */
 	public void setReload(boolean reload) {
 		set("reload", reload);
 	}
-	
+
+	/**
+	 * Set a flash message.
+	 * <p>
+	 * This message will be shown on the client screen.
+	 * </p>
+	 * 
+	 * @param flash
+	 *            the message to show on client
+	 */
 	public void setFlash(String flash) {
 		set("flash", flash);
 	}
 
+	/**
+	 * Set record values.
+	 * <p>
+	 * The client updates current view with these values.
+	 * </p>
+	 * 
+	 * @param context
+	 *            a map or a model instance
+	 * @see #setValue(String, Object)
+	 */
 	public void setValues(Object context) {
 		set("values", context);
 	}
 
+	/**
+	 * Set value for the given field.
+	 * 
+	 * @param fieldName
+	 *            name of the field
+	 * @param value
+	 *            field name
+	 * @see #setValues(Object)
+	 */
+	@SuppressWarnings("all")
+	public void setValue(String fieldName, Object value) {
+		Map<String, Object> values = (Map) _data.get("values");
+		if (values == null) {
+			values = Maps.newHashMap();
+		}
+		values.put(fieldName, value);
+		setValues(values);
+	}
+
+	/**
+	 * Inform the client to open the given view.
+	 * 
+	 */
 	public void setView(Map<String, Object> view) {
 		set("view", view);
 	}
-	
-	public void setView(String title, String entity, String mode, String criteria) {
-		setView(ImmutableMap.<String,Object>of("title", title, "entity", entity, "view", mode, "criteria", criteria));
+
+	/**
+	 * Inform the client to open a view for the given model.
+	 * 
+	 * @param title
+	 *            the view title
+	 * @param model
+	 *            the model name
+	 * @param mode
+	 *            the view mode (e.g. form, grid etc)
+	 * @param domain
+	 *            the filter
+	 */
+	public void setView(String title, String model, String mode, String domain) {
+		Map<String, Object> view = Maps.newHashMap();
+		view.put("title", title);
+		view.put("model", model);
+		view.put("type", mode);
+		view.put("domain", domain);
+		setView(view);
 	}
-	
+
+	/**
+	 * Send an arbitrary signal to the client view with the specified data.
+	 * 
+	 * @param signal
+	 *            signal name
+	 * @param data
+	 *            signal data
+	 */
+	public void setSignal(String signal, Object data) {
+		set("signal", signal);
+		set("signal-data", data);
+	}
+
+	/**
+	 * Set field attributes.
+	 * <p>
+	 * The client view may update the view fields with the given attributes.
+	 * </p>
+	 * 
+	 * @param attrs
+	 *            attribute map for the fields
+	 */
 	public void setAttrs(Map<String, Map<String, Object>> attrs) {
 		set("attrs", attrs);
 	}
-	
+
+	/**
+	 * Set an attribute of a field.
+	 * 
+	 * @param fieldName
+	 *            name of the field
+	 * @param attr
+	 *            attribute name
+	 * @param value
+	 *            attribute value
+	 */
 	@SuppressWarnings("all")
-	private void setAttr(String fieldName, String attr, Object value) {
-		
+	public void setAttr(String fieldName, String attr, Object value) {
+
 		Map<String, Map<String, Object>> attrs = null;
-		
+
 		try {
 			attrs = (Map) ((Map) getItem(0)).get("attrs");
-		}catch(Exception e) {
+		} catch (Exception e) {
 		}
 
 		if (attrs == null) {
-			attrs = new HashMap<String, Map<String,Object>>();
+			attrs = new HashMap<String, Map<String, Object>>();
 		}
-		
+
 		Map<String, Object> my = attrs.get(fieldName);
-		if (my == null)
+		if (my == null) {
 			my = new HashMap<String, Object>();
-		
+		}
+
 		my.put(attr, value);
 		attrs.put(fieldName, my);
-		
+
 		setAttrs(attrs);
 	}
-	
+
+	/**
+	 * Set the <code>required</code> attribute for the given field.
+	 * 
+	 * @param fieldName
+	 *            name of the field
+	 * @param required
+	 *            true or false
+	 */
+	public void setRequired(String fieldName, boolean required) {
+		setAttr(fieldName, "required", required);
+	}
+
+	/**
+	 * Set the <code>readonly</code> attribute for the given field.
+	 * 
+	 * @param fieldName
+	 *            name of the field
+	 * @param readonly
+	 *            true or false
+	 */
 	public void setReadonly(String fieldName, boolean readonly) {
 		setAttr(fieldName, "readonly", readonly);
 	}
-	
+
+	/**
+	 * Set the <code>hidden</code> attribute for the given field.
+	 * 
+	 * @param fieldName
+	 *            name of the field
+	 * @param hidden
+	 *            true or false
+	 */
 	public void setHidden(String fieldName, boolean hidden) {
 		setAttr(fieldName, "hidden", hidden);
 	}
-	
+
+	/**
+	 * Set the <code>color</code> attribute for the given field.
+	 * 
+	 * @param fieldName
+	 *            name of the field
+	 * @param color
+	 *            CSS compatible color value
+	 */
 	public void setColor(String fieldName, String color) {
 		setAttr(fieldName, "color", color);
 	}
