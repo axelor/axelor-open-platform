92d49f37e96c55cb189f6dddb96529e81bcfc950
pbe-axelor <p.belloy@axelor.com>
2013-06-12 09:22:19 +0200

    Added condition setter.

--- axelor-data/src/main/java/com/axelor/data/csv/CSVBinding.java
+++ axelor-data/src/main/java/com/axelor/data/csv/CSVBinding.java
@@ -88,6 +88,10 @@ public class CSVBinding {
 	public String getCondition() {
 		return condition;
 	}
+
+	public void setCondition(String condition) {
+		this.condition = condition;
+	}
 	
 	public List<CSVBinding> getBindings() {
 		return bindings;
