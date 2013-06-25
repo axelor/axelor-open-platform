6a717acd246700b63c2c39d168757b14f6c5036e
Pierre Belloy <p.belloy@axelor.com>
2013-06-25 09:54:54 +0200

    Support for custom filters
    - MetaFilter stores the custom filters
    - added related controller/services

--- axelor-meta/src/main/java/com/axelor/meta/service/MetaUserService.java
+++ axelor-meta/src/main/java/com/axelor/meta/service/MetaUserService.java
@@ -1,8 +1,14 @@
 package com.axelor.meta.service;
 
+import java.util.List;
+
+import org.apache.shiro.authz.AuthorizationException;
+
 import com.axelor.auth.AuthUtils;
 import com.axelor.auth.db.User;
+import com.axelor.meta.db.MetaFilter;
 import com.axelor.meta.db.MetaUser;
+import com.google.common.base.Objects;
 import com.google.inject.persist.Transactional;
 
 public class MetaUserService {
@@ -17,5 +23,50 @@ public class MetaUserService {
 			prefs = prefs.save();
 		}
 		return prefs;
+	}
+	
+	@Transactional
+	public MetaFilter saveFilter(MetaFilter ctx) {
+		User user = AuthUtils.getUser();
+		String query = "self.name = ?1 AND self.filterView = ?2 AND (self.user.code = ?3 OR self.shared = true)";
+		MetaFilter filter = MetaFilter.filter(query, ctx.getName(), ctx.getFilterView(), user.getCode()).fetchOne();
+		
+		if (filter == null) {
+			filter = new MetaFilter();
+			filter.setName(ctx.getName());
+			filter.setUser(user);
+			filter.setFilterView(ctx.getFilterView());
+		}
+
+		filter.setTitle(ctx.getTitle());
+		filter.setFilters(ctx.getFilters());
+		filter.setFilterCustom(ctx.getFilterCustom());
+		
+		if (Objects.equal(filter.getUser(), user)) {
+			filter.setShared(ctx.getShared());
+		}
+		
+		return filter.save();
+	}
+	
+	@Transactional
+	public MetaFilter removeFilter(MetaFilter ctx) {
+		User user = AuthUtils.getUser();
+		String query = "self.name = ?1 AND self.filterView = ?2 AND (self.user.code = ?3 OR self.shared = true)";
+		MetaFilter filter = MetaFilter.filter(query, ctx.getName(), ctx.getFilterView(), user.getCode()).fetchOne();
+		
+		if (!Objects.equal(filter.getUser(), user)) {
+			throw new AuthorizationException("You are not allowed to remove this filter");
+		}
+		
+		filter.remove();
+
+		return ctx;
+	}
+	
+	public List<MetaFilter> getFilters(String filterView) {
+		User user = AuthUtils.getUser();
+		String query = "self.filterView = ?1 AND (self.user.code = ?2 OR self.shared = true)";
+		return MetaFilter.filter(query, filterView, user.getCode()).order("id").fetch();
 	}
 }
