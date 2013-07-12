68c9289cec0a5a314d1d805d70c3fbaafc4d711f
pbe-axelor <p.belloy@axelor.com>
2013-07-12 10:34:55 +0100

    Improved AuthFilter
    - override getLoginUrl()
    - some refactoring

--- axelor-auth/src/main/java/com/axelor/auth/AuthFilter.java
+++ axelor-auth/src/main/java/com/axelor/auth/AuthFilter.java
@@ -2,6 +2,8 @@ package com.axelor.auth;
 
 import java.util.Map;
 
+import javax.inject.Inject;
+import javax.inject.Named;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
@@ -15,32 +17,53 @@ import org.apache.shiro.web.filter.authc.FormAuthenticationFilter;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class AuthFilter extends FormAuthenticationFilter {
-	
+
+	@Inject
+	@Named("app.loginUrl")
+	private String loginUrl;
+
+	@Override
+	public String getLoginUrl() {
+		if (loginUrl != null) {
+			return loginUrl;
+		}
+		return super.getLoginUrl();
+	}
+
 	@Override
 	protected boolean onAccessDenied(ServletRequest request, ServletResponse response) throws Exception {
 
-		if ("XMLHttpRequest".equals(((HttpServletRequest) request).getHeader("X-Requested-With"))) {
-
+		if (isXHR(request)) {
 			if (isLoginRequest(request, response) && isLoginSubmission(request, response)) {
-
-				ObjectMapper mapper = new ObjectMapper();
-				@SuppressWarnings("unchecked")
-				Map<String, String> data = mapper.readValue(request.getInputStream(), Map.class);
-				
-				String username = data.get("username");
-				String password = data.get("password");
-				
-				AuthenticationToken token = createToken(username, password, request, response);
-				 try {
-		            Subject subject = getSubject(request, response);
-		            subject.login(token);
-		            return onLoginSuccess(token, subject, request, response);
-		        } catch (AuthenticationException e) {
-		        }
+				return doLogin(request, response);
 			}
 			((HttpServletResponse) response).setStatus(401);
 			return false;
 		}
 		return super.onAccessDenied(request, response);
+	}
+
+	@SuppressWarnings("unchecked")
+	private boolean doLogin(ServletRequest request, ServletResponse response) throws Exception {
+
+		ObjectMapper mapper = new ObjectMapper();
+		Map<String, String> data = mapper.readValue(request.getInputStream(), Map.class);
+
+		String username = data.get("username");
+		String password = data.get("password");
+
+		AuthenticationToken token = createToken(username, password, request, response);
+
+		try {
+			Subject subject = getSubject(request, response);
+			subject.login(token);
+			return onLoginSuccess(token, subject, request, response);
+		} catch (AuthenticationException e) {
+		}
+		return false;
+	}
+
+	private boolean isXHR(ServletRequest request) {
+		return "XMLHttpRequest".equals(((HttpServletRequest) request).getHeader("X-Requested-With"));
 	}
 }
