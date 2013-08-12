2a42cb117755f2f8dc76b6151329a479219b1450
Pierre Belloy <p.belloy@axelor.com>
2013-08-12 21:25:01 +0200

    Implemented ImageLink widget.

--- axelor-web/src/main/webapp/js/form/form.input.binary.js
+++ axelor-web/src/main/webapp/js/form/form.input.binary.js
@@ -2,27 +2,48 @@
 
 var ui = angular.module('axelor.ui');
 
-ui.formInput('Image', {
-	
+ui.formInput('ImageLink', {
 	css: 'image-item',
-	cellCss: 'form-item image-item',
-	
+	cssClass: 'from-item image-item',
+
 	BLANK: "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7",
-	
+
 	init: function(scope) {
 		var field = scope.field;
-		
+
 		scope.width = field.width || 140;
 		scope.height = field.height || 140;
 	},
 
+	link_readonly: function(scope, element, attrs, model) {
+
+		var BLANK = this.BLANK;
+		var image = element.children('img:first');
+
+		scope.$render_readonly = function() {
+			var content = model.$viewValue || null;
+			if (content == null) {
+				image.get(0).src = BLANK;
+			}
+			image.get(0).src = content;
+		};
+	},
+	template_editable: '<input type="text">',
+	template_readonly:
+		'<div style="min-width: {{width}}px;">'+
+			'<img class="img-polaroid" style="width: {{width}}px; height: {{height}}px;">'+
+		'</div>'
+});
+
+ui.formInput('Image', 'ImageLink', {
+
 	link_editable: function(scope, element, attrs, model) {
-		
+
 		var input = element.children('input:first');
 		var image = element.children('img:first');
 		var buttons = element.children('.btn-group');
 		var BLANK = this.BLANK;
-		
+
 		input.add(buttons).hide();
 		image.add(buttons).hover(function(){
 			buttons.show();
@@ -33,25 +54,25 @@ ui.formInput('Image', {
 		scope.doSelect = function() {
 			input.click();
 		};
-		
+
 		scope.doSave = function() {
 			var content = image.get(0).src;
 			if (content) {
 				window.open(content);
 			}
 		};
-		
+
 		scope.doRemove = function() {
 			image.get(0).src = BLANK;
 			image.get(0).src = null;
 			input.val(null);
 			update(null);
 		};
-		
+
 		input.change(function(e, ui) {
 			var file = input.get(0).files[0];
 			var reader = new FileReader();
-			
+
 			reader.onload = function(e){
 				var content = e.target.result;
 				image.get(0).src = content;
@@ -60,7 +81,7 @@ ui.formInput('Image', {
 
 			reader.readAsDataURL(file);
 		});
-		
+
 		function update(value) {
 			setTimeout(function(){
 				model.$setViewValue(value);
@@ -69,19 +90,6 @@ ui.formInput('Image', {
 		}
 
 		scope.$render_editable = function() {
-			var content = model.$viewValue || null;
-			if (content == null) {
-				image.get(0).src = BLANK;
-			}
-			image.get(0).src = content;
-		};
-	},
-	link_readonly: function(scope, element, attrs, model) {
-
-		var BLANK = this.BLANK;
-		var image = element.children('img:first');
-		
-		scope.$render_readonly = function() {
 			var content = model.$viewValue || null;
 			if (content == null) {
 				image.get(0).src = BLANK;
@@ -98,21 +106,16 @@ ui.formInput('Image', {
 			'<button ng-click="doSave()" class="btn" type="button"><i class="icon-download-alt"></i></button>' +
 			'<button ng-click="doRemove()" class="btn" type="button"><i class="icon-remove"></i></button>' +
 		'</div>' +
-	'</div>',
-	
-	template_readonly:
-	'<div style="min-width: {{width}}px;">'+
-		'<img class="img-polaroid" style="width: {{width}}px; height: {{height}}px;">'+
 	'</div>'
 });
 
 ui.formInput('Binary', {
-	
+
 	css: 'file-item',
 	cellCss: 'form-item file-item',
-	
+
 	link_editable: function(scope, element, attrs, model) {
-		
+
 		var field = scope.field;
 		var input = element.children('input:first').hide();
 		var frame = element.children("iframe").hide();
@@ -120,18 +123,18 @@ ui.formInput('Binary', {
 		scope.doSelect = function() {
 			input.click();
 		};
-		
+
 		scope.doSave = function() {
 			var record = scope.record,
 				model = scope._model;
-			
+
 			var url = "ws/rest/" + model + "/" + record.id + "/" + field.name + "/download";
 			frame.attr("src", url);
 			setTimeout(function(){
 				frame.attr("src", "");
 			},100);
 		};
-		
+
 		scope.doRemove = function() {
 			var record = scope.record;
 			input.val(null);
@@ -159,7 +162,7 @@ ui.formInput('Binary', {
 				});
 			}
 		});
-		
+
 	},
 	template_editable:
 	'<div>' +
