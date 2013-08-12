9f1cbe788114f81691c02f5fba57f8d2f1c9b2fb
pbe-axelor <p.belloy@axelor.com>
2013-08-12 08:50:20 +0100

    Fixed unique-constraint issue (#1276)

--- axelor-tools/src/main/groovy/com/axelor/tool/x2j/pojo/Constraint.groovy
+++ axelor-tools/src/main/groovy/com/axelor/tool/x2j/pojo/Constraint.groovy
@@ -2,27 +2,49 @@ package com.axelor.tool.x2j.pojo
 
 import groovy.util.slurpersupport.NodeChild
 
+import com.google.common.base.CaseFormat
+
 class Constraint {
 
 	String name
 
-	String columns
-	
+	List<String> columns
+
+	List<String> fields
+
 	Entity entity
 
 	Constraint(Entity entity, NodeChild node) {
 		this.entity = entity
 		this.name = node.@name
-		this.columns = node.@columns
-		
-		if (!this.columns) {
-			throw new IllegalArgumentException("Column names not specified.")
+		this.columns = []
+		this.fields = []
+
+		((node.@columns as String).trim().split(/\,/) as List).each { String column ->
+			String field = column
+			if (field.empty) return
+			if (field.contains("_")) {
+				field = CaseFormat.LOWER_UNDERSCORE.to(CaseFormat.LOWER_CAMEL, field)
+			}
+
+			Property p = entity.getField(field)
+
+			fields += field
+			columns += p ? this.column(p) : column
 		}
 	}
 
+	private String column(Property p) {
+		if (p.attrs.column) return p.attrs.column
+		if (p.isReference()) return CaseFormat.LOWER_CAMEL.to(CaseFormat.LOWER_UNDERSCORE, p.name)
+		return p.name
+	}
+
 	List<String> getColumns() {
-		if (this.columns == null || this.columns.empty)
-			return []
-		return this.columns.split(/,/) as List
+		return this.columns
+	}
+
+	List<String> getFields() {
+		return this.fields
 	}
 }
