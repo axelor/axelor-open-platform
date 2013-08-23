b63be906a67ea9a5f1c47fd9efd01b8139b31eea
Pierre Belloy <p.belloy@axelor.com>
2013-08-23 08:13:39 +0200

    Added view menu support.

--- axelor-meta/src/main/java/com/axelor/meta/schema/views/AbstractView.java
+++ axelor-meta/src/main/java/com/axelor/meta/schema/views/AbstractView.java
@@ -36,72 +36,84 @@ public abstract class AbstractView {
 
 	@XmlAttribute
 	private String name;
-	
+
 	@XmlAttribute
 	private String title;
-	
+
 	@XmlAttribute
 	private String model;
-	
+
 	@XmlAttribute
 	private Boolean editable;
-	
+
 	@XmlElementWrapper
 	@XmlElement(name = "button")
 	private List<Button> toolbar;
 
+	@XmlElementWrapper
+	@XmlElement(name = "menu")
+	private List<Menu> menubar;
+
 	public String getName() {
 		return name;
 	}
-	
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
 	public String getDefaultTitle() {
 		return title;
 	}
-	
+
 	public String getTitle() {
 		return JPA.translate(title);
 	}
-	
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
-	
+
 	public String getModel() {
 		if(model != null)
 			return model;
-		
+
 		MetaView view = MetaView.all().filter("self.name = ?1", name).fetchOne();
 		if(view != null && view.getModel() != null){
 			model = view.getModel();
 		}
-		
+
 		return model;
 	}
-	
+
 	public void setModel(String model) {
 		this.model = model;
 	}
-	
+
 	public Boolean getEditable() {
 		return editable;
 	}
-	
+
 	public void setEditable(Boolean editable) {
 		this.editable = editable;
 	}
-	
+
 	public List<Button> getToolbar() {
 		return toolbar;
 	}
-	
+
 	public void setToolbar(List<Button> toolbar) {
 		this.toolbar = toolbar;
 	}
-	
+
+	public List<Menu> getMenubar() {
+		return menubar;
+	}
+
+	public void setMenubar(List<Menu> menubar) {
+		this.menubar = menubar;
+	}
+
 	@XmlTransient
 	public String getType() {
 		try {
---
+++ axelor-meta/src/main/java/com/axelor/meta/schema/views/Menu.java
@@ -0,0 +1,56 @@
+package com.axelor.meta.schema.views;
+
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElements;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlType
+public class Menu {
+
+	@XmlAttribute
+	private String title;
+
+	@XmlAttribute
+	private String icon;
+
+	@XmlAttribute
+	private Boolean showTitle;
+
+	@XmlElements({ @XmlElement(name = "item"), @XmlElement(name = "divider") })
+	private List<MenuItem> items;
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getIcon() {
+		return icon;
+	}
+
+	public void setIcon(String icon) {
+		this.icon = icon;
+	}
+
+	public Boolean getShowTitle() {
+		return showTitle;
+	}
+
+	public void setShowTitle(Boolean showTitle) {
+		this.showTitle = showTitle;
+	}
+
+	public List<MenuItem> getItems() {
+		return items;
+	}
+
+	public void setItems(List<MenuItem> items) {
+		this.items = items;
+	}
+}
