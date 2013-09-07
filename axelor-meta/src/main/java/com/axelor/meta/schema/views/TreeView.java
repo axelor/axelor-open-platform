2724191149e94b82646718640905ac9e477ccbe4
pbe-axelor <p.belloy@axelor.com>
2013-09-07 08:13:37 +0100

    utilize selection value from domain object (tree view)

--- axelor-meta/src/main/java/com/axelor/meta/schema/views/TreeView.java
+++ axelor-meta/src/main/java/com/axelor/meta/schema/views/TreeView.java
@@ -36,6 +36,8 @@ import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlType;
 
+import com.axelor.db.mapper.Mapper;
+import com.axelor.db.mapper.Property;
 import com.axelor.meta.schema.views.Search.SearchField;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
@@ -47,13 +49,13 @@ public class TreeView extends AbstractView {
 
 	@XmlAttribute
 	private Boolean showHeader;
-	
+
 	@XmlElement(name = "column")
 	private List<TreeColumn> columns;
 
 	@XmlElement(name = "node")
 	private List<Node> nodes;
-	
+
 	public Boolean getShowHeader() {
 		return showHeader;
 	}
@@ -61,7 +63,7 @@ public class TreeView extends AbstractView {
 	public List<TreeColumn> getColumns() {
 		return columns;
 	}
-	
+
 	public List<Node> getNodes() {
 		return nodes;
 	}
@@ -78,16 +80,16 @@ public class TreeView extends AbstractView {
 
 		@XmlAttribute
 		private String model;
-		
+
 		@XmlAttribute
 		private String parent;
 
 		@XmlAttribute
 		private String onClick;
-		
+
 		@XmlAttribute
 		private Boolean draggable;
-		
+
 		@XmlAttribute
 		private String domain;
 
@@ -96,19 +98,19 @@ public class TreeView extends AbstractView {
 
 		@XmlElement(name = "field")
 		private List<NodeField> fields;
-		
+
 		public String getModel() {
 			return model;
 		}
-		
+
 		public String getParent() {
 			return parent;
 		}
-		
+
 		public String getOnClick() {
 			return onClick;
 		}
-		
+
 		public Boolean getDraggable() {
 			return draggable;
 		}
@@ -130,16 +132,35 @@ public class TreeView extends AbstractView {
 			return fields;
 		}
 	}
-	
+
 	@XmlType
 	@JsonInclude(Include.NON_NULL)
 	public  static class NodeField extends Field {
-		
+
 		@XmlAttribute
 		private String as;
-		
+
 		public String getAs() {
 			return as;
+		}
+
+		@Override
+		public String getSelection() {
+			String selection = super.getSelection();
+			Class<?> klass = null;
+			if (selection != null) {
+				return selection;
+			}
+			try {
+				klass = Class.forName(getModel());
+			} catch (ClassNotFoundException e) {
+				return null;
+			}
+			Property property = Mapper.of(klass).getProperty(getName());
+			if (property == null) {
+				return null;
+			}
+			return property.getSelection();
 		}
 	}
 }
