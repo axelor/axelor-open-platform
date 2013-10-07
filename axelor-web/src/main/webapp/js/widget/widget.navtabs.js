4917bca3886ca53fa25458c81f1e2d305a55234c
pbe-axelor <p.belloy@axelor.com>
2013-10-07 11:40:16 +0100

    Fixed axelor logo issue

--- axelor-web/src/main/webapp/js/widget/widget.navtabs.js
+++ axelor-web/src/main/webapp/js/widget/widget.navtabs.js
@@ -37,18 +37,6 @@ angular.module('axelor.ui').directive('navTabs', function() {
 				if (value != oldValue) $.event.trigger('adjust');
 			});
 			$(elem).bsTabs();
-			setTimeout(function() {
-				var wrap = elem.children('.nav-tabs-wrap').css('margin-right', 100);
-				var logo = $('<img src="img/axelor.png" width="100" height="32">');
-				$('<a href="http://axelor.com" tabindex="-1" target="_blank"></a>').append(logo)
-					.css("position", "absolute")
-					.css("right", 0).css("top", 0)
-					.css("border-bottom", wrap.find('ul.nav-tabs').css('border-bottom'))
-					.appendTo(elem);
-				
-				logo.height(wrap.height() - 1);
-				wrap.find('ul').height(wrap.height() - 1);
-			});
 		},
 		templateUrl: 'partials/nav-tabs.html'
 	};
