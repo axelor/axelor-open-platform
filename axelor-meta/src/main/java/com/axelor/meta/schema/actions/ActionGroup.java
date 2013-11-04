98e5381bbab2410effb4a3125e518141bbfb9d46
pbe-axelor <p.belloy@axelor.com>
2013-11-04 14:46:45 +0100

    Reverted previous commit about action-validate.

--- axelor-meta/src/main/java/com/axelor/meta/schema/actions/ActionGroup.java
+++ axelor-meta/src/main/java/com/axelor/meta/schema/actions/ActionGroup.java
@@ -190,10 +190,11 @@ public class ActionGroup extends Action {
 
             if (action instanceof ActionValidate && value instanceof Map) {
             	String pending = iter.hasNext() ? this.getPending(iter) : null;
-            	String onConfirm = (String) ((Map<String, Object>) value).get("action");
-            	if (onConfirm != null) {
-            		pending = Joiner.on(",").skipNulls().join(onConfirm, action.getName(), pending);
-            	}
+            	//TODO : #1090
+//            	String onConfirm = (String) ((Map<String, Object>) value).get("action");
+//            	if (onConfirm != null) {
+//            		pending = Joiner.on(",").skipNulls().join(onConfirm, action.getName(), pending);
+//            	}
             	log.debug("wait for validation: {}, {}", name, value);
             	log.debug("pending actions: {}", pending);
 				((Map<String, Object>) value).put("pending", pending);
