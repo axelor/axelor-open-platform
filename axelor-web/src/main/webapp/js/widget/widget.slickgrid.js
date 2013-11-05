fbc33c7e86680b11e4c39eaf47577ebd3a85a3b4
pbe-axelor <p.belloy@axelor.com>
2013-11-05 19:36:28 +0100

    Fixed hilite and groupBy issue (#1594)

--- axelor-web/src/main/webapp/js/widget/widget.slickgrid.js
+++ axelor-web/src/main/webapp/js/widget/widget.slickgrid.js
@@ -868,7 +868,7 @@ Grid.prototype.hilite = function (row, field) {
 		record = this.grid.getDataItem(row),
 		params = null;
 
-	if (!view || !record) {
+	if (!view || !record || record.__group) {
 		return null;
 	}
 
