4e9ea78bb8f3abb5ee71736a49c1e6a4449ef791
Pierre Belloy <p.belloy@axelor.com>
2013-12-27 22:21:01 +0100

    Reverting open tab hack

--- axelor-web/src/main/webapp/js/axelor.nav.js
+++ axelor-web/src/main/webapp/js/axelor.nav.js
@@ -366,25 +366,12 @@ function NavCtrl($scope, $rootScope, $location, NavService) {
 		return NavService.canCloseTab(tab);
 	};
 	
-	function ensureScopeApply(func) {
-		var args = _.rest(arguments);
-		var promise = func.apply(NavService, args);
-		if (promise && promise.then) {
-			promise.then(function () {
-				$scope.applyLater();
-			});
-		} else {
-			$scope.applyLater();
-		}
-		return promise;
-	}
-
 	$scope.openTab = function(tab, options) {
-		return ensureScopeApply(NavService.openTab, tab, options);
+		return NavService.openTab(tab, options);
 	};
 
 	$scope.openTabByName = function(name, options) {
-		return ensureScopeApply(NavService.openTabByName, name, options);
+		return NavService.openTabByName(name, options);
 	};
 
 	$scope.closeTab = function(tab, callback) {
