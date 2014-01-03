7d56694bce4e43c290520db6b2481771a020a7c8
pbe-axelor <p.belloy@axelor.com>
2014-01-03 15:27:21 +0100

    Fixed MasterDetail widget.

--- axelor-web/src/main/webapp/js/form/form.relational.nested.js
+++ axelor-web/src/main/webapp/js/form/form.relational.nested.js
@@ -176,7 +176,7 @@ var EmbeddedEditor = {
 			'<div class="btn-toolbar pull-right">'+
 				'<button type="button" class="btn btn btn-info" ng-click="onClose()" ng-show="isReadonly()"><span x-translate>Back</span></button> '+
 				'<button type="button" class="btn btn-primary" ng-click="onOK()" ng-show="!isReadonly() && canUpdate()"><span x-translate>OK</span></button>'+
-				'<button type="button" class="btn btn-primary" ng-click="onAdd()" ng-show="!isReadonly()"><span x-translate>Add</span></button> '+
+				'<button type="button" class="btn btn-primary" ng-click="onAdd()" ng-show="!isReadonly() && !canUpdate()"><span x-translate>Add</span></button> '+
 				'<button type="button" class="btn btn-danger" ng-click="onClose()" ng-show="!isReadonly()"><span x-translate>Cancel</span></button> '+
 			'</div>'+
 		'</fieldset>'
