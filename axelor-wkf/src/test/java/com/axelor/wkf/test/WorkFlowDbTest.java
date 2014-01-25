c9795b7252b2aa2380b5b5c8da0eb6e160f1d1f8
Pierre Belloy <p.belloy@axelor.com>
2014-01-25 07:02:09 +0100

    Fixed wkf unit tests.

--- axelor-wkf/src/test/java/com/axelor/wkf/test/WorkFlowDbTest.java
+++ axelor-wkf/src/test/java/com/axelor/wkf/test/WorkFlowDbTest.java
@@ -42,8 +42,8 @@ import com.axelor.wkf.WkfTest;
 import com.axelor.wkf.data.CreateData;
 import com.axelor.wkf.db.Node;
 import com.axelor.wkf.db.node.EndEvent;
+import com.axelor.wkf.db.node.NodeTask;
 import com.axelor.wkf.db.node.StartEvent;
-import com.axelor.wkf.db.node.Activity;
 
 @RunWith(GuiceRunner.class)
 @GuiceModules({ WkfTest.class })
@@ -63,7 +63,7 @@ public class WorkFlowDbTest {
 		});
 		
 		Assert.assertEquals(1, StartEvent.allStartEvent().count());
-		Assert.assertEquals(2, Activity.allActivity().count());
+		Assert.assertEquals(2, NodeTask.allNodeTask().count());
 		Assert.assertEquals(1, EndEvent.allEndEvent().count());
 		Assert.assertEquals(4, Node.all().count());
 		
