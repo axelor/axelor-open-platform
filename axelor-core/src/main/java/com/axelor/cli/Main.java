b3f59f68663c2e73c9dfb1121915e7774fa96d98
pbe-axelor <p.belloy@axelor.com>
2014-02-10 13:18:26 +0100

    Refactor command line  class.

--- axelor-core/src/main/java/com/axelor/cli/Main.java
+++ axelor-core/src/main/java/com/axelor/cli/Main.java
@@ -43,57 +43,60 @@ import com.google.inject.Guice;
 import com.google.inject.Injector;
 
 public class Main {
-	
+
+	private static final String PROGRAM_NAME = "axelor";
+
 	static class MainModule extends AbstractModule {
-		
+
 		protected Properties properties = new Properties();
-		
+
 		private String jpaUnit;
 
 		public MainModule(String jpaUnit) {
 			this.jpaUnit = jpaUnit;
 		}
-		
+
 		@Override
 		protected void configure() {
-			
+
 			bind(ObjectMapper.class).toProvider(ObjectMapperProvider.class);
 
 			install(new JpaModule(jpaUnit).properties(properties));
 			install(new AuthModule.Simple().properties(properties));
 		}
 	}
-	
+
 	public static void main(String[] args) {
-		
+		System.exit(process(args));
+	}
+
+	public static Integer process(String[] args) {
+
 		Options opts = new Options();
 		JCommander cmd = new JCommander(opts);
 
-		cmd.setProgramName("axelor");
-		
+		cmd.setProgramName(PROGRAM_NAME);
+
 		try {
 			cmd.parse(args);
 		} catch (Exception e) {
 			System.err.println(e.getMessage());
 			cmd.usage();
-			System.exit(-1);
-			return;
+			return -1;
 		}
 
 		if ((opts.showHelp == Boolean.TRUE)
 				|| (opts.init == Boolean.FALSE && opts.update == Boolean.FALSE)) {
 			cmd.usage();
-			System.exit(0);
-			return;
+			return 0;
 		}
-		
+
 		Injector injector = Guice.createInjector(new MainModule(opts.unit));
 		ModuleManager manager = injector.getInstance(ModuleManager.class);
-		
+
 		if (opts.init == Boolean.TRUE) {
 			manager.initialize(opts.update == Boolean.TRUE, opts.importDemo == Boolean.TRUE);
-			System.exit(0);
-			return;
+			return 0;
 		}
 
 		String[] names = {};
@@ -101,6 +104,6 @@ public class Main {
 			names = opts.modules.toArray(new String[] {});
 		}
 		manager.update(opts.importDemo == Boolean.TRUE, names);
-		System.exit(0);
+		return 0;
 	}
 }
