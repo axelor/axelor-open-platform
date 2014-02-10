1ca4b4e8f4539bc7fbfcc3b58181dfb08d90c4e8
pbe-axelor <p.belloy@axelor.com>
2014-02-10 08:14:36 +0100

    Implemented command line tool.
    The axelor command line tool allows to initialize or
    update database with various options.

--- axelor-core/scripts/axelor.sh
+++ axelor-core/scripts/axelor.sh
@@ -31,5 +31,5 @@
 #
 
 args=`echo "$@"`
-exec mvn -q exec:java -Dexec.mainClass="com.axelor.cli.CliRunner" -Dexec.args="$args"
+exec mvn -q exec:java -Dexec.mainClass="com.axelor.cli.Main" -Dexec.args="$args"
 
