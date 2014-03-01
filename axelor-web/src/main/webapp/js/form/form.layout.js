dfbe5cb5b120dbea582187076b9ebcdd0930479e
Pierre Belloy <p.belloy@axelor.com>
2014-03-01 15:04:09 +0100

    Support for disabling hot-edit feature (RM-1891)

--- axelor-web/src/main/webapp/js/form/form.layout.js
+++ axelor-web/src/main/webapp/js/form/form.layout.js
@@ -158,7 +158,7 @@ function TableLayout(items, attrs, $scope, $compile) {
 				if (_.isArray(widths) && widths[i]) {
 					el.width(widths[i]);
 				}
-				if ($(cell.elem).is('.form-item-container')) {
+				if ($(cell.elem).is('.form-item-container') && __appSettings['view.form.hot-edit']) {
 					$(cell.elem).prepend($('<span class="icon icon-pencil hot-edit-icon"></span>'));
 				}
 				numCells += cell.colspan || 1;
