e54a0ced3d0158451a5e38837c35182b018b5ede
pbe-axelor <p.belloy@axelor.com>
2014-04-03 15:48:02 +0100

    Template api for file/reader objects (#1953).

--- axelor-core/src/main/java/com/axelor/text/Templates.java
+++ axelor-core/src/main/java/com/axelor/text/Templates.java
@@ -17,6 +17,10 @@
  */
 package com.axelor.text;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.Reader;
+
 /**
  * This interface defines API for template engine integration.
  * 
@@ -31,4 +35,26 @@ public interface Templates {
 	 * @return an instance of {@link Template}
 	 */
 	Template fromText(String text);
+
+	/**
+	 * Create a new {@link Template} instance from the given template file.
+	 * 
+	 * @param file
+	 *            the template file
+	 * @return an instance of {@link Template}
+	 * @throws IOException
+	 *             if file read throws {@link IOException}
+	 */
+	Template from(File file) throws IOException;
+
+	/**
+	 * Create a new {@link Template} instance from the given reader.
+	 * 
+	 * @param reader
+	 *            the {@link Reader} to read the template
+	 * @return an instance of {@link Template}
+	 * @throws IOException
+	 *             if reader throws {@link IOException}
+	 */
+	Template from(Reader reader) throws IOException;
 }
