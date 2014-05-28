e839fb0a064e8438fc271e4d32dfd57719d78063
Pierre Belloy <p.belloy@axelor.com>
2014-05-28 16:49:15 +0200

    Fixed french translations

--- axelor-core/src/main/java/com/axelor/db/JpaSecurity.java
+++ axelor-core/src/main/java/com/axelor/db/JpaSecurity.java
@@ -23,37 +23,37 @@ import com.axelor.i18n.I18n;
 import com.axelor.rpc.filter.Filter;
 
 public interface JpaSecurity {
-	
+
 	public static enum AccessType {
-		
-		READ	("You are not authorized to read this resource."),
-		WRITE	("You are not authorized to update this resource."),
-		CREATE	("You are not authorized to create this resource."),
-		REMOVE	("You are not authorized to remove this resource."),
-		EXPORT 	("You are not authorized to export the data.");
+
+		READ	(/*$$(*/ "You are not authorized to read this resource." /*)*/),
+		WRITE	(/*$$(*/ "You are not authorized to update this resource." /*)*/),
+		CREATE	(/*$$(*/ "You are not authorized to create this resource." /*)*/),
+		REMOVE	(/*$$(*/ "You are not authorized to remove this resource." /*)*/),
+		EXPORT 	(/*$$(*/ "You are not authorized to export the data." /*)*/);
 
 		private String message;
 		private AccessType(String message) {
 			this.message = message;
 		}
-		
+
 		public String getMessage() {
 			return I18n.get(message);
 		}
 	}
-	
+
 	public static final AccessType CAN_READ = AccessType.READ;
 	public static final AccessType CAN_WRITE = AccessType.WRITE;
 	public static final AccessType CAN_CREATE = AccessType.CREATE;
 	public static final AccessType CAN_REMOVE = AccessType.REMOVE;
 	public static final AccessType CAN_EXPORT = AccessType.EXPORT;
-	
+
 	Set<AccessType> getAccessTypes(Class<? extends Model> model, Long id);
-	
+
 	boolean hasRole(String name);
 
 	Filter getFilter(AccessType type, Class<? extends Model> model, Long... ids);
-	
+
 	boolean isPermitted(AccessType type, Class<? extends Model> model, Long... ids);
 
 	void check(AccessType type, Class<? extends Model> model, Long... ids);
