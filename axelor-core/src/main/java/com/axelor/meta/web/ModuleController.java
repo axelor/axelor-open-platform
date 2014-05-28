7c3828ceaf4d83e8a7575078b8937a1d87f7b6cf
pbe-axelor <p.belloy@axelor.com>
2014-05-28 17:05:08 +0100

    Show message on module install/uninstall (#2106)

--- axelor-core/src/main/java/com/axelor/meta/web/ModuleController.java
+++ axelor-core/src/main/java/com/axelor/meta/web/ModuleController.java
@@ -17,8 +17,13 @@
  */
 package com.axelor.meta.web;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.inject.Inject;
 
+import com.axelor.i18n.I18n;
+import com.axelor.meta.db.MetaModule;
 import com.axelor.meta.loader.ModuleManager;
 import com.axelor.rpc.ActionResponse;
 import com.axelor.rpc.Response;
@@ -28,12 +33,52 @@ public class ModuleController {
 	
 	@Inject
 	private ModuleManager loader;
+	
+	private String doAction(String module, boolean uninstall) {
+		final List<String> previous = new ArrayList<>();
+		final List<String> updated = new ArrayList<>();
+		final StringBuilder sb = new StringBuilder();
+		
+		for (MetaModule m : MetaModule.all().filter("installed = true").fetch()) {
+			previous.add(m.getName());
+		}
+		
+		if (uninstall) {
+			loader.uninstall(module);
+			sb.append(I18n.get("Following modules have been uninstalled:"));
+		} else {
+			loader.install(module, false, false);
+			sb.append(I18n.get("Following modules have been installed:"));
+		}
+		
+		for (MetaModule m : MetaModule.all().filter("installed = true").fetch()) {
+			String name = m.getName();
+			if (uninstall) {
+				previous.remove(name);
+			} else if (!previous.contains(name)) {
+				updated.add(name);
+			}
+		}
+		
+		if (uninstall) {
+			updated.clear();
+			updated.addAll(previous);
+		}
+		
+		sb.append("<br><br>");
+		sb.append("<ul>");
+		for (String mn : updated) {
+			sb.append("<li>").append(mn).append("</li>");
+		}
+		sb.append("</ul>");
+		
+		return sb.toString();
+	}
 
 	public Response install(final String name) {
 		final ActionResponse response = new ActionResponse();
 		try {
-			loader.install(name, false, false);
-			response.setReload(true);
+			response.setFlash(doAction(name, false));
 			response.setSignal("refresh-app", true);
 		} catch (Exception e) {
 			response.setException(e);
@@ -45,8 +90,7 @@ public class ModuleController {
 	public Response uninstall(final String name) {
 		final ActionResponse response = new ActionResponse();
 		try {
-			loader.uninstall(name);
-			response.setReload(true);
+			response.setFlash(doAction(name, true));
 			response.setSignal("refresh-app", true);
 		} catch (Exception e) {
 			response.setException(e);
