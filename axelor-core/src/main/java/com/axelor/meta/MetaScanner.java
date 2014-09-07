af13723cfca4ddb7abb7917652c75437683a831d
pbe-axelor <p.belloy@axelor.com>
2014-09-07 14:13:20 +0100

    Fixed meta file scanner issues.
    Resources of unpacked modules should be
    loaded from the build/resources/main folder.

--- axelor-core/src/main/java/com/axelor/meta/MetaScanner.java
+++ axelor-core/src/main/java/com/axelor/meta/MetaScanner.java
@@ -18,6 +18,9 @@
 package com.axelor.meta;
 
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import com.axelor.common.reflections.Reflections;
 import com.axelor.meta.loader.ModuleManager;
@@ -34,12 +37,21 @@ public class MetaScanner {
 	}
 	
 	public static ImmutableList<URL> findAll(String module, String directory, String pattern) {
-		final String path = ModuleManager.getModulePath(module);
+
+		String path = ModuleManager.getModulePath(module);
 		if (path == null) {
 			return ImmutableList.of();
 		}
-		final String namePattern = "(^|/|\\\\)" + directory + "(/|\\\\)" + pattern;
-		final String pathPattern = String.format("^(%s)", path.replaceFirst("module\\.properties$", ""));
+
+		String namePattern = "(^|/|\\\\)" + directory + "(/|\\\\)" + pattern;
+		String pathPattern = String.format("^%s", path.replaceFirst("module\\.properties$", ""));
+
+		Path parent = Paths.get(path).getParent();
+		Path resources = parent.resolve("../../resources/main").normalize();
+
+		if (Files.exists(resources)) {
+			pathPattern = String.format("(%s)|(^%s)", pathPattern, resources);
+		}
 		return Reflections.findResources().byName(namePattern).byURL(pathPattern).find();
 	}
 }
