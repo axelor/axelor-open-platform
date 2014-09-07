a7dc56de15874069654620db4054dffe8b1ae344
Pierre Belloy <p.belloy@axelor.com>
2014-09-07 14:13:01 +0200

    Refactoring old cli command.

--- axelor-core/src/main/java/com/axelor/commands/DBCommands.java
+++ axelor-core/src/main/java/com/axelor/commands/DBCommands.java
@@ -15,32 +15,51 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package com.axelor.cli;
+package com.axelor.commands;
 
+import java.util.List;
 import java.util.Properties;
 
 import com.axelor.app.AppModule;
+import com.axelor.app.AppSettings;
 import com.axelor.auth.AuthModule;
 import com.axelor.db.JpaModule;
 import com.axelor.meta.loader.ModuleManager;
 import com.axelor.rpc.ObjectMapperProvider;
 import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 
-public class Main {
+public class DBCommands {
 
 	private static final String PROGRAM_NAME = "axelor";
+	private static final String PERSISTENCE_UNIT = "persistenceUnit";
 
-	static class MainModule extends AbstractModule {
+	static class MyOptions {
+
+		@Parameter( names = { "-h", "--help" }, description = "show this help message", help = true)
+		public Boolean showHelp;
+
+		@Parameter( names = { "-i", "--init" }, description = "initialize the database")
+		public Boolean init;
+
+		@Parameter( names = { "-u", "--update" }, description = "update the installed modules")
+		public Boolean update;
+
+		@Parameter( names = { "-m", "--modules" }, description = "list of modules to update", variableArity = true)
+		public List<String> modules;
+	}
+
+	static class MyModule extends AbstractModule {
 
 		protected Properties properties = new Properties();
 
 		private String jpaUnit;
 
-		public MainModule(String jpaUnit) {
+		public MyModule(String jpaUnit) {
 			this.jpaUnit = jpaUnit;
 		}
 
@@ -55,19 +74,9 @@ public class Main {
 		}
 	}
 
-	public static void main(String[] args) {
-		int status = 0;
-		try {
-			status = process(args);
-		} catch (Exception e) {
-			status = -1;
-		}
-		System.exit(status);
-	}
-
 	public static int process(String[] args) {
 
-		Options opts = new Options();
+		MyOptions opts = new MyOptions();
 		JCommander cmd = new JCommander(opts);
 
 		cmd.setProgramName(PROGRAM_NAME);
@@ -86,11 +95,13 @@ public class Main {
 			return 0;
 		}
 
-		Injector injector = Guice.createInjector(new MainModule(opts.unit));
+		Injector injector = Guice.createInjector(new MyModule(PERSISTENCE_UNIT));
 		ModuleManager manager = injector.getInstance(ModuleManager.class);
 
+		boolean demo = AppSettings.get().getBoolean("data.import.demo-data", true);
+
 		if (opts.init == Boolean.TRUE) {
-			manager.initialize(opts.update == Boolean.TRUE, opts.importDemo == Boolean.TRUE);
+			manager.initialize(opts.update == Boolean.TRUE, demo);
 			return 0;
 		}
 
@@ -98,7 +109,17 @@ public class Main {
 		if (opts.modules != null) {
 			names = opts.modules.toArray(new String[] {});
 		}
-		manager.update(opts.importDemo == Boolean.TRUE, names);
+		manager.update(demo, names);
 		return 0;
+	}
+
+	public static void main(String[] args) {
+		int status = 0;
+		try {
+			status = process(args);
+		} catch (Exception e) {
+			status = -1;
+		}
+		System.exit(status);
 	}
 }
