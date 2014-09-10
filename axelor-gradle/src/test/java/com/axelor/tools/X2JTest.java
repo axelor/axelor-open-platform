db378a9ea4f68cc1cae630fce281a0c28badbc1c
pbe-axelor <p.belloy@axelor.com>
2014-09-10 06:01:33 +0100

    Fixed import statement missing issue.

--- axelor-gradle/src/test/java/com/axelor/tools/X2JTest.java
+++ axelor-gradle/src/test/java/com/axelor/tools/X2JTest.java
@@ -34,6 +34,8 @@ public class X2JTest {
 	@Test
 	public void testDomains() throws IOException {
 
+		System.setProperty("codegen.pojo", "true");
+
 		File domainPath = new File("src/test/resources/domains");
 		File outputPath = new File("build/src-gen");
 
