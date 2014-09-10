30625548dae8162c983f2d30aac989d8e64b4ad3
Pierre Belloy <p.belloy@axelor.com>
2014-09-10 13:38:53 +0200

    Fixed button-group spacing issue (RM-2339)

--- axelor-web/src/main/webapp/js/form/form.container.js
+++ axelor-web/src/main/webapp/js/form/form.container.js
@@ -336,6 +336,18 @@ ui.formWidget('Tab', {
 });
 
 ui.formWidget('ButtonGroup', {
+
+	link: function (scope, element, attrs) {
+		var count = 0;
+		scope.$watch(function () {
+			var visible = element.children('a.btn:visible').size();
+			if (visible !== count) {
+				element.children('a.btn:visible')
+					.css('max-width', Math.round(100/visible) + '%')
+					.css('width', Math.round(100/visible) + '%');
+			}
+		});
+	},
 	transclude: true,
 	template_editable: null,
 	template_readonly: null,
