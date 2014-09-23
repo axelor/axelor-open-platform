1169d90fefb93a563a4c360a113d74bbb33a11de
Pierre Belloy <p.belloy@axelor.com>
2014-09-23 10:35:07 +0200

    Implemented VersionUtils
    The VersionUtils can be used to get version
    information of the axelor modules.

---
+++ axelor-common/src/test/java/com/axelor/common/TestVersionUtils.java
@@ -0,0 +1,46 @@
+/**
+ * Axelor Business Solutions
+ *
+ * Copyright (C) 2012-2014 Axelor (<http://axelor.com>).
+ *
+ * This program is free software: you can redistribute it and/or  modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.axelor.common;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.axelor.common.VersionUtils.Version;
+
+public class TestVersionUtils {
+
+	@Test
+	public void test() {
+
+		Version v1 = new Version("3.0.1");
+		Version v2 = new Version("3.0.1-rc1");
+
+		Assert.assertEquals("3.0", v1.feature);
+		Assert.assertEquals("3.0", v2.feature);
+
+		Assert.assertEquals(3, v1.major);
+		Assert.assertEquals(0, v1.minor);
+		Assert.assertEquals(1, v1.patch);
+		Assert.assertEquals(0, v1.rc);
+
+		Assert.assertEquals(3, v2.major);
+		Assert.assertEquals(0, v2.minor);
+		Assert.assertEquals(1, v2.patch);
+		Assert.assertEquals(1, v2.rc);
+	}
+}
