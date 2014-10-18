99ab62792cbd41ccb23f544bc6882fde4df8becb
Pierre Belloy <p.belloy@axelor.com>
2014-10-18 17:33:20 +0200

    Fix menu toggle issue

--- axelor-web/src/main/webapp/js/axelor.app.js
+++ axelor-web/src/main/webapp/js/axelor.app.js
@@ -81,7 +81,7 @@
 				small: false,
 				large: false
 			};
-			device.large = $(window).width() > 767;
+			device.large = $(window).width() > 768;
 			device.small = !device.large;
 			device.mobile = /Mobile|Android|iPhone|iPad|iPod|BlackBerry|Windows Phone/i.test(navigator.userAgent);
 			device.webkit = /Webkit/i.test(navigator.userAgent);
