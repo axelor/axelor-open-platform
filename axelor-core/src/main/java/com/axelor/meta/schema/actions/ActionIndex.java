fdcabf14c32303284adc731986693ee55b197f04
admin <admin@axelor.com>
2015-07-06 15:58:46 +0200

    Fix indexed action execution
    Wrong usages of thread local variable to
    keep track of index is causing wrong index
    information.
    
    Fixes RM-3630, RM-3463

--- axelor-core/src/main/java/com/axelor/meta/schema/actions/ActionIndex.java
+++ axelor-core/src/main/java/com/axelor/meta/schema/actions/ActionIndex.java
@@ -17,20 +17,7 @@
  */
 package com.axelor.meta.schema.actions;
 
-abstract class ActionIndex extends Action {
+abstract class ActionIndex<T extends ActionIndex<?>> extends Action {
 
-	private static final ThreadLocal<Integer> INDEX = new ThreadLocal<Integer>();
-	
-	public void setIndex(int index) {
-		INDEX.set(index);
-	}
-
-	public int getIndex() {
-		final Integer n = INDEX.get();
-		if (n == null) {
-			return 0;
-		}
-		INDEX.remove();
-		return n;
-	}
+	protected abstract T copy(int index);
 }
