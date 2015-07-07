00a221099627a287d4d37ec17e16c3b632413181
admin <admin@axelor.com>
2015-07-07 15:40:00 +0200

    Fix save action issues in popup editor
    Fixes RM-3684

--- axelor-web/src/main/webapp/js/form/form.actions.js
+++ axelor-web/src/main/webapp/js/form/form.actions.js
@@ -260,7 +260,11 @@ ActionHandler.prototype = {
 
 	handle: function() {
 		var action = this.action.trim();
-		return this._handleAction(action);
+		var promise = this._handleAction(action);
+		if (this.scope.$actionPromises) {
+			this.scope.$actionPromises.push(promise);
+		}
+		return promise;
 	},
 	
 	_blockUI: function() {
