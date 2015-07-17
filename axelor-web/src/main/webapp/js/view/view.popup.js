8430ba25533d4eeebfb11ddb9ce8261fc6b8211c
admin <admin@axelor.com>
2015-07-17 16:10:49 +0200

    Revert e34548 and 96ee19 for a point release

--- axelor-web/src/main/webapp/js/view/view.popup.js
+++ axelor-web/src/main/webapp/js/view/view.popup.js
@@ -162,16 +162,16 @@ function EditorCtrl($scope, $element, DataSource, ViewService, $q) {
 					});
 				});
 			}
-			if ($scope.isValid()) {
-				close(record);
-			}
+			close(record);
 		});
 	};
 	
 	$scope.onOK = function() {
-		$scope.ajaxStop(function () {
-			afterActions(onOK);
-		}, 100);
+		if ($scope.isValid()) {
+			setTimeout(function () {
+				afterActions(onOK);
+			});
+		}
 	};
 
 	$scope.onBeforeClose = function(event, ui) {
