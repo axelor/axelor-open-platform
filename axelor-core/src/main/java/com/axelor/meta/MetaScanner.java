969fcdcf8e66cdd42a16e183dc7968e5c987a4d3
admin <admin@axelor.com>
2015-08-31 16:40:32 +0200

    Add optimized reflection utilities to meta scanner
    Added optimized classpath scanning methods, delegated
    to relfection APIs using a custom classloader.

--- axelor-core/src/main/java/com/axelor/meta/MetaScanner.java
+++ axelor-core/src/main/java/com/axelor/meta/MetaScanner.java
@@ -17,25 +17,54 @@
  */
 package com.axelor.meta;
 
+import java.io.IOException;
 import java.net.URL;
+import java.net.URLClassLoader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
+import com.axelor.common.reflections.ClassFinder;
 import com.axelor.common.reflections.Reflections;
 import com.axelor.meta.loader.ModuleManager;
 import com.google.common.collect.ImmutableList;
 
+/**
+ * This class provides some utility methods to scan classpath for
+ * resources/classes.
+ *
+ */
 public class MetaScanner {
 	
 	private MetaScanner() {
 		
 	}
 
-	public static ImmutableList<URL> findAll(String regex) {
-		return Reflections.findResources().byName(regex).find();
+	/**
+	 * Find all resources matched by the given pattern.
+	 * 
+	 * @param pattern
+	 *            the resource name pattern to match
+	 * @return list of resources matched
+	 */
+	public static ImmutableList<URL> findAll(String pattern) {
+		return Reflections.findResources(loader()).byName(pattern).find();
 	}
-	
+
+	/**
+	 * Find all resources within a directory of the given module matching the
+	 * pattern.
+	 * 
+	 * @param module
+	 *            the module name
+	 * @param directory
+	 *            the directory name
+	 * @param pattern
+	 *            the resource name pattern to match
+	 * @return list of resources matched
+	 */
 	public static ImmutableList<URL> findAll(String module, String directory, String pattern) {
 
 		URL pathUrl = ModuleManager.getModulePath(module);
@@ -57,5 +86,62 @@ public class MetaScanner {
 		}
 
 		return Reflections.findResources().byName(namePattern).byURL(pathPattern).find();
+	}
+
+	/**
+	 * Find module path URLs as in current classpath.
+	 * 
+	 */
+	public static URL[] findURLs() {
+		final List<URL> path = new ArrayList<>();
+		final URLClassLoader loader = (URLClassLoader) Thread.currentThread().getContextClassLoader();
+		for (URL item : loader.getURLs()) {
+			final URL[] urls = { item };
+			try (final URLClassLoader cl = new URLClassLoader(urls)) {
+				URL res = cl.findResource("module.properties");
+				if (res == null) {
+					res = cl.findResource("application.properties");
+				}
+				if (res != null) {
+					path.add(item);
+				}
+			} catch (IOException e) {
+			}
+		}
+		return path.toArray(new URL[]{});
+	}
+
+	private static ClassLoader loader() {
+		final ClassLoader loader = Thread.currentThread().getContextClassLoader();
+		return new URLClassLoader(findURLs(), null) {
+			@Override
+			public Class<?> loadClass(String name) throws ClassNotFoundException {
+				return loader.loadClass(name);
+			}
+		};
+	}
+
+	/**
+	 * Delegates to {@link Reflections#findSubTypesOf(Class, ClassLoader)}
+	 * method and uses custom {@link ClassLoader} to speedup searching.
+	 * 
+	 * This method search within module jar/directories only.
+	 * 
+	 * @see Reflections#findSubTypesOf(Class)
+	 */
+	public static <T> ClassFinder<T> findSubTypesOf(Class<T> type) {
+		return Reflections.findSubTypesOf(type, loader());
+	}
+
+	/**
+	 * Delegates to {@link Reflections#findTypes(ClassLoader)}
+	 * method and uses custom {@link ClassLoader} to speedup searching.
+	 * 
+	 * This method search within module jar/directories only.
+	 * 
+	 * @see Reflections#findTypes()
+	 */
+	public static ClassFinder<?> findTypes() {
+		return Reflections.findTypes(loader());
 	}
 }
