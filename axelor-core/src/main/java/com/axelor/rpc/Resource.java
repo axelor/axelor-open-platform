5a812abeb624bdeb9c6c50d11bfb68ec89971965
admin <admin@axelor.com>
2015-12-24 14:13:03 +0100

    Format number & date values in csv export
    Fixes RM-4486

--- axelor-core/src/main/java/com/axelor/rpc/Resource.java
+++ axelor-core/src/main/java/com/axelor/rpc/Resource.java
@@ -39,6 +39,9 @@ import javax.persistence.EntityTransaction;
 import javax.persistence.OptimisticLockException;
 
 import org.hibernate.StaleObjectStateException;
+import org.joda.time.DateTime;
+import org.joda.time.LocalDate;
+import org.joda.time.LocalDateTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -58,6 +61,7 @@ import com.axelor.db.mapper.Property;
 import com.axelor.db.mapper.PropertyType;
 import com.axelor.i18n.I18n;
 import com.axelor.i18n.I18nBundle;
+import com.axelor.i18n.L10n;
 import com.axelor.inject.Beans;
 import com.axelor.meta.MetaPermissions;
 import com.axelor.meta.MetaStore;
@@ -503,6 +507,8 @@ public class Resource<T extends Model> {
 
 		List<?> data = selector.values(limit, offset);
 
+		final L10n formatter = L10n.getInstance();
+
 		while(!data.isEmpty()) {
 
 			for(Object item : data) {
@@ -514,6 +520,18 @@ public class Resource<T extends Model> {
 					Object objValue = value == null ? "" : value;
 					if(selection.containsKey(index-3)) {
 						objValue = selection.get(index-3).get(objValue.toString());
+					}
+					if (objValue instanceof Number) {
+						objValue = formatter.format((Number) objValue);
+					}
+					if (objValue instanceof LocalDate) {
+						objValue = formatter.format((LocalDate) objValue);
+					}
+					if (objValue instanceof LocalDateTime) {
+						objValue = formatter.format((LocalDateTime) objValue);
+					}
+					if (objValue instanceof DateTime) {
+						objValue = formatter.format((DateTime) objValue);
 					}
 					String strValue = objValue == null ? "" : escapeCsv(objValue.toString());
 					line.add(strValue);
