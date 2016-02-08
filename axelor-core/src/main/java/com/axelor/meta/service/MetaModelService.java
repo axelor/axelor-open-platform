0cc91dd917003f31c268ecfda0c14cac3a869596
admin <admin@axelor.com>
2016-02-08 14:30:53 +0100

    Generate MetaField instances for audit fields
    Closes RM-4674

--- axelor-core/src/main/java/com/axelor/meta/service/MetaModelService.java
+++ axelor-core/src/main/java/com/axelor/meta/service/MetaModelService.java
@@ -18,6 +18,7 @@
 package com.axelor.meta.service;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
@@ -34,7 +35,9 @@ import org.hibernate.annotations.Formula;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.axelor.auth.db.AuditableModel;
 import com.axelor.db.JPA;
+import com.axelor.db.Model;
 import com.axelor.db.Query;
 import com.axelor.db.annotations.Widget;
 import com.axelor.meta.db.MetaField;
@@ -147,7 +150,7 @@ public class MetaModelService {
 		
 		MetaField metaField = null;
 		
-		if (!field.isSynthetic() && !field.isAnnotationPresent(Formula.class)){
+		if (!field.isSynthetic() && !field.isAnnotationPresent(Formula.class) && !Modifier.isTransient(field.getModifiers())){
 			
 			log.trace("Create field : {}", field.getName());
 			
@@ -209,14 +212,42 @@ public class MetaModelService {
 		
 		List<MetaField> modelFields = new ArrayList<MetaField>();
 		MetaField metaField = new MetaField();
-		
-		for (Field field : klass.getDeclaredFields()){
-			metaField = this.createField(metaModel, field);
-			if (metaField != null){
+
+		// first add id field
+		Field idField = null;
+		try {
+			idField = klass.getDeclaredField("id");
+			idField = Model.class.getDeclaredField("id");
+		} catch (Exception e) {
+		}
+		if (idField != null) {
+			metaField = this.createField(metaModel, idField);
+			if (metaField != null) {
 				modelFields.add(metaField);
 			}
 		}
-		
+
+		// then own fields
+		for (Field field : klass.getDeclaredFields()) {
+			if ("id".equals(field.getName()) && idField != null) {
+				continue;
+			}
+			metaField = this.createField(metaModel, field);
+			if (metaField != null) {
+				modelFields.add(metaField);
+			}
+		}
+
+		// finally audit fields
+		if (AuditableModel.class.isAssignableFrom(klass)) {
+			for (Field field : AuditableModel.class.getDeclaredFields()) {
+				metaField = this.createField(metaModel, field);
+				if (metaField != null) {
+					modelFields.add(metaField);
+				}
+			}
+		}
+
 		return modelFields;
 	}
 	
