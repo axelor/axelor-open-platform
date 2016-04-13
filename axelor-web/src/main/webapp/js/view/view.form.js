c393cc2ffd82f6c45880c9cd1bd002e0b70cfe8c
admin <admin@axelor.com>
2016-04-13 11:24:49 +0200

    Revert support for silent reload

--- axelor-web/src/main/webapp/js/view/view.form.js
+++ axelor-web/src/main/webapp/js/view/view.form.js
@@ -435,7 +435,7 @@ function FormViewCtrl($scope, $element) {
 			checkVersion = "" + context.__check_version;
 		};
 
-		if (!record.id || !(checkVersion === "true" || checkVersion === "silent")) {
+		if (!record.id || checkVersion !== "true") {
 			return;
 		}
 
@@ -443,11 +443,6 @@ function FormViewCtrl($scope, $element) {
 			if (verified) {
 				return;
 			}
-
-			if (checkVersion === "silent") {
-				return $scope.reload();
-			}
-
 			axelor.dialogs.confirm(
 					_t("The record has been updated or delete by another action.") + "<br>" +
 					_t("Would you like to reload the current record?"),
