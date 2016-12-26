1e58840609b2a9af4e7698da8fe67aa77b1ed30e
admin <admin@axelor.com>
2016-12-26 15:50:20 +0100

    Fix file name encoding issue
    Files with accents in names, when uploaded with some binary widgets
    doesn't save file name properly.
    
    Fixes RM-5500

---
+++ axelor-web/src/main/java/com/axelor/web/service/InputPartFilter.java
@@ -0,0 +1,35 @@
+/**
+ * Axelor Business Solutions
+ *
+ * Copyright (C) 2005-2016 Axelor (<http://axelor.com>).
+ *
+ * This program is free software: you can redistribute it and/or  modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.axelor.web.service;
+
+import java.io.IOException;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.ext.Provider;
+
+import org.jboss.resteasy.plugins.providers.multipart.InputPart;
+
+@Provider
+public class InputPartFilter implements ContainerRequestFilter {
+
+	@Override
+	public void filter(ContainerRequestContext requestContext) throws IOException {
+		requestContext.setProperty(InputPart.DEFAULT_CHARSET_PROPERTY, "UTF-8");
+	}
+}
