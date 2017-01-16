13670450597521c92720b915d1fabc9ea40418ca
admin <admin@axelor.com>
2017-01-16 13:55:00 +0100

    Add minified css/js to login.jsp
    - upgraded npm/gulp dependencies
    - login.jsp is now using css/js bundle
    
    Closes RM-5284

--- axelor-web/src/main/webapp/gulpfile.js
+++ axelor-web/src/main/webapp/gulpfile.js
@@ -17,7 +17,7 @@
  */
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
-var minifyCss = require('gulp-minify-css');
+var cleanCss = require('gulp-clean-css');
 var autoprefixer = require('gulp-autoprefixer');
 var concat = require('gulp-concat');
 var rename = require('gulp-rename');
@@ -43,35 +43,52 @@ function jsList(input) {
 	return files;
 }
 
+function minifyCss(name) {
+	return function () {
+		return gulp.src('css/' + name + '.css')
+		  	.pipe(autoprefixer())
+		    .pipe(cleanCss())
+		    .pipe(base64({extensions: ['svg', 'png', 'jpg', 'gif'] }))
+		    .pipe(rename(name + '.min.css'))
+		    .pipe(gulp.dest('css'))
+		  	.pipe(gzip())
+		  	.pipe(rename(name + '.gzip.css'))
+		  	.pipe(gulp.dest('css'));
+	};
+}
+
+function minifyJs(name) {
+	return function () {
+		return gulp.src(jsList('js/' + name + '.js'))
+		  	.pipe(concat(name + '.min.js'))
+		    .pipe(uglify({ compress: false }))
+		    .pipe(gulp.dest('js'))
+		    .pipe(gzip())
+		    .pipe(rename(name + '.gzip.js'))
+		    .pipe(gulp.dest('js'));
+	};
+}
+
 gulp.task("clean", function() {
 	return del.sync([
 		'css/application.min.*',
 		'css/application.gzip.*',
+		'css/application.login.min.*',
+		'css/application.login.gzip.*',
 		'js/application.min.*',
-		'js/application.gzip.*'
+		'js/application.gzip.*',
+		'js/application.login.min.*',
+		'js/application.login.gzip.*'
 	]);
 });
 
-gulp.task('minify-css', function() {
-  return gulp.src('css/application.css')
-  	.pipe(autoprefixer())
-    .pipe(minifyCss())
-    .pipe(base64({extensions: ['svg', 'png', 'jpg', 'gif'] }))
-    .pipe(rename('application.min.css'))
-    .pipe(gulp.dest('css'))
-  	.pipe(gzip())
-  	.pipe(rename('application.gzip.css'))
-  	.pipe(gulp.dest('css'));
-});
+gulp.task('minify-css:app', minifyCss('application'));
+gulp.task('minify-css:login', minifyCss('application.login'));
 
-gulp.task('minify-js', function() {
-  return gulp.src(jsList('js/application.js'))
-  	.pipe(concat('application.min.js'))
-    .pipe(uglify({ compress: false }))
-    .pipe(gulp.dest('js'))
-    .pipe(gzip())
-    .pipe(rename('application.gzip.js'))
-    .pipe(gulp.dest('js'));
-});
+gulp.task('minify-js:app', minifyJs('application'));
+gulp.task('minify-js:login', minifyJs('application.login'));
+
+gulp.task('minify-css', ['minify-css:app', 'minify-css:login']);
+gulp.task('minify-js', ['minify-js:app', 'minify-js:login']);
 
 gulp.task('default', ['minify-css', 'minify-js']);
--- axelor-web/src/main/webapp/login.jsp
+++ axelor-web/src/main/webapp/login.jsp
@@ -17,11 +17,12 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 --%>
-<%@page import="java.util.Calendar"%>
-<%@page import="java.util.Date"%>
+<%@ taglib prefix="x" uri="WEB-INF/axelor.tld" %>
 <%@ page language="java" session="true" %>
-<%@ page import="com.axelor.i18n.I18n" %>
 <%@ page contentType="text/html; charset=UTF-8" %>
+<%@ page import="java.util.Calendar"%>
+<%@ page import="java.util.Date"%>
+<%@ page import="com.axelor.i18n.I18n" %>
 <%
 String loginTitle = I18n.get("Please sign in");
 String loginRemember = I18n.get("Remember me");
@@ -45,14 +46,8 @@ if (pageContext.getServletContext().getResource(loginHeader) == null) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <meta name="google" content="notranslate">
     <link rel="shortcut icon" href="ico/favicon.ico">
-    <link href="lib/bootstrap/css/bootstrap.css" rel="stylesheet">
-    <link href="lib/bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
-    <link href="lib/font-awesome/css/font-awesome.css" rel="stylesheet">
-    <link href="css/view.form.checkbox.css" rel="stylesheet">
-    <link href="css/colors.css" rel="stylesheet">
-    <link href="css/login.css" rel="stylesheet">
-    <script type="text/javascript" src="lib/jquery.ui/js/jquery.js"></script>
-    <script type="text/javascript" src="lib/bootstrap/js/bootstrap.js"></script>
+    <x:style src="css/application.login.css" />
+    <x:script src="js/application.login.js" />
   </head>
   <body>
 
