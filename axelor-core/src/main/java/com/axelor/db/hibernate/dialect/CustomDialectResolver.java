8e7ff45120c1214c875ddb868748822c7e1ed111
admin <admin@axelor.com>
2017-02-17 09:30:20 +0100

    Log about unsupported database versions

--- axelor-core/src/main/java/com/axelor/db/hibernate/dialect/CustomDialectResolver.java
+++ axelor-core/src/main/java/com/axelor/db/hibernate/dialect/CustomDialectResolver.java
@@ -20,42 +20,44 @@ package com.axelor.db.hibernate.dialect;
 import org.hibernate.dialect.Dialect;
 import org.hibernate.engine.jdbc.dialect.internal.StandardDialectResolver;
 import org.hibernate.engine.jdbc.dialect.spi.DialectResolutionInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class CustomDialectResolver extends StandardDialectResolver {
 
 	private static final long serialVersionUID = 8211296180497513187L;
+	
+	private static final Logger log = LoggerFactory.getLogger(CustomDialectResolver.class);
 
 	public static final CustomDialectResolver INSTANCE = new CustomDialectResolver();
 
 	@Override
 	public Dialect resolveDialect(DialectResolutionInfo info) {
 		final String databaseName = info.getDatabaseName();
+		final int majorVersion = info.getDatabaseMajorVersion();
+		final int minorVersion = info.getDatabaseMinorVersion();
 		if ("HSQL Database Engine".equals(databaseName)) {
 			return new HSQLDialect();
 		}
 		if ("PostgreSQL".equals(databaseName)) {
-			final int majorVersion = info.getDatabaseMajorVersion();
-			final int minorVersion = info.getDatabaseMinorVersion();
 			if (majorVersion >= 9 && minorVersion >= 4) {
 				return new PostgreSQLDialect();
 			}
-			throw new RuntimeException("PostgreSQL 9.4 or later is required.");
+			log.error("PostgreSQL 9.4 or later is required.");
 		}
 		if ("Oracle".equals(databaseName)) {
-			final int majorVersion = info.getDatabaseMajorVersion();
 			if (majorVersion > 11) {
 				return new OracleDialect();
 			}
-			throw new RuntimeException("Oracle 12c or later is required.");
+			log.error("Oracle 12c or later is required.");
 		}
 		if ("MySQL".equals(databaseName)) {
-			final int majorVersion = info.getDatabaseMajorVersion();
-			final int minorVersion = info.getDatabaseMinorVersion();
 			if (majorVersion >= 5 && minorVersion >= 7) {
 				return new MySQLDialect();
 			}
-			throw new RuntimeException("MySQL 5.7 or later is required.");
+			log.error("MySQL 5.7 or later is required.");
 		}
-		return super.resolveDialect(info);
+		log.error("{} {}.{} is not suppported.", databaseName, majorVersion, minorVersion);
+		return null;
 	}
 }
