ad721c9fe13d6472e8a351257babd3f20b5eaba5
admin <admin@axelor.com>
2017-05-02 22:32:36 +0200

    Fix tests

--- axelor-web/src/test/java/com/axelor/web/TestModule.java
+++ axelor-web/src/test/java/com/axelor/web/TestModule.java
@@ -35,7 +35,12 @@ public class TestModule extends AbstractModule {
 
 		bind(ObjectMapper.class).toProvider(ObjectMapperProvider.class);
 
-		install(new JpaModule("testUnit", true, false).scan("com.axelor.web.db").properties(properties));
+		install(new JpaModule("testUnit", true, false)
+				.scan("com.axelor.auth.db")
+				.scan("com.axelor.meta.db")
+				.scan("com.axelor.dms.db")
+				.scan("com.axelor.web.db")
+				.properties(properties));
 		install(new AuthModule().properties(properties));
 		install(new AppModule());
 	}
