e7d4ebe5ce5277572e8622cba77d61940466b7b6
Pierre Belloy <p.belloy@axelor.com>
2017-05-12 17:22:43 +0200

    Fixed translation loading issue.

--- axelor-core/src/main/java/com/axelor/i18n/I18nBundle.java
+++ axelor-core/src/main/java/com/axelor/i18n/I18nBundle.java
@@ -79,7 +79,7 @@ public class I18nBundle extends ResourceBundle {
 		}
 
 		String lang = locale.getLanguage();
-		Query<MetaTranslation> query = Query.of(MetaTranslation.class).filter("self.language = ?1", lang).autoFlush(false);
+		Query<MetaTranslation> query = Query.of(MetaTranslation.class).filter("self.language = ?1", lang).order("id").autoFlush(false);
 		
 		if (query.count() == 0 && lang.length() > 2) {
 			query = Query.of(MetaTranslation.class).filter("self.language = ?1", lang.substring(0, 2));
