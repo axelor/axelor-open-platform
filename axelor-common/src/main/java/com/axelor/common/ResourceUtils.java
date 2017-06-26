b1ccc8cc9405ddff5a3503146a3240196fa51a32
admin <admin@axelor.com>
2017-06-26 09:45:23 +0200

    Add common resource utilities for reading properties files

--- axelor-common/src/main/java/com/axelor/common/ResourceUtils.java
+++ axelor-common/src/main/java/com/axelor/common/ResourceUtils.java
@@ -17,9 +17,13 @@
  */
 package com.axelor.common;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.util.Objects;
+import java.util.Properties;
 
 /**
  * The class provides static helper methods to work with resources.
@@ -57,5 +61,41 @@ public final class ResourceUtils {
 		} catch (IOException e) {
 			return null;
 		}
+	}
+
+	/**
+	 * Read {@link Properties} from the given file.
+	 *
+	 * @param file
+	 *            the properties file url
+	 * @return {@link Properties}
+	 * @throws IOException
+	 *             if unable to read file
+	 */
+	public static Properties getProperties(URL file) throws IOException {
+		Objects.requireNonNull(file, "file cannot be null");
+		final Properties properties = new Properties();
+		try (InputStream is = file.openStream()) {
+			properties.load(is);
+		}
+		return properties;
+	}
+
+	/**
+	 * Read {@link Properties} from the given file.
+	 *
+	 * @param file
+	 *            the properties file url
+	 * @return {@link Properties}
+	 * @throws IOException
+	 *             if unable to read file
+	 */
+	public static Properties getProperties(File file) throws IOException {
+		Objects.requireNonNull(file, "file cannot be null");
+		final Properties properties = new Properties();
+		try (InputStream is = new FileInputStream(file)) {
+			properties.load(is);
+		}
+		return properties;
 	}
 }
