5618740c26c0c5ab0e2fa65a6748b0d4644947dd
admin <admin@axelor.com>
2017-10-03 10:12:16 +0200

    Fix regression
    Fixes RM-8200, RM-8332

--- axelor-web/src/main/webapp/js/form/form.base.js
+++ axelor-web/src/main/webapp/js/form/form.base.js
@@ -541,7 +541,7 @@ var FormInput = {
 					scope.$apply(function() {
 						var val = scope.parse(value);
 						var txt = scope.format(value);
-						if (!scope.record) { // m2o editor with null value?
+						if (scope.$$isPanelEditor && !scope.record) { // m2o editor with null value?
 							scope.record = {};
 						}
 						model.$setViewValue(val);
@@ -569,12 +569,15 @@ var FormInput = {
 		if (element.is(':input')) {
 			setTimeout(bindListeners);
 			// clear input value
-			scope.$on('on:edit', function () {
-				if (model.$viewValue && !scope.record) {
-					model.$setViewValue(undefined);
-					scope.$render_editable();
-				}
-			});
+			if (scope.$$isPanelEditor) {
+				scope.$on('on:edit', function () {
+					if (model.$viewValue && !scope.record) {
+						scope.record = {};
+						model.$setViewValue(undefined);
+						scope.$render_editable();
+					}
+				});
+			}
 		}
 
 		scope.$render_editable();
