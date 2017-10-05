d3fb14c4946220aabd3dd2914d05a0a9a96eae06
admin <admin@axelor.com>
2017-10-05 17:58:19 +0200

    Fix regression
    Fixes RM-8404

--- axelor-web/src/main/webapp/js/form/form.relational.multiple.js
+++ axelor-web/src/main/webapp/js/form/form.relational.multiple.js
@@ -915,7 +915,7 @@ ui.formInput('InlineOneToMany', 'OneToMany', {
 					}
 					return !isEmpty(item);
 				});
-				if (changed) {
+				if (changed && values.length) {
 					model.$setViewValue(values);
 				}
 			}
