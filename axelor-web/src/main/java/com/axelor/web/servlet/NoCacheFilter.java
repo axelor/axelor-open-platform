2316f99bfc4c875548ef5cc542f54f25c21a8d82
Pierre Belloy <p.belloy@axelor.com>
2017-10-11 14:54:47 +0200

    Fix IE font loading issues
    We only support modern browsers, so load woff/woff2 fonts only.
    
    Fixes RM-8351

--- axelor-web/src/main/java/com/axelor/web/servlet/NoCacheFilter.java
+++ axelor-web/src/main/java/com/axelor/web/servlet/NoCacheFilter.java
@@ -85,9 +85,16 @@ public class NoCacheFilter implements Filter {
 
 		if (!production) {
 			res.setHeader("Expires", "Fri, 01 Jan 1990 00:00:00 GMT");
-	        res.setHeader("Last-Modified", new Date().toString());
-	        res.setHeader("Cache-Control", "no-store, no-cache, must-revalidate, max-age=0, post-check=0, pre-check=0");
-	        res.setHeader("Pragma", "no-cache");
+			res.setHeader("Last-Modified", new Date().toString());
+			if (uri.matches(".*\\.(eot|ttf|woff|woff2).*")) {
+				res.setHeader("Pragma", "no-cache");
+				res.setHeader("Cache-Control", "no-cache, must-revalidate, max-age=0, post-check=0, pre-check=0");
+			} else {
+				res.setHeader("Cache-Control", "no-store, no-cache, must-revalidate, max-age=0, post-check=0, pre-check=0");
+				res.setHeader("Pragma", "no-cache");
+			}
+		}
+
 		}
 
         chain.doFilter(request, response);
