12b435dc03f8f03285e8d8a31a99c546d12b2a55
admin <admin@axelor.com>
2017-11-14 10:59:01 +0100

    Clean up

--- axelor-core/src/main/java/com/axelor/data/csv/CSVImporter.java
+++ axelor-core/src/main/java/com/axelor/data/csv/CSVImporter.java
@@ -20,7 +20,6 @@ package com.axelor.data.csv;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
