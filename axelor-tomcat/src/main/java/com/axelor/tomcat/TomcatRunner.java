76ec20159ef5df9c072bea9827eec4932631ea67
admin <admin@axelor.com>
2017-12-30 23:57:29 +0100

    Add config file support to tomcat runner
    We can now provide a properties file with runner command
    values with camelCase keys corresponding to command line
    options.

--- axelor-tomcat/src/main/java/com/axelor/tomcat/TomcatRunner.java
+++ axelor-tomcat/src/main/java/com/axelor/tomcat/TomcatRunner.java
@@ -17,11 +17,17 @@
  */
 package com.axelor.tomcat;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.Properties;
 import java.util.stream.Collectors;
 
 import org.apache.commons.cli.CommandLine;
@@ -39,6 +45,7 @@ public class TomcatRunner {
 	private static final String OPTION_CONTEXT = "context-path";
 	private static final String OPTION_CLASSES = "extra-classes";
 	private static final String OPTION_LIBS = "extra-libs";
+	private static final String OPTION_CONFIG = "config";
 
 	private static Option addOption(Options options, String name, String argName, String desc) {
 		final Option option = Option.builder().longOpt(name).desc(desc).build();
@@ -56,6 +63,17 @@ public class TomcatRunner {
 		formatter.printHelp("tomcat-runner [options] <WEBAPP>", options);
 	}
 
+	private static List<String> getList(Properties props, String key) {
+		String value = props.getProperty(key, "");
+		if (value.trim().length() == 0) {
+			return Collections.emptyList();
+		}
+		return Arrays.stream(value.split(","))
+				.map(String::trim)
+				.filter(s -> s.length() > 0)
+				.collect(Collectors.toList());
+	}
+
 	public static void main(String[] args) {
 		final Options options = new Options();
 		final CommandLineParser parser = new DefaultParser();
@@ -67,6 +85,7 @@ public class TomcatRunner {
 		addOption(options, OPTION_BASE, "DIR", "The tomcat server base directory.");
 		addOption(options, OPTION_CLASSES, "DIR,...", "The list of extra classes dirs.");
 		addOption(options, OPTION_LIBS, "JAR,...", "The list of extra jar libs.");
+		addOption(options, OPTION_CONFIG, "FILE", "The config file.");
 
 		try {
 			cli = parser.parse(options, args);
@@ -75,37 +94,57 @@ public class TomcatRunner {
 			return;
 		}
 
-		final List<Path> webapps = cli.getArgList().stream().map(Paths::get).collect(Collectors.toList());
+		if (cli.hasOption(OPTION_HELP)) {
+			usage(options);
+			return;
+		}
 
-		if (cli.hasOption(OPTION_HELP) || webapps.isEmpty()) {
+		final Properties props = new Properties();
+		
+		if (cli.hasOption(OPTION_CONFIG)) {
+			File config = new File(cli.getOptionValue(OPTION_CONFIG));
+			try (FileInputStream is = new FileInputStream(config)) {
+				props.load(is);
+			} catch (IOException e) {
+				System.err.println("invalid config file: " + config);
+			}
+		}
+
+		final List<Path> webapps = new ArrayList<>();
+
+		cli.getArgList().stream().map(Paths::get).forEach(webapps::add);
+		getList(props, "webapps").stream().map(Paths::get).forEach(webapps::add);
+
+		if (webapps.isEmpty()) {
 			usage(options);
 			return;
 		}
 
 		final TomcatOptions settings = new TomcatOptions(webapps);
 
-		if (cli.hasOption(OPTION_CONTEXT)) {
-			settings.setContextPath(cli.getOptionValue(OPTION_CONTEXT));
-		}
+		settings.setContextPath(cli.getOptionValue(OPTION_CONTEXT, props.getProperty("contextPath", "/")));
 
-		if (cli.hasOption(OPTION_PORT)) {
-			try {
-				settings.setPort(Integer.parseInt(cli.getOptionValue(OPTION_PORT)));
-			} catch (NumberFormatException e) {
-				System.err.println("Invalid port.");
-				return;
-			}
+		try {
+			settings.setPort(Integer.parseInt(cli.getOptionValue(OPTION_PORT, props.getProperty("port", "8080"))));
+		} catch (NumberFormatException e) {
+			System.err.println("Invalid port.");
+			return;
 		}
-
+		
+		Path baseDir = null;
 		if (cli.hasOption(OPTION_BASE)) {
-			Path baseDir = Paths.get(cli.getOptionValue(OPTION_BASE));
+			baseDir = Paths.get(cli.getOptionValue(OPTION_BASE));
+		} else if (props.containsKey("baseDir")) {
+			baseDir = Paths.get(props.getProperty("baseDir"));
+		}
+		if (baseDir != null) {
 			if (Files.exists(baseDir) && Files.isRegularFile(baseDir)) {
 				System.err.println("invalid base directory.");
 				return;
 			}
 			settings.setBaseDir(baseDir);
 		}
-		
+
 		if (cli.hasOption(OPTION_CLASSES)) {
 			Arrays.stream(cli.getOptionValues(OPTION_CLASSES))
 				.flatMap(value -> Arrays.stream(value.split(",")))
@@ -113,7 +152,11 @@ public class TomcatRunner {
 				.map(Paths::get)
 				.forEach(settings::addClasses);
 		}
-		
+
+		getList(props, "extraClasses").stream()
+			.map(Paths::get)
+			.forEach(settings::addClasses);
+
 		if (cli.hasOption(OPTION_LIBS)) {
 			Arrays.stream(cli.getOptionValues(OPTION_LIBS))
 				.flatMap(value -> Arrays.stream(value.split(",")))
@@ -121,6 +164,10 @@ public class TomcatRunner {
 				.map(Paths::get)
 				.forEach(settings::addLib);
 		}
+
+		getList(props, "extraLibs").stream()
+			.map(Paths::get)
+			.forEach(settings::addLib);
 
 		final TomcatServer server = new TomcatServer(settings);
 		server.start();
