861489e1cbe456496ed29a1e3150b800a67f3c27
admin <admin@axelor.com>
2017-12-31 00:00:18 +0100

    Use tomcat runner config with run task
    Also the IDE launchers now uses config file.

--- axelor-gradle/src/main/java/com/axelor/gradle/tasks/TomcatRun.java
+++ axelor-gradle/src/main/java/com/axelor/gradle/tasks/TomcatRun.java
@@ -20,20 +20,16 @@ package com.axelor.gradle.tasks;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Function;
 
 import org.gradle.api.Project;
 import org.gradle.api.internal.tasks.options.Option;
-import org.gradle.api.plugins.WarPlugin;
 import org.gradle.api.tasks.JavaExec;
 import org.gradle.api.tasks.TaskAction;
-import org.gradle.api.tasks.bundling.War;
 import org.gradle.jvm.tasks.Jar;
 
 import com.axelor.common.FileUtils;
 import com.axelor.gradle.support.HotswapSupport;
 import com.axelor.gradle.support.TomcatSupport;
-import com.google.common.base.Joiner;
 
 public class TomcatRun extends JavaExec {
 
@@ -52,44 +48,15 @@ public class TomcatRun extends JavaExec {
 	}
 
 	public static List<String> getArgs(Project project, int port) {
-		final War war = (War) project.getTasks().getByName(WarPlugin.WAR_TASK_NAME);
-
 		final File baseDir = FileUtils.getFile(project.getBuildDir(), "tomcat");
-		final File warDir = FileUtils.getFile(baseDir, "webapps", "exploded");
-
-		final List<String> webapps = new ArrayList<>();
-		final List<String> classes = new ArrayList<>();
-		
-		final Function<File, String> toRelativePath = path -> TomcatSupport.toRelativePath(project, path);
-		
-		final File webapp = new File(project.getProjectDir(), "src/main/webapp");
-		if (webapp.exists()) {
-			webapps.add(toRelativePath.apply(webapp));
-		}
-
-		// try to use linked axelor-web's webapp dir
-		project.getGradle().getIncludedBuilds().stream()
-			.map(it -> new File(it.getProjectDir(), "axelor-web/src/main/webapp"))
-			.filter(it -> it.exists())
-			.findFirst().ifPresent(dir -> webapps.add(toRelativePath.apply(dir)));
-
-		// finally add exploded war
-		webapps.add(toRelativePath.apply(warDir));
-
-		// find IDE output folders
-		HotswapSupport.findOutputPaths(project).stream().map(toRelativePath).forEach(classes::add);
+		final File confFile = FileUtils.getFile(baseDir, TomcatSupport.TOMCAT_RUNNER_CONFIG);
 
 		final List<String> args = new ArrayList<>();
 
 		args.add("--port");
 		args.add("" + port);
-		args.add("--base-dir");
-		args.add(toRelativePath.apply(baseDir));
-		args.add("--context-path");
-		args.add(war.getBaseName());
-		args.add("--extra-classes");
-		args.add(Joiner.on(",").join(classes));
-		args.addAll(webapps);
+		args.add("--config");
+		args.add(TomcatSupport.toRelativePath(project, confFile));
 
 		return args;
 	}
