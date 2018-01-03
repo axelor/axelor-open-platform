b1115cd66ccf18c4541cdd512550a9a7a67dc4b7
admin <admin@axelor.com>
2018-01-03 14:05:17 +0100

    Cleanup

--- axelor-gradle/src/main/java/com/axelor/gradle/support/HotswapSupport.java
+++ axelor-gradle/src/main/java/com/axelor/gradle/support/HotswapSupport.java
@@ -28,7 +28,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
 import java.util.Properties;
 import java.util.function.Function;
 import java.util.stream.Collectors;
