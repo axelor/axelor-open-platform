ac8b499ecc31d3593e0a1681d7c49d67c4e1090f
admin <admin@axelor.com>
2018-05-16 16:06:44 +0200

    Fix search text validation on m2o
    When m2o uses numeric field as name field, search text
    validation fails.
    
    Fixes RM-12108, RM-11775

--- axelor-web/src/main/webapp/js/form/form.relational.single.js
+++ axelor-web/src/main/webapp/js/form/form.relational.single.js
@@ -395,8 +395,8 @@ ui.formInput('ManyToOne', 'Select', {
 
 		var validateSearch = _.debounce(function () {
 			if (input.length === 0) return;
-			var my = input.val() || '';
-			var text = scope.text || '';
+			var my = String(input.val() || '');
+			var text = String(scope.text || '');
 			var invalid = my !== text;
 			scope.$applyAsync(function () {
 				scope.setValidity('search', !invalid);
