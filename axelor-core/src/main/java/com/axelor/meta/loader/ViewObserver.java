a5634957f109796dc9088c5d430f544a1632ca1e
Phi-Long Do <p.do@axelor.com>
2019-01-08 17:22:18 +0100

    Regenerate computed views when an extension view is removed

--- axelor-core/src/main/java/com/axelor/meta/loader/ViewObserver.java
+++ axelor-core/src/main/java/com/axelor/meta/loader/ViewObserver.java
@@ -17,10 +17,12 @@
  */
 package com.axelor.meta.loader;
 
+import com.axelor.db.JPA;
 import com.axelor.event.Observes;
 import com.axelor.events.FeatureChanged;
 import com.axelor.events.ModuleChanged;
 import com.axelor.events.PostRequest;
+import com.axelor.events.PreRequest;
 import com.axelor.events.RequestEvent;
 import com.axelor.events.qualifiers.EntityType;
 import com.axelor.meta.db.MetaView;
@@ -28,6 +30,7 @@ import com.axelor.meta.db.repo.MetaViewRepository;
 import com.axelor.rpc.RequestUtils;
 import com.google.inject.Singleton;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 import javax.inject.Inject;
@@ -67,5 +70,24 @@ public class ViewObserver {
                 .ifPresent(finalViewGenerator::generate));
   }
 
+  void onPreRemove(
+      @Observes @Named(RequestEvent.REMOVE) @EntityType(MetaView.class) PreRequest event) {
+    RequestUtils.processRequest(
+        event.getRequest(),
+        values -> {
+          final MetaView view = JPA.edit(MetaView.class, values);
+          final String name = view.getName();
+          toRegenerate.add(name);
+        });
+  }
+
+  void onPostRemove(
+      @Observes @Named(RequestEvent.REMOVE) @EntityType(MetaView.class) PostRequest event) {
+    toRegenerate
+        .stream()
+        .map(name -> metaViewRepo.findByNameAndComputed(name, false))
+        .filter(Objects::nonNull)
+        .forEach(finalViewGenerator::generate);
+    toRegenerate.clear();
   }
 }
