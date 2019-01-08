d04f1a3ea0a1a1084dab7413ead2aa7e6d687bb7
Phi-Long Do <p.do@axelor.com>
2019-01-08 23:06:08 +0100

    Update computed views in case of removable modules

--- axelor-core/src/main/java/com/axelor/meta/loader/XMLViews.java
+++ axelor-core/src/main/java/com/axelor/meta/loader/XMLViews.java
@@ -659,6 +659,14 @@ public class XMLViews {
         Document document, Node extensionNode, MetaView view, MetaView extensionView)
         throws XPathExpressionException {
 
+      Optional.ofNullable(ModuleManager.getModule(extensionView.getModule()))
+          .map(Module::isRemovable)
+          .filter(removable -> removable)
+          .ifPresent(
+              removable -> {
+                addDependentModule(view, extensionView.getModule());
+              });
+
       final NamedNodeMap extendAttributes = extensionNode.getAttributes();
       final String feature = getNodeAttributeValue(extendAttributes, "if-feature");
 
