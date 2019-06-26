axelor/axelor-open-platform

commit 4c636ce72605e6e38b9211d80281db9024b01ed4

Fix tests

axelor-core/src/main/java/com/axelor/dms/db/repo/DMSFileRepository.java
@@ -56,6 +56,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import javax.annotation.Nullable;
 import javax.inject.Inject;
 import javax.persistence.PersistenceException;
@@ -190,7 +191,20 @@ public class DMSFileRepository extends JpaRepository<DMSFile> {
       entity.setParent(dmsHome);
     }
 
+    copyParentPermissions(entity);
+
     return super.save(entity);
+  }
+
+  private void copyParentPermissions(DMSFile entity) {
+    Optional.ofNullable(entity.getParent())
+        .map(DMSFile::getPermissions)
+        .ifPresent(
+            permissions ->
+                permissions
+                    .stream()
+                    .map(permission -> dmsPermissions.copy(permission, false))
+                    .forEach(entity::addPermission));
   }
 
   /**
@@ -228,7 +242,7 @@ public class DMSFileRepository extends JpaRepository<DMSFile> {
       dmsRoot.setRelatedModel(related.getClass().getName());
       dmsRoot.setIsDirectory(true);
       dmsRoot.setParent(dmsRootParent);
-      dmsRoot = super.save(dmsRoot); // Should get id before its child.
+      dmsRoot = save(dmsRoot); // Should get id before its child.
     }
 
     DMSFile dmsHome = findHomeByRelated(related);
@@ -253,7 +267,7 @@ public class DMSFileRepository extends JpaRepository<DMSFile> {
       dmsHome.setRelatedModel(related.getClass().getName());
       dmsHome.setParent(dmsRoot);
       dmsHome.setIsDirectory(true);
-      dmsHome = super.save(dmsHome); // Should get id before its child.
+      dmsHome = save(dmsHome); // Should get id before its child.
     }
 
     return dmsHome;

axelor-core/src/main/java/com/axelor/dms/db/repo/DMSFileListener.java
@@ -1,44 +1,0 @@
-/*
- * Axelor Business Solutions
- *
- * Copyright (C) 2005-2019 Axelor (<http://axelor.com>).
- *
- * This program is free software: you can redistribute it and/or  modify
- * it under the terms of the GNU Affero General Public License, version 3,
- * as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-package com.axelor.dms.db.repo;
-
-import com.axelor.dms.db.DMSFile;
-import com.axelor.inject.Beans;
-import java.util.Optional;
-import javax.persistence.PostPersist;
-
-public class DMSFileListener {
-
-  private final DMSPermissionRepository dmsPermissionRepo;
-
-  public DMSFileListener() {
-    dmsPermissionRepo = Beans.get(DMSPermissionRepository.class);
-  }
-
-  @PostPersist
-  public void copyParentPermissions(DMSFile dmsFile) {
-    Optional.ofNullable(dmsFile.getParent())
-        .map(DMSFile::getPermissions)
-        .ifPresent(
-            permissions ->
-                permissions
-                    .stream()
-                    .map(permission -> dmsPermissionRepo.copy(permission, false))
-                    .forEach(dmsFile::addPermission));
-  }
-}

axelor-core/src/main/java/com/axelor/meta/MetaPermissions.java
@@ -58,6 +58,10 @@ public class MetaPermissions {
   }
 
   private MetaPermission find(User user, String object, String field) {
+    if (user == null) {
+      return null;
+    }
+
     MetaPermission permission = find(user.getMetaPermissions(), object, field);
     if (permission == null && user.getGroup() != null) {
       permission = find(user.getGroup().getMetaPermissions(), object, field);

axelor-core/src/main/resources/domains/DMS.xml
@@ -26,8 +26,6 @@
     <!-- content -->
     <string name="content" large="true" />
     <string name="contentType" />
-
-    <entity-listener class="com.axelor.dms.db.repo.DMSFileListener"/>
   </entity>
 
   <entity name="DMSFileTag" table="DMS_FILE_TAG" repository="none">

axelor-core/src/test/java/com/axelor/auth/AuthTest.java
@@ -18,6 +18,7 @@
 package com.axelor.auth;
 
 import com.axelor.JpaTest;
+import com.axelor.auth.AuthFilter.UsernamePasswordTokenWithParams;
 import com.axelor.auth.db.Group;
 import com.axelor.auth.db.Permission;
 import com.axelor.auth.db.Role;
@@ -51,7 +52,8 @@ public class AuthTest extends JpaTest {
 
   private void login(String user, String password) {
     final Subject subject = AuthUtils.getSubject();
-    final UsernamePasswordToken token = new UsernamePasswordToken(user, password);
+    final UsernamePasswordToken token =
+        new UsernamePasswordTokenWithParams(user, password, false, null, null);
 
     subject.login(token);
   }
@@ -155,7 +157,7 @@ public class AuthTest extends JpaTest {
   }
 
   @Test
-  public void testNoramlUser() {
+  public void testNormalUser() {
     login("demo", "demo");
     normalUserTest();
   }

commit eb501dc811e83c553f1eeeec5388e5e35f0e9436

Fix DMS permissions being copied again after update
Regression caused by 4c636ce72605e6e38b9211d80281db9024b01ed4

Addresses RM-12663

axelor-core/src/main/java/com/axelor/dms/db/repo/DMSFileRepository.java
@@ -191,7 +191,9 @@ public class DMSFileRepository extends JpaRepository<DMSFile> {
       entity.setParent(dmsHome);
     }
 
-    copyParentPermissions(entity);
+    if (entity.getVersion() == null || entity.getVersion() == 0) {
+      copyParentPermissions(entity);
+    }
 
     return super.save(entity);
   }