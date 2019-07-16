axelor/axelor-open-platform

commit 47deed88c8ec32d678d6c47c4e8d581a45c94004

Properly handle expression errors
Only return null for missing property errors. All other errors
should be propagated to the caller.

These are breaking changes. Please fix your eval expressions!

Closes RM-18679

axelor-core/src/main/java/com/axelor/rpc/ContextHandler.java
@@ -231,20 +231,20 @@ public class ContextHandler<T> {
   }
 
   public Object interceptJsonAccess(Method method, Object[] args) throws Exception {
-    switch (method.getName()) {
-      case "get":
-        if ("class".equals(args[0])) {
-          return proxy.getClass();
-        }
-      case "put":
-        final String name = (String) args[0];
-        final Method found =
-            args.length == 2 ? beanMapper.getSetter(name) : beanMapper.getGetter(name);
-        if (found == null) {
-          return method.invoke(getJsonContext(), args);
-        }
-        final Object[] params = args.length == 2 ? new Object[] {args[1]} : new Object[] {};
-        return found.invoke(proxy, params);
+    final String name = (String) args[0];
+    if ("class".equals(name)) return method.invoke(proxy, args);
+    if ("get".equals(method.getName()) || "set".equals(method.getName())) {
+      Method accessor = args.length == 1 ? beanMapper.getGetter(name) : beanMapper.getSetter(name);
+      Object[] params = args.length == 1 ? new Object[] {} : new Object[] {args[1]};
+      if (accessor != null) {
+        return accessor.invoke(proxy, params);
+      }
+      JsonContext ctx = getJsonContext();
+      if (args.length == 1) {
+        if (ctx.containsKey(name) || ctx.hasField(name)) return ctx.get(name);
+        throw new NoSuchFieldException(name);
+      }
+      return ctx.put(name, args[1]);
     }
     throw new UnsupportedOperationException("cannot call '" + method + "' on proxy object");
   }

axelor-core/src/main/java/com/axelor/rpc/JsonContext.java
@@ -133,6 +133,10 @@ public class JsonContext extends SimpleBindings {
     return context == null ? null : (Long) context.get(Context.KEY_ID);
   }
 
+  public boolean hasField(String name) {
+    return fields.containsKey(name);
+  }
+
   @Override
   @SuppressWarnings({"unchecked", "rawtypes"})
   public Object get(Object key) {

axelor-core/src/main/java/com/axelor/script/AbstractScriptHelper.java
@@ -82,9 +82,16 @@ public abstract class AbstractScriptHelper implements ScriptHelper {
   public Object eval(String expr) {
     try {
       return eval(expr, getBindings());
-    } catch (Exception e) {
-      log.error("Script error: {}", expr, e);
+    } catch (NoSuchFieldException e) {
+      log.warn("No such field in: {} -- ({})", expr, e.getMessage());
       return null;
+    } catch (Exception e) {
+      if (e.getCause() instanceof NoSuchFieldException) {
+        log.warn("No such field in: {} -- ({})", expr, e.getMessage());
+        return null;
+      }
+      log.error("Script error: {}", expr, e);
+      throw new IllegalArgumentException(e);
     }
   }
 }

axelor-core/src/main/java/com/axelor/script/GroovyScriptHelper.java
@@ -130,8 +130,8 @@ public class GroovyScriptHelper extends AbstractScriptHelper {
             try {
               return super.getVariable(name);
             } catch (MissingPropertyException e) {
+              throw new IllegalArgumentException(new NoSuchFieldException(name));
             }
-            return null;
           }
         });
     return script.run();

commit cba98d6f18f0d31023a3414a374b79a7416e7e2d

Fix regression caused in commit 47deed88c
Fixes RM-19370

axelor-core/src/main/java/com/axelor/rpc/ContextHandler.java
@@ -237,7 +237,7 @@ public class ContextHandler<T> {
   public Object interceptJsonAccess(Method method, Object[] args) throws Exception {
     final String name = (String) args[0];
     if ("class".equals(name)) return method.invoke(proxy, args);
-    if ("get".equals(method.getName()) || "set".equals(method.getName())) {
+    if ("get".equals(method.getName()) || "put".equals(method.getName())) {
       Method accessor = args.length == 1 ? beanMapper.getGetter(name) : beanMapper.getSetter(name);
       Object[] params = args.length == 1 ? new Object[] {} : new Object[] {args[1]};
       if (accessor != null) {