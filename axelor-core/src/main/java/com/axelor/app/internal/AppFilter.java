de4501f85e71d4e495115e873d6180518f15c891
kaoe <kaoe@sistemasprofissionais.com.br>
2019-07-20 11:20:22 -0300

    Language support with country variation like pt-BR or pt_BR

--- axelor-core/src/main/java/com/axelor/app/internal/AppFilter.java
+++ axelor-core/src/main/java/com/axelor/app/internal/AppFilter.java
@@ -22,6 +22,7 @@ import com.axelor.auth.AuthUtils;
 import com.axelor.auth.db.User;
 import com.axelor.common.StringUtils;
 import com.google.inject.Singleton;
+
 import java.io.IOException;
 import java.util.Locale;
 import javax.servlet.Filter;
@@ -35,71 +36,75 @@ import javax.servlet.http.HttpServletRequest;
 @Singleton
 public class AppFilter implements Filter {
 
-  private static final ThreadLocal<String> BASE_URL = new ThreadLocal<>();
-  private static final ThreadLocal<Locale> LANGUAGE = new ThreadLocal<>();
+    private static final ThreadLocal<String> BASE_URL = new ThreadLocal<>();
+    private static final ThreadLocal<Locale> LANGUAGE = new ThreadLocal<>();
 
-  private static Locale APP_LOCALE;
+    private static Locale APP_LOCALE;
 
-  @Override
-  public void init(FilterConfig filterConfig) throws ServletException {
-    try {
-      final String appLocale = AppSettings.get().get("application.locale", null);
-      APP_LOCALE = appLocale == null ? null : new Locale(appLocale);
-    } catch (Exception e) {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        try {
+            final String appLocale = AppSettings.get().get("application.locale", null);
+            APP_LOCALE = appLocale == null ? null : AppFilter.getLocaleByTag(appLocale);
+        } catch (Exception e) {
+        }
     }
-  }
 
-  public static String getBaseURL() {
-    return BASE_URL.get();
-  }
-
-  public static Locale getLocale() {
-    User user = AuthUtils.getUser();
-    if (user != null && user.getLanguage() != null) {
-      return new Locale(user.getLanguage());
+    public static String getBaseURL() {
+        return BASE_URL.get();
     }
-    if (user != null && APP_LOCALE != null) {
-      return APP_LOCALE;
+
+    private static Locale getLocaleByTag(String tag) {
+        return Locale.forLanguageTag(tag.replaceAll("\\_", "-"));
     }
-    if (LANGUAGE.get() == null) {
-      return Locale.getDefault();
+
+    public static Locale getLocale() {
+        User user = AuthUtils.getUser();
+        if (user != null && user.getLanguage() != null) {
+            return getLocaleByTag(user.getLanguage());
+        }
+        if (user != null && APP_LOCALE != null) {
+            return APP_LOCALE;
+        }
+        if (LANGUAGE.get() == null) {
+            return Locale.getDefault();
+        }
+        return LANGUAGE.get();
     }
-    return LANGUAGE.get();
-  }
 
-  private String getHeader(ServletRequest req, String name, String defaultValue) {
-    final String value = ((HttpServletRequest) req).getHeader(name);
-    return StringUtils.isBlank(value) ? defaultValue : value;
-  }
-
-  private String getBaseUrl(ServletRequest req) {
-    final String proto = getHeader(req, "X-Forwarded-Proto", req.getScheme());
-    final String port = getHeader(req, "X-Forwarded-Port", "" + req.getServerPort());
-    final String host = getHeader(req, "X-Forwarded-Host", req.getServerName());
-    final String context =
-        getHeader(req, "X-Forwarded-Context", req.getServletContext().getContextPath());
-    return port.equals("80") || port.equals("443")
-        ? proto + "://" + host + context
-        : proto + "://" + host + ":" + port + context;
-  }
-
-  @Override
-  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-      throws IOException, ServletException {
-
-    BASE_URL.set(getBaseUrl(request));
-    LANGUAGE.set(request.getLocale());
-    try {
-      chain.doFilter(request, response);
-    } finally {
-      LANGUAGE.remove();
-      BASE_URL.remove();
+    private String getHeader(ServletRequest req, String name, String defaultValue) {
+        final String value = ((HttpServletRequest) req).getHeader(name);
+        return StringUtils.isBlank(value) ? defaultValue : value;
     }
-  }
 
-  @Override
-  public void destroy() {
-    LANGUAGE.remove();
-    BASE_URL.remove();
-  }
+    private String getBaseUrl(ServletRequest req) {
+        final String proto = getHeader(req, "X-Forwarded-Proto", req.getScheme());
+        final String port = getHeader(req, "X-Forwarded-Port", "" + req.getServerPort());
+        final String host = getHeader(req, "X-Forwarded-Host", req.getServerName());
+        final String context =
+                getHeader(req, "X-Forwarded-Context", req.getServletContext().getContextPath());
+        return port.equals("80") || port.equals("443")
+                ? proto + "://" + host + context
+                : proto + "://" + host + ":" + port + context;
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+
+        BASE_URL.set(getBaseUrl(request));
+        LANGUAGE.set(request.getLocale());
+        try {
+            chain.doFilter(request, response);
+        } finally {
+            LANGUAGE.remove();
+            BASE_URL.remove();
+        }
+    }
+
+    @Override
+    public void destroy() {
+        LANGUAGE.remove();
+        BASE_URL.remove();
+    }
 }
