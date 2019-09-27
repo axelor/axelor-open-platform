c7e4f9811f6bd46aee1390521e879328257ee727
Pierre Belloy <p.belloy@axelor.com>
2019-09-27 12:32:13 +0200

    Silence requests should not hide loading indicator.
    Fixed RM-21075
    
    Signed-off-by: Pierre Belloy <p.belloy@axelor.com>

--- axelor-web/src/main/webapp/js/axelor.app.js
+++ axelor-web/src/main/webapp/js/axelor.app.js
@@ -348,7 +348,9 @@
 
     return {
       response: function(response) {
-        onHttpStop();
+        if (!response.config || !response.config.silent) {
+          onHttpStop();
+        }
         if (response.data && response.data.status === -1) {
           if (!response.config.silent) $rootScope.$broadcast('event:http-error', response.data);
           return $q.reject(response);
