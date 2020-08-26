f3f19f874cb4785c2062f85634a4e8fa67d71629
admin <admin@axelor.com>
2020-08-26 09:40:49 +0200

    Fix boolean field focus issue in editable grid
    Fixes RM-30712

--- axelor-web/src/main/webapp/js/widget/widget.slickgrid.js
+++ axelor-web/src/main/webapp/js/widget/widget.slickgrid.js
@@ -1209,6 +1209,9 @@ Grid.prototype.zIndexFix = function() {
   var zIndex = this.element.parents('.ui-dialog:first').zIndex();
   if (zIndex) {
     this.element.find('.slick-headerrow-column').zIndex(zIndex);
+    if (this.editorForm.is(':visible')) {
+      this.editorForm.find('.boolean-item input').zIndex(zIndex);
+    }
   }
 };
 
@@ -1966,6 +1969,7 @@ Grid.prototype.adjustEditor = function () {
   });
 
   form.css('padding-left', leftPadding);
+  this.zIndexFix();
 }
 
 Grid.prototype.showEditor = function (activeCell) {
