1fe3a0f656b5482e4528c2aaa50417ff5c0ce71e
Phi-Long Do <p.do@axelor.com>
2020-09-04 11:41:16 +0200

    Ignore hiding of fields in editable grid
    Fixes RM-26423

--- axelor-web/src/main/webapp/js/form/form.base.js
+++ axelor-web/src/main/webapp/js/form/form.base.js
@@ -235,12 +235,15 @@ ui.formCompile = function(element, attrs, linkerFn) {
 
     var hideFn = _.contains(this.handles, 'isHidden') ? angular.noop : hideWidget;
 
-    var hiddenSet = false;
-    scope.$watch("isHidden()", function isHiddenWatch(hidden, old) {
-      if (hiddenSet && hidden === old) return;
-      hiddenSet = true;
-      return hideFn(hidden);
-    });
+    // Ignore hiding of fields in editable grid
+    if (!scope.view || scope.view.type !== 'grid') {
+      var hiddenSet = false;
+      scope.$watch("isHidden()", function isHiddenWatch(hidden, old) {
+        if (hiddenSet && hidden === old) return;
+        hiddenSet = true;
+        return hideFn(hidden);
+      });
+    }
 
     var readonlySet = false;
     scope.$watch("isReadonly()", function isReadonlyWatch(readonly, old) {
