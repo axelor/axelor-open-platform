29c0a538a239d99a067e8fcb1afcaf0a4060741e
Phi-Long Do <p.do@axelor.com>
2021-01-14 18:31:57 +0100

    Set "SameSite=None" attribute to session cookie to allow CORS requests
    Setting "SameSite=None" is allowed only over a secure connection.
    It is set on session cookie after successful login.
    
    Fixes RM-34203
    
    (cherry picked from commit 2af9c2de68654ff40858966002c6fc0cf7c948da)

--- axelor-core/src/main/java/com/axelor/auth/pac4j/AuthPac4jListener.java
+++ axelor-core/src/main/java/com/axelor/auth/pac4j/AuthPac4jListener.java
@@ -17,13 +17,17 @@
  */
 package com.axelor.auth.pac4j;
 
+import com.axelor.auth.AuthFilter;
 import com.axelor.auth.UserAuthenticationInfo;
 import com.axelor.auth.db.User;
 import com.axelor.event.Event;
 import com.axelor.event.NamedLiteral;
 import com.axelor.events.PostLogin;
+import com.axelor.inject.Beans;
 import java.lang.invoke.MethodHandles;
 import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationInfo;
@@ -47,6 +51,11 @@ public class AuthPac4jListener implements AuthenticationListener {
       final User user = ((UserAuthenticationInfo) info).getUser();
 
       if (user != null) {
+        final HttpServletRequest request = Beans.get(HttpServletRequest.class);
+        request.changeSessionId();
+        if (request.isSecure()) {
+          AuthFilter.setSessionSameSiteNone(Beans.get(HttpServletResponse.class));
+        }
         firePostLoginSuccess(token, user);
         return;
       }
