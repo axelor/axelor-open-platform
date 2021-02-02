ee80784ffdc2e39cfc3cfad6460acdb6a2aede70
Phi-Long Do <p.do@axelor.com>
2021-02-02 19:59:03 +0100

    Set "SameSite=None" attribute to pre-authenticated session cookie
    Fixes RM-34203

--- axelor-core/src/main/java/com/axelor/auth/AuthFilter.java
+++ axelor-core/src/main/java/com/axelor/auth/AuthFilter.java
@@ -66,6 +66,8 @@ public class AuthFilter extends FormAuthenticationFilter {
   public void doFilterInternal(ServletRequest request, ServletResponse response, FilterChain chain)
       throws ServletException, IOException {
 
+    setSessionSameSiteNone((HttpServletRequest) request, (HttpServletResponse) response);
+
     // tomcat 7.0.67 doesn't redirect with / if root request is sent without slash
     // see RM-4500 for more details
     if (!SecurityUtils.getSubject().isAuthenticated() && isRootWithoutSlash(request)) {
@@ -115,14 +117,26 @@ public class AuthFilter extends FormAuthenticationFilter {
       AuthenticationToken token, Subject subject, ServletRequest request, ServletResponse response)
       throws Exception {
     // change session id to prevent session fixation
-    ((HttpServletRequest) request).changeSessionId();
-    if (request.isSecure()) {
-      setSessionSameSiteNone((HttpServletResponse) response);
-    }
+    changeSessionId((HttpServletRequest) request, (HttpServletResponse) response);
     return super.onLoginSuccess(token, subject, request, response);
   }
 
-  public static void setSessionSameSiteNone(HttpServletResponse response) {
+  private static void changeSessionId(HttpServletRequest request, HttpServletResponse response) {
+    request.changeSessionId();
+    setSessionSameSiteNone(request, response);
+  }
+
+  public static void setSessionSameSiteNone(
+      HttpServletRequest request, HttpServletResponse response) {
+    if (request.isSecure()) {
+      setSessionSameSiteNone(response);
+    }
+  }
+
+  private static void setSessionSameSiteNone(HttpServletResponse response) {
+    final Subject subject = SecurityUtils.getSubject();
+    subject.getSession();
+
     final Collection<String> headers = response.getHeaders(HttpHeaders.SET_COOKIE);
     final Iterator<String> it = headers.iterator();
     if (it.hasNext()) {
