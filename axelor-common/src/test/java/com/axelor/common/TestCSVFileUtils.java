18db9a708c04c759984c7e0d9b702bec67807442
Pierre Belloy <p.belloy@axelor.com>
2021-12-10 18:50:47 +0100

    Format code
    Signed-off-by: Pierre Belloy <p.belloy@axelor.com>

--- axelor-common/src/test/java/com/axelor/common/TestCSVFileUtils.java
+++ axelor-common/src/test/java/com/axelor/common/TestCSVFileUtils.java
@@ -1,16 +1,32 @@
+/*
+ * Axelor Business Solutions
+ *
+ * Copyright (C) 2005-2021 Axelor (<http://axelor.com>).
+ *
+ * This program is free software: you can redistribute it and/or  modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.axelor.common;
 
 import com.axelor.common.csv.CSVFile;
-import org.apache.commons.csv.CSVParser;
-import org.apache.commons.csv.CSVRecord;
-import org.junit.Assert;
-import org.junit.Test;
-
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class TestCSVFileUtils {
 
