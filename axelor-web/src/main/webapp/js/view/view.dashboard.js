d6413a6fe60e4f1031049fe338f12ebfb88a6d75
Pierre Belloy <p.belloy@axelor.com>
2022-03-10 09:18:00 +0100

    Takes into account view.customization global config
    Addresses RM-47180

--- axelor-web/src/main/webapp/js/view/view.dashboard.js
+++ axelor-web/src/main/webapp/js/view/view.dashboard.js
@@ -116,7 +116,7 @@ ui.directive('uiViewDashboard', ['ViewService', function(ViewService) {
           schema.items = items;
           return ViewService.save(schema);
         },
-        disabled: !axelor.config['user.viewCustomizationPermission']
+        disabled: !axelor.config['user.viewCustomizationPermission'] || axelor.config["view.customization"] === false
       };
 
       var unwatch = scope.$watch("schema", function dashboardSchemaWatch(schema) {
