dde3f2546643760f7a853ff25d145c93ebcefad3
Pierre Belloy <p.belloy@axelor.com>
2022-03-24 17:56:11 +0100

    Use Gradle Node Plugin to run npm tasks
    No more need to install Node.js locally to build web resource bundles,
    aka `npm-build` task.
    
    To use it, you need to Declare Node.js repository in `settings.gradle`,
    section `dependencyResolutionManagement` :
    
    See https://github.com/node-gradle/gradle-node-plugin/blob/3.1.1/docs/faq.md#is-this-plugin-compatible-with-centralized-repositories-declaration

--- axelor-gradle/src/main/java/com/axelor/gradle/support/ScriptsSupport.java
+++ axelor-gradle/src/main/java/com/axelor/gradle/support/ScriptsSupport.java
@@ -19,43 +19,69 @@ package com.axelor.gradle.support;
 
 import com.axelor.gradle.AxelorPlugin;
 import com.axelor.gradle.tasks.DatabaseTask;
+import com.github.gradle.node.NodeExtension;
+import com.github.gradle.node.NodePlugin;
+import com.github.gradle.node.npm.task.NpmInstallTask;
+import com.github.gradle.node.npm.task.NpmTask;
+import java.io.File;
+import java.util.List;
 import org.gradle.api.Project;
-import org.gradle.api.tasks.Exec;
+import org.gradle.api.plugins.WarPlugin;
 
 public class ScriptsSupport extends AbstractSupport {
 
   public static final String DATABASE_CONFIGURATION = "database";
+
+  public static final String NODE_VERSION = "16.14.2";
+
+  public static final String NPM_INSTALL_TASK_NAME = "npm-install";
+  public static final String NPM_BUILD_TASK_NAME = "npm-build";
 
   @Override
   public void apply(Project project) {
     project.getConfigurations().create(DATABASE_CONFIGURATION);
     applyConfigurationLibs(project, DATABASE_CONFIGURATION, DATABASE_CONFIGURATION);
 
-    project
-        .getTasks()
-        .create(
-            "npm-install",
-            Exec.class,
-            task -> {
-              task.setDescription("Run 'npm install' command to install npm packages.");
-              task.setGroup(AxelorPlugin.AXELOR_BUILD_GROUP);
-              task.setWorkingDir(project.getBuildDir() + "/webapp");
-              task.setCommandLine("npm", "install");
-              task.dependsOn("copyWebapp");
-            });
+    project.getPlugins().apply(NodePlugin.class);
+
+    final NodeExtension nodeExtension = NodeExtension.get(project);
+    final File webappDir = new File(project.getBuildDir(), "webapp");
+    nodeExtension.getNodeProjectDir().set(webappDir);
+    nodeExtension.getVersion().set(NODE_VERSION);
+    nodeExtension.getDistBaseUrl().set((String) null);
+    nodeExtension.getDownload().set(true);
 
     project
         .getTasks()
         .create(
-            "npm-build",
-            Exec.class,
+            NPM_INSTALL_TASK_NAME,
+            NpmInstallTask.class,
             task -> {
-              task.setDescription("Run 'npm run build' command to build web resource bundles.");
+              task.setDescription("Run 'npm install' command to install npm packages.");
               task.setGroup(AxelorPlugin.AXELOR_BUILD_GROUP);
-              task.setWorkingDir(project.getBuildDir() + "/webapp");
-              task.setCommandLine("npm", "run", "build");
-              task.dependsOn("npm-install");
+              task.dependsOn(WarSupport.COPY_WEBAPP_TASK_NAME);
             });
+
+    NpmTask npmTask =
+        project
+            .getTasks()
+            .create(
+                NPM_BUILD_TASK_NAME,
+                NpmTask.class,
+                task -> {
+                  task.setDescription("Run 'npm run build' command to build web resource bundles.");
+                  task.setGroup(AxelorPlugin.AXELOR_BUILD_GROUP);
+                  task.getArgs().set(List.of("run", "build"));
+                  task.dependsOn(NPM_INSTALL_TASK_NAME);
+                  task.getInputs()
+                      .files(project.fileTree(webappDir, tree -> tree.exclude("dist/**")));
+                  task.getOutputs().dir(new File(webappDir, "dist"));
+                });
+
+    project.afterEvaluate(
+        p -> {
+          project.getTasks().getByName(WarPlugin.WAR_TASK_NAME).mustRunAfter(npmTask);
+        });
 
     project
         .getTasks()
