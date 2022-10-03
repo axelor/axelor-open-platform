976382694cb5c9f9876ffe3e22ad7a2235b8e66b
Pierre Belloy <p.belloy@axelor.com>
2022-10-03 16:45:50 +0200

    Fix grouped grid display when already initialized
    Fixes RM-42601
    
    Signed-off-by: Pierre Belloy <p.belloy@axelor.com>

--- axelor-web/src/main/webapp/js/widget/widget.slickgrid.js
+++ axelor-web/src/main/webapp/js/widget/widget.slickgrid.js
@@ -1292,7 +1292,11 @@ Grid.prototype._doInit = function(view) {
       });
     }
   }
+  function detachGrid() {
+    _containerH = _containerW = 0;
+  }
   scope.$on('dom:attach', resetScroll);
+  scope.$on('dom:detach', detachGrid);
   scope.$on('tab:select', resetScroll);
 
   function setWidthChanged() {
