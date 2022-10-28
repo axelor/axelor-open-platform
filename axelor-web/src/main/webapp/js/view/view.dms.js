4fb4917d56f1f271873e1c0ec9b50d3fec55d220
Pierre Belloy <p.belloy@axelor.com>
2022-10-28 11:35:26 +0200

    Fix wrong number of attachments displayed on record toolbar icon
    Use the total number of record return by the search query instead of
    the size of the records.
    
    Fixes RM-55804
    
    Signed-off-by: Pierre Belloy <p.belloy@axelor.com>

--- axelor-web/src/main/webapp/js/view/view.dms.js
+++ axelor-web/src/main/webapp/js/view/view.dms.js
@@ -1665,8 +1665,8 @@ ui.directive("uiDmsPopup", ['$compile', function ($compile) {
           context: context
         });
 
-        promise.success(function (records) {
-          record.$attachments = _.size(records);
+        promise.success(function (records, page) {
+          record.$attachments = page.total;
           forScope.$broadcast('on:load-messages', record);
         });
 
