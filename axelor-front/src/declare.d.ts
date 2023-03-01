7a8e1c726e274b6bc41ee3f7c8ff2a0d8a7db59e
Amit Mendapara <amit@axelor.com>
2023-03-01 12:17:34 +0100

    Add declare.d.ts
    Fixes typescript errors for css modules and svg components.

---
+++ axelor-front/src/declare.d.ts
@@ -0,0 +1,13 @@
+// CSS modules
+type CSSModuleClasses = { readonly [key: string]: string };
+
+declare module '*.module.css' {
+  const classes: CSSModuleClasses;
+  export default classes;
+}
+declare module '*.module.scss' {
+  const classes: CSSModuleClasses;
+  export default classes;
+}
+
+declare module '*.svg';
