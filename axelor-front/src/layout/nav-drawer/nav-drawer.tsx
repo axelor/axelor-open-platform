7e9d593b7408fc2a688109fa62394b6b50b89c85
Amit Mendapara <amit@axelor.com>
2023-03-01 15:26:37 +0100

    Add icons to sidebar menu

--- axelor-front/src/layout/nav-drawer/nav-drawer.tsx
+++ axelor-front/src/layout/nav-drawer/nav-drawer.tsx
@@ -1,21 +1,32 @@
 import { useMenu } from "@/hooks/use-menu/use-menu";
 import { useTabs } from "@/hooks/use-tabs/use-tabs";
 import { MenuItem } from "@/services/client/meta.types";
-import { Box, NavBar, NavItemProps } from "@axelor/ui";
+import { legacyClassNames } from "@/styles/legacy";
+import { Box, Icon, NavBar, NavItemProps } from "@axelor/ui";
+import { ReactComponent as BiListIcon } from "bootstrap-icons/icons/list.svg";
 import { useCallback, useMemo } from "react";
 
 function load(res: MenuItem[]) {
   const menus = res.filter((item) => item.left !== false);
   const toNavItemProps = (item: MenuItem): NavItemProps => {
-    const { name, title, action } = item;
+    const { name, title, action, icon, iconBackground } = item;
     const items = action
       ? undefined
       : menus.filter((x) => x.parent === name).map(toNavItemProps);
-    return {
+
+    const props: NavItemProps = {
       id: name,
       title,
       items,
     };
+
+    if (icon || !item.parent) {
+      props.icon = icon
+        ? ({ color }) => <i className={legacyClassNames("fa", icon)} />
+        : ({ color }) => <Icon as={BiListIcon} style={{ color }} />;
+    }
+
+    return props;
   };
   return menus.filter((item) => !item.parent).map(toNavItemProps);
 }
