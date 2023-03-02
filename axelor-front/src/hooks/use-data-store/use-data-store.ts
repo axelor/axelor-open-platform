d60081d6a06d8b28e731559f5dcba11ce37884cd
Amit Mendapara <amit@axelor.com>
2023-03-02 13:48:27 +0100

    Implement useDataStore
    The hook can be used to deal with data backed by data service.
    The hook will make sure to keep track of loaded records and search
    options.

---
+++ axelor-front/src/hooks/use-data-store/use-data-store.ts
@@ -0,0 +1,132 @@
+import {
+  DataSource,
+  DeleteOption,
+  ReadOptions,
+  SearchOptions,
+  SearchPage,
+} from "@/services/client/data";
+import { DataRecord } from "@/services/client/data.types";
+import { atom, useAtom } from "jotai";
+import { useCallback, useMemo, useState } from "react";
+
+export interface DataStore extends DataSource {
+  readonly records: DataRecord[];
+  readonly page: SearchPage;
+}
+
+export function useDataStore(model: string): DataStore;
+export function useDataStore(model: string, options: SearchOptions): DataStore;
+export function useDataStore(
+  model: string,
+  options: SearchOptions = {}
+): DataStore {
+  const [recordsAtom] = useState(() => atom<DataRecord[]>([]));
+  const [pageAtom] = useState(() => atom<SearchPage>({}));
+  const [optsAtom] = useState(() => atom<SearchOptions>(options));
+
+  const [records, setRecords] = useAtom(recordsAtom);
+  const [page, setPage] = useAtom(pageAtom);
+  const [opts, setOpts] = useAtom(optsAtom);
+
+  const ds = useMemo(() => new DataSource(model), [model]);
+
+  const prepareOptions = useCallback(
+    (options: SearchOptions) => {
+      // have to re-use previous domain and context
+      const { _domain, _domainContext } = opts?.filter ?? {};
+      const filter = {
+        _domain,
+        _domainContext,
+        ...options.filter,
+      };
+      return {
+        ...opts,
+        ...options,
+        filter,
+      };
+    },
+    [opts]
+  );
+
+  const doSearch = useCallback(
+    async (options: SearchOptions) => {
+      const args = prepareOptions(options);
+      const res = await ds.search(args);
+
+      // preserve updated options
+      setOpts(args);
+
+      // preserve result
+      setRecords(res.records);
+      setPage(res.page);
+
+      return res;
+    },
+    [ds, opts, setOpts, setRecords, setPage]
+  );
+
+  const doRead = useCallback(
+    async (id: number, options?: ReadOptions) => {
+      const { fields } = opts;
+      const args = {
+        fields,
+        ...options,
+      };
+      return await ds.read(id, args);
+    },
+    [ds, opts]
+  );
+
+  const doSave = useCallback(
+    async (record: DataRecord) => {
+      return ds.save(record);
+    },
+    [ds]
+  );
+
+  const doCopy = useCallback(
+    async (id: number) => {
+      return ds.copy(id);
+    },
+    [ds]
+  );
+
+  const doDelete = useCallback(
+    async (options: DeleteOption | DeleteOption[]) => {
+      const ids = Array.isArray(options)
+        ? options.map((x) => x.id)
+        : [options.id];
+      const res = await ds.delete(options);
+      const data = records.filter((x) => !ids.includes(x.id!));
+
+      setRecords(data);
+      setPage((prev) => {
+        const { totalCount = records.length } = prev;
+        return {
+          ...prev,
+          totalCount: totalCount - res,
+        };
+      });
+
+      return res;
+    },
+    [ds, setRecords, setPage]
+  );
+
+  const doExport = useCallback(async (options: SearchOptions) => {
+    const args = prepareOptions(options);
+    return ds.export(args);
+  }, []);
+
+  return {
+    records,
+    page,
+    model,
+    search: doSearch,
+    read: doRead,
+    save: doSave,
+    copy: doCopy,
+    delete: doDelete,
+    export: doExport,
+  } as DataStore;
+}
