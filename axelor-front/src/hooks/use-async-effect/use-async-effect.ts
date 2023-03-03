9c822d3f869d2a4ef9ef70c3a5068989ddf4b2cd
Amit Mendapara <amit@axelor.com>
2023-03-03 16:27:55 +0100

    Add useAsyncEffect hook
    This hook can be used to run async side effects. It also takes care
    of double rendering in strict mode.

---
+++ axelor-front/src/hooks/use-async-effect/use-async-effect.ts
@@ -0,0 +1,42 @@
+import { DependencyList, EffectCallback, useEffect, useRef } from "react";
+
+type AsyncEffectCallbackResult = ReturnType<EffectCallback>;
+type AsyncEffectCallback = (
+  signal: AbortSignal
+) => Promise<AsyncEffectCallbackResult>;
+
+export function useAsyncEffect(
+  effect: AsyncEffectCallback,
+  deps?: DependencyList
+) {
+  const abortRef = useRef<AbortController>();
+
+  useEffect(() => {
+    abortRef.current = new AbortController();
+    return () => {
+      abortRef.current?.abort();
+    };
+  }, []);
+
+  useEffect(() => {
+    let canceled = false;
+    let signal = abortRef.current!.signal;
+    let result: AsyncEffectCallbackResult;
+    let load = async () => {
+      await Promise.resolve(); // wait for clean up in strict mode
+      if (canceled || signal.aborted) {
+        return;
+      }
+      result = await effect(signal);
+    };
+
+    load();
+
+    return () => {
+      canceled = true;
+      if (result) {
+        result();
+      }
+    };
+  }, deps);
+}
