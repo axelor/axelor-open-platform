87978320f0b9925b5289baff3b01bca6fa46acae
Amit Mendapara <amit@axelor.com>
2023-03-06 00:40:37 +0100

    Improve useMeta hook
    Add findView and findFields methods.

--- axelor-front/src/hooks/use-meta/use-meta.ts
+++ axelor-front/src/hooks/use-meta/use-meta.ts
@@ -1,14 +1,16 @@
 import * as meta from "@/services/client/meta";
-import { ActionView, View } from "@/services/client/meta.types";
+import { ActionView } from "@/services/client/meta.types";
 import { atom, useAtom } from "jotai";
 import { useCallback } from "react";
 
 const actionViewsAtom = atom<Record<string, ActionView>>({});
-const viewsAtom = atom<Record<string, View>>({});
+const viewsAtom = atom<Record<string, meta.ViewData<any>>>({});
+const fieldsAtom = atom<Record<string, meta.MetaData>>({});
 
 export function useMeta() {
   const [actionViews, setActionViews] = useAtom(actionViewsAtom);
   const [views, setViews] = useAtom(viewsAtom);
+  const [fields, setFields] = useAtom(fieldsAtom);
 
   const findActionView = useCallback(
     async (name: string) => {
@@ -24,13 +26,53 @@ export function useMeta() {
     [actionViews, setActionViews]
   );
 
-  const findView = (opts: {
-    type: string;
-    name?: string;
-    model?: string;
-  }) => {};
+  const findView = useCallback(
+    async ({
+      type,
+      name,
+      model,
+    }: {
+      type: string;
+      name?: string;
+      model?: string;
+    }) => {
+      let key = `${type}:${name}:${model}`;
+      let view = views[key];
+      if (view) {
+        return view;
+      }
+
+      view = await meta.view({ type: type as any, name, model });
+      view = processView(view);
+
+      setViews((state) => ({ ...state, [key]: view }));
+
+      return view;
+    },
+    [setViews, views]
+  );
+
+  const findFields = useCallback(
+    async (model: string) => {
+      let res = fields[model];
+      if (res) {
+        return res;
+      }
+      res = await meta.fields(model);
+      setFields((state) => ({ ...state, [model]: res }));
+      return res;
+    },
+    [fields, setFields]
+  );
 
   return {
     findActionView,
+    findFields,
+    findView,
   };
+}
+
+function processView<T>(data: meta.ViewData<T>): meta.ViewData<T> {
+  // TODO: process view
+  return data;
 }
