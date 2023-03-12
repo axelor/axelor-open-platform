e7ccaa275cb0f104d21e4b8a95d8d58ccc3f1222
Amit Mendapara <amit@axelor.com>
2023-03-12 01:35:45 +0100

    Fix form layout issues

--- axelor-front/src/views/form/widgets/panel/panel.tsx
+++ axelor-front/src/views/form/widgets/panel/panel.tsx
@@ -1,19 +1,32 @@
-import { Box } from "@axelor/ui";
+import { Box, StyleProps } from "@axelor/ui";
 import { GridLayout, WidgetProps } from "../../builder";
 import styles from "./panel.module.css";
 
+const withHeaderProps: StyleProps = {
+  rounded: true,
+  border: true,
+};
+
+const withHeaderBodyProps: StyleProps = {
+  p: 3,
+};
+
 export function Panel(props: WidgetProps) {
   const { schema, formAtom } = props;
-  const { title } = schema;
+  const { title, showTitle } = schema;
+
+  const hasHeader = showTitle !== false && title;
+  const moreProps = hasHeader && withHeaderProps;
+  const bodyProps = hasHeader && withHeaderBodyProps;
 
   return (
-    <Box className={styles.panel} rounded border>
-      {title && (
-        <Box className={styles.panelHeader} borderBottom>
+    <Box className={styles.panel} {...moreProps}>
+      {hasHeader && (
+        <Box className={styles.panelHeader} borderBottom px={3} py={2}>
           {title}
         </Box>
       )}
-      <Box className={styles.panelBody}>
+      <Box className={styles.panelBody} {...bodyProps}>
         <GridLayout formAtom={formAtom} schema={schema} />
       </Box>
     </Box>
