59f789be805c18718ca18f09f467aabb589a011c
Amit Mendapara <amit@axelor.com>
2023-03-18 22:06:10 +0100

    Implement ViewToolbar component

---
+++ axelor-front/src/view-containers/view-toolbar/view-toolbar.tsx
@@ -0,0 +1,180 @@
+import { Box, CommandBar, CommandItemProps } from "@axelor/ui";
+import { useView } from "../views/hooks";
+
+import { useSession } from "@/hooks/use-session";
+import { View } from "@/services/client/meta.types";
+import { toTitleCase } from "@/utils/names";
+import { MaterialIconProps } from "@axelor/ui/src/icons/meterial-icon";
+import { useCallback, useMemo } from "react";
+import styles from "./view-toolbar.module.scss";
+
+export type ViewToolBarProps = {
+  actions: CommandItemProps[];
+  children?: React.ReactNode;
+  pagination?: {
+    text?: string | (() => JSX.Element);
+    onNext: () => any;
+    onPrev: () => any;
+  };
+};
+
+const ViewIcons: Record<string, MaterialIconProps["icon"]> = {
+  grid: "table",
+  form: "article",
+  cards: "grid_view",
+  tree: "account_tree",
+  calendar: "calendar_month",
+  kanban: "view_kanban",
+  gantt: "calendar_month",
+  chart: "monitoring",
+  dashboard: "dashboard",
+};
+
+export function ViewToolBar(props: ViewToolBarProps) {
+  const {
+    actions = [],
+    children,
+    pagination: { text: pageTextOrComp, onNext, onPrev } = {},
+  } = props;
+
+  const pageActions = onPrev || onNext;
+
+  const { data: sessionInfo } = useSession();
+  const [viewState, setViewState] = useView();
+
+  const {
+    type: viewType,
+    view: { views = [] },
+  } = viewState;
+
+  const switchTo = useCallback(
+    (type: string) => {
+      setViewState((prev) => {
+        return prev.type === type ? prev : { ...prev, type };
+      });
+    },
+    [setViewState]
+  );
+
+  const switchActions = useMemo(() => {
+    if (views.length === 1) return;
+    return views.map((item) => {
+      const key = item.type;
+      const text = toTitleCase(item.type);
+      return {
+        key,
+        text,
+        description: text,
+        iconOnly: true,
+        disabled: viewType === key,
+        iconProps: {
+          icon: ViewIcons[key] ?? "table_view",
+        },
+        onClick: () => switchTo(key),
+      };
+    });
+  }, [switchTo, views, viewType]);
+
+  const farItems = useMemo(() => {
+    const items: CommandItemProps[] = [];
+    const view: View | null = {} as any; // TODO: get current view meta
+
+    if (view?.viewId) {
+      items.push({
+        key: "view",
+        text: "View...",
+      });
+    }
+
+    if (view?.modelId) {
+      items.push({
+        key: "model",
+        text: "Model...",
+      });
+    }
+
+    if (viewState?.view.actionId) {
+      items.push({
+        key: "action",
+        text: "Action...",
+      });
+    }
+
+    const command: CommandItemProps = {
+      key: "settings",
+      iconOnly: true,
+      iconProps: {
+        icon: "settings",
+      },
+      items,
+    };
+
+    return [command];
+  }, [viewState?.view.actionId, viewType, views]);
+
+  const pageText =
+    typeof pageTextOrComp === "string" ? pageTextOrComp : undefined;
+  const PageComp =
+    typeof pageTextOrComp === "function" ? pageTextOrComp : undefined;
+
+  return (
+    <Box className={styles.toolbar} shadow>
+      <CommandBar
+        className={styles.actions}
+        iconProps={{
+          weight: 300,
+        }}
+        iconOnly
+        items={actions}
+      />
+      <Box className={styles.extra}>{children}</Box>
+      {pageText && <Box className={styles.pageInfo}>{pageText}</Box>}
+      {PageComp && <PageComp />}
+      {pageActions && (
+        <CommandBar
+          className={styles.pageActions}
+          iconProps={{
+            weight: 300,
+          }}
+          items={[
+            {
+              key: "prev",
+              text: "Prev",
+              iconProps: {
+                icon: "navigate_before",
+              },
+              onClick: onPrev,
+            },
+            {
+              key: "next",
+              text: "Next",
+              iconSide: "end",
+              iconProps: {
+                icon: "navigate_next",
+              },
+              onClick: onNext,
+            },
+          ]}
+        />
+      )}
+      {switchActions && (
+        <CommandBar
+          items={switchActions}
+          className={styles.viewSwitch}
+          iconProps={{
+            weight: 300,
+          }}
+        />
+      )}
+      {sessionInfo?.user.technical && (
+        <CommandBar
+          items={farItems}
+          className={styles.farItems}
+          iconProps={{
+            weight: 300,
+          }}
+        />
+      )}
+    </Box>
+  );
+}
