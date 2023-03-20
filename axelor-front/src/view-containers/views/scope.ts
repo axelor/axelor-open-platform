5b3c77c1014081706cb64f12b90bca2c025a9acb
Amit Mendapara <amit@axelor.com>
2023-03-21 00:36:11 +0100

    Document ViewScope hooks

--- axelor-front/src/view-containers/views/scope.ts
+++ axelor-front/src/view-containers/views/scope.ts
@@ -30,26 +30,58 @@ const viewMolecule = molecule((getMol, getScope) => {
   return atom(initialView);
 });
 
+/**
+ * This scoped hook can be used to access current Tab.
+ *
+ * @returns current Tab
+ */
 export function useViewTab() {
   const viewAtom = useMolecule(viewMolecule);
   return useAtomValue(viewAtom);
 }
 
+/**
+ * This scoped hook can be used to access current action view.
+ *
+ * @returns current ActionView
+ */
 export function useViewAction() {
   const tab = useViewTab();
   return tab.action;
 }
 
+/**
+ * This scoped hook can be used to access current tab state.
+ *
+ * @returns tuple of current state and setter
+ */
 export function useViewState() {
   const tab = useViewTab();
   return useAtom(tab.state);
 }
 
 interface SwitchTo {
+  /**
+   * Switch to the given view type.
+   *
+   * @param type view type
+   */
   (type: string): void;
+
+  /**
+   * Update the route with the given options.
+   *
+   * @param options route options
+   */
   (route: { mode?: string; id?: string; qs?: Record<string, string> }): void;
 }
 
+/**
+ * This scoped hook can be used to switch between different
+ * views or update route of the current tab.
+ *
+ * @returns a function to switch views or update route
+ */
 export function useViewSwitch() {
   const tab = useViewTab();
   const action = tab.id;
