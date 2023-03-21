05233fae234df301446efa0f41368dafaada1d8a
Phi-Long <p.do@axelor.com>
2023-03-21 10:59:37 +0100

    Refactor useLocale hook

--- axelor-front/src/hooks/use-locale/use-locale.ts
+++ axelor-front/src/hooks/use-locale/use-locale.ts
@@ -1,13 +1,9 @@
 import { useMemo } from "react";
 import { useSession } from "../use-session/use-session";
 import { getPreferredLocale } from "../../utils/locale";
-import { SessionInfo } from "@/services/client/session";
 
 export function useLocale() {
-  const {
-    user: { lang },
-  } = useSession().info || ({} as SessionInfo);
-  const preferredLocale = useMemo(() => getPreferredLocale(lang), [lang]);
-
-  return preferredLocale;
+  const lang = useSession().data?.user.lang;
+  const locale = useMemo(() => getPreferredLocale(lang), [lang]);
+  return locale;
 }
