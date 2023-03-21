8f9e077e9d5174fa5dc2ffd091d3ec865c51d5ab
Amit Mendapara <amit@axelor.com>
2023-03-21 12:14:06 +0100

    Document useTabs hook

--- axelor-front/src/hooks/use-tabs/use-tabs.ts
+++ axelor-front/src/hooks/use-tabs/use-tabs.ts
@@ -6,6 +6,10 @@ import { navigate } from "@/routes";
 import { findActionView } from "@/services/client/meta-cache";
 import { ActionView } from "@/services/client/meta.types";
 
+/**
+ * The route state of a specific view type.
+ *
+ */
 export type TabRoute = {
   action: string;
   mode?: string;
@@ -13,19 +17,72 @@ export type TabRoute = {
   qs?: Record<string, string>;
 };
 
+/**
+ * The TabState represents volatile state of a tab.
+ *
+ */
 export type TabState = {
+  /**
+   * The tab title
+   *
+   */
   title: string;
+
+  /**
+   * The current view type
+   *
+   */
   type: string;
+
+  /**
+   * Whether the tab is dirty
+   *
+   * Can be used to show confirmation dialong before closing the tab.
+   */
   dirty?: boolean;
+
+  /**
+   * Keeps track of route options per view type.
+   *
+   */
   routes?: Record<string, TabRoute>;
 };
 
+/**
+ * This atom is used to keep track of tab state.
+ *
+ */
 export type TabAtom = WritableAtom<TabState, Partial<TabState>[], void>;
 
+/**
+ * A tab represents an action-view visible as a tab or popup view.
+ *
+ */
 export type Tab = {
+  /**
+   * Unique id of the tab, usually the name of the action-view.
+   *
+   * If id starts with `$`, browser route won't be handled for that tab.
+   */
   readonly id: string;
+
+  /**
+   * The initial display title.
+   *
+   * The actual title is displayed from the `state`.
+   */
   readonly title: string;
+
+  /**
+   * The action-view of this tab.
+   *
+   */
   readonly action: ActionView;
+
+  /**
+   * The volatile state of the tab
+   *
+   */
   state: TabAtom;
 };
 
@@ -55,6 +112,17 @@ const getViewType = (mode: string) => {
   return mode;
 };
 
+/**
+ * This function updates route state of a specific view type.
+ *
+ * The target view type will be determined by the `route.mode`
+ * or if not given by the `state.type`.
+ *
+ * @param action the action name
+ * @param state the `tab.state` to update
+ * @param route the route options to update
+ * @returns updated `state`
+ */
 const updateRouteState = (
   action: string,
   state: TabState,
@@ -83,6 +151,25 @@ const updateRouteState = (
 
   return { ...state, type, routes: { ...state.routes, [type]: n } };
 };
+
+/**
+ * Open the given view with the given optional route options.
+ *
+ * @param view the action-view or name
+ * @param route the route options
+ * @returns a Tab or null if no tab was opened (due to non-existance of the action-view)
+ */
+export type OpenTab = (
+  view: ActionView | string,
+  route?: Omit<TabRoute, "action">
+) => Promise<Tab | null>;
+
+/**
+ * Close the given view.
+ *
+ * @param view the action-view or name
+ */
+export type CloseTab = (view: ActionView | string) => void;
 
 const openAtom = atom(
   null,
@@ -182,12 +269,18 @@ const closeAtom = atom(null, async (get, set, view: ActionView | string) => {
   }
 });
 
+/**
+ * This hook can be used to handle view tabs/popups.
+ *
+ * @returns current state of opened views and open/close
+ *          methods to open new views or close a view.
+ */
 export function useTabs() {
   const { tabs: items } = useAtomValue(tabsAtom);
   const active = useAtomValue(activeAtom) ?? null;
 
-  const open = useSetAtom(openAtom);
-  const close = useSetAtom(closeAtom);
+  const open: OpenTab = useSetAtom(openAtom);
+  const close: CloseTab = useSetAtom(closeAtom);
 
   return {
     active,
