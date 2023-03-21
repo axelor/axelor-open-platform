5268654170cc551f512c482437a89acc3b37d576
Amit Mendapara <amit@axelor.com>
2023-03-21 13:47:10 +0100

    Add usePerms hook
    This hook can be used to check permissions

---
+++ axelor-front/src/hooks/use-perms/use-perms.ts
@@ -0,0 +1,82 @@
+import { Perms, Schema } from "@/services/client/meta.types";
+import { useCallback } from "react";
+
+const defaultPerms: Perms = {};
+
+const PERMS = {
+  new: "create",
+  edit: "write",
+  save: "write",
+  copy: "create",
+  delete: "remove",
+  archive: "remove",
+  export: "export",
+  attach: "__",
+} as const;
+
+const ACTIONS = {
+  new: "canNew",
+  edit: "canEdit",
+  save: "canSave",
+  copy: "canCopy",
+  delete: "canDelete",
+  archive: "canArchive",
+  export: "__",
+  attach: "canAttach",
+} as const;
+
+type PermisionName = keyof typeof PERMS | keyof typeof ACTIONS;
+
+function attr(props: Schema, which: string) {
+  return props[which] !== false;
+}
+
+function perm(perms: Perms, which: keyof Perms) {
+  return perms[which] !== false;
+}
+
+function can(props: Schema, perms: Perms, what: PermisionName) {
+  return attr(props, ACTIONS[what]) && perm(perms, PERMS[what] as keyof Perms);
+}
+
+/**
+ * This hook can be used to check permissions
+ * and use of a specific action button.
+ *
+ * @param state the current state of the view/field
+ * @param perms the permissions state
+ * @returns `{ hasPermission, hasButton }` to check permission and button
+ *
+ */
+export function usePerms(state: Schema, perms = defaultPerms) {
+  const hasPermission = useCallback(
+    /**
+     * Check whether the given operation is permitted.
+     *
+     * @param name operation name
+     * @returns true if permitted false otherwise
+     */
+    (name: PermisionName) => {
+      return can(state, perms, name);
+    },
+    [perms, state]
+  );
+
+  const hasButton = useCallback(
+    /**
+     * Check whether the given operation button is available.
+     *
+     * The button is not available is specified by the meta attrs
+     * or not permitted for that operation.
+     *
+     * @param name operation name
+     * @returns true if permitted false otherwise
+     */
+    (name: string) => {
+      return can(state, perms, name as PermisionName) && state[name] !== false;
+    },
+    [perms, state]
+  );
+
+  return { hasPermission, hasButton } as const;
+}
