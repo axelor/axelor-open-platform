81748d26437704b918a6743c48984ec5d3d7fbf8
Mayank Mahadevwala <m.mahadevwala@axelor.com>
2023-03-22 15:40:19 +0530

    Add edit column in grid view

--- axelor-front/src/views/grid/grid.tsx
+++ axelor-front/src/views/grid/grid.tsx
@@ -1,13 +1,15 @@
 import { useAtom } from "jotai";
 import { atomWithImmer } from "jotai-immer";
-import { useCallback, useMemo } from "react";
-
+import { useCallback, useEffect, useMemo } from "react";
 import _ from "lodash";
 import {
   Grid as AxGrid,
   GridProvider as AxGridProvider,
+  GridColumn,
+  GridRow,
   GridState,
 } from "@axelor/ui/grid";
+import { MaterialIcon } from "@axelor/ui/icons/meterial-icon";
 
 import { useAsync } from "@/hooks/use-async";
 import { useDataStore } from "@/hooks/use-data-store";
@@ -18,6 +20,7 @@ import { ViewToolBar } from "@/view-containers/view-toolbar";
 import { ViewProps } from "../types";
 import { useViewProps } from "@/view-containers/views/scope";
 import styles from "./grid.module.scss";
+import { Box } from "@axelor/ui";
 
 function formatter(column: Field, value: any, record: any) {
   return format(value, {
@@ -49,7 +52,7 @@ export function Grid(props: ViewProps<GridView>) {
 
   const { columns, names } = useMemo(() => {
     const names: string[] = [];
-    const columns = view.items!.map((item) => {
+    const columns: GridColumn[] = view.items!.map((item) => {
       const field = fields?.[item.name!];
       const title = item.title ?? item.autoTitle;
       const attrs = item.widgetAttrs;
@@ -68,8 +71,29 @@ export function Grid(props: ViewProps<GridView>) {
         serverType,
         title,
         formatter,
-      };
+      } as any;
     });
+
+    if (view.editIcon !== false) {
+      columns.unshift({
+        title: "",
+        name: "$$edit",
+        renderer: ({ style, onClick, className }: any) => (
+          <Box
+            d="flex"
+            justifyContent="center"
+            alignItems="center"
+            {...{ style, onClick, className }}
+          >
+            <MaterialIcon icon="edit" opticalSize={20} />
+          </Box>
+        ),
+        computed: true,
+        sortable: false,
+        searchable: false,
+        width: 32,
+      });
+    }
 
     return { columns, names: _.uniq(names) };
   }, [view, fields]);
@@ -83,6 +107,28 @@ export function Grid(props: ViewProps<GridView>) {
       onSearch();
     }
   }, [dataStore]);
+
+  const handleCellClick = useCallback(
+    (
+      e: React.SyntheticEvent,
+      col: GridColumn,
+      colIndex: number,
+      row: GridRow,
+      rowIndex: number
+    ) => {
+      if (col.name === "$$edit") {
+      // TODO: open record i.e. row.record in edit mode
+      }
+    },
+    []
+  );
+
+  const handleRowDoubleClick = useCallback(
+    (e: React.SyntheticEvent, row: GridRow, rowIndex: number) => {
+      // TODO: open record i.e. row.record in view mode
+    },
+    []
+  );
 
   useEffect(() => {
     if (viewProps?.selectedCell !== state.selectedCell) {
@@ -152,7 +198,6 @@ export function Grid(props: ViewProps<GridView>) {
           allowGrouping
           allowSorting
           allowSelection
-          allowCheckboxSelection
           allowCellSelection
           allowColumnHide
           allowColumnOptions
@@ -163,6 +208,8 @@ export function Grid(props: ViewProps<GridView>) {
           columns={columns}
           state={state}
           setState={setState}
+          onCellClick={handleCellClick}
+          onRowDoubleClick={handleRowDoubleClick}
         />
       </AxGridProvider>
     </div>
