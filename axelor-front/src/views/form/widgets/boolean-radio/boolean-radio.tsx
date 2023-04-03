69fa93fdbc3583cec0cd660264db51016ec95cd4
Mayank Mahadevwala <m.mahadevwala@axelor.com>
2023-04-03 13:55:17 +0530

    Implement boolean-radio form widget

---
+++ axelor-front/src/views/form/widgets/boolean-radio/boolean-radio.tsx
@@ -0,0 +1,49 @@
+import { Box, Input, InputLabel } from "@axelor/ui";
+import { FieldContainer, FieldProps } from "../../builder";
+import { useAtom } from "jotai";
+import { i18n } from "@/services/client/i18n";
+
+export function BooleanRadio({
+  schema,
+  readonly,
+  valueAtom,
+}: FieldProps<boolean | null>) {
+  const { uid, name, title, widgetAttrs, nullable, direction } = schema;
+  const { falseText = i18n.get("No"), trueText = i18n.get("Yes") } =
+    widgetAttrs || {};
+  const [value = false, setValue] = useAtom(valueAtom);
+
+  function renderRadio($value: boolean, label: string) {
+    const checked = value === $value;
+    return (
+      <Box d="flex" alignItems="center">
+        <Input
+          m={0}
+          name={name}
+          type="radio"
+          value={`${$value}`}
+          checked={checked}
+          onChange={() => {}}
+          onClick={() => setValue(nullable && checked ? null : $value, true)}
+        />
+        <Box as="span" d="inline-block" ms={1} me={3}>
+          {i18n.get(label)}
+        </Box>
+      </Box>
+    );
+  }
+
+  return (
+    <FieldContainer readonly={readonly}>
+      <InputLabel htmlFor={uid}>{title}</InputLabel>
+      <Box
+        d="flex"
+        flexDirection={direction === 'vertical' ? "column" : "row"}
+        className="input"
+      >
+        {renderRadio(true, trueText)}
+        {renderRadio(false, falseText)}
+      </Box>
+    </FieldContainer>
+  );
+}
