66699668a9c4e062d6673e90e707a47ece95b5b6
Mayank Mahadevwala <m.mahadevwala@axelor.com>
2023-04-03 13:53:52 +0530

    Implement boolean-select form widget

---
+++ axelor-front/src/views/form/widgets/boolean-select/boolean-select.tsx
@@ -0,0 +1,69 @@
+import { Input, InputLabel, Select } from "@axelor/ui";
+import { FieldContainer, FieldProps } from "../../builder";
+import { useAtom } from "jotai";
+import { useCallback, useMemo } from "react";
+import { i18n } from "@/services/client/i18n";
+
+type SelectOption = {
+  title: string;
+  value: boolean | null;
+};
+
+export function BooleanSelect({
+  schema,
+  readonly,
+  valueAtom,
+}: FieldProps<boolean | null>) {
+  const { uid, title, widgetAttrs, nullable } = schema;
+  const {
+    nullText = " ",
+    falseText = i18n.get("No"),
+    trueText = i18n.get("Yes"),
+  } = widgetAttrs || {};
+  const [value = false, setValue] = useAtom(valueAtom);
+
+  const handleOnChange = useCallback(
+    (option: SelectOption) => {
+      setValue(nullable && option.value === null ? null : option.value, true);
+    },
+    [nullable, setValue]
+  );
+
+  const options = useMemo<SelectOption[]>(
+    () => [
+      ...(nullable ? [{ title: i18n.get(nullText), value: null }] : []),
+      { title: i18n.get(trueText), value: true },
+      { title: i18n.get(falseText), value: false },
+    ],
+    [nullable, nullText, trueText, falseText]
+  );
+
+  const selected = options.find((option) => option.value === value);
+
+  return (
+    <FieldContainer readonly={readonly}>
+      <InputLabel htmlFor={uid}>{title}</InputLabel>
+      {readonly ? (
+        <Input
+          type="text"
+          defaultValue={(selected?.value === null ? "" : selected?.title) || ""}
+          disabled
+          readOnly
+          bg="body"
+          border={false}
+        />
+      ) : (
+        <Select
+          value={selected}
+          onChange={handleOnChange}
+          options={options}
+          optionLabel="title"
+          optionValue="value"
+          isSearchable={false}
+          isClearable={false}
+          isClearOnDelete={false}
+        />
+      )}
+    </FieldContainer>
+  );
+}
