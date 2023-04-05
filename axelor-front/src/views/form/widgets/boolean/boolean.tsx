a772b4bcfc645aeb7ca51707cb7772fa3db05f81
Mayank Mahadevwala <m.mahadevwala@axelor.com>
2023-04-05 18:15:07 +0530

    Implement inline-checkbox form widget

--- axelor-front/src/views/form/widgets/boolean/boolean.tsx
+++ axelor-front/src/views/form/widgets/boolean/boolean.tsx
@@ -1,14 +1,16 @@
 import { Input } from "@axelor/ui";
 import { FieldContainer, FieldProps } from "../../builder";
 import { useAtom } from "jotai";
-import classes from './boolean.module.scss';
+import { toKebabCase } from "@/utils/names";
+import classes from "./boolean.module.scss";
 
 export function Boolean({ schema, readonly, valueAtom }: FieldProps<boolean>) {
   const { uid, name, title } = schema;
   const [value, setValue] = useAtom(valueAtom);
+  const showTitle = toKebabCase(schema.widget) !== "inline-checkbox";
   return (
     <FieldContainer readonly={readonly}>
-      <label htmlFor={uid}>{title}</label>
+      {showTitle && <label htmlFor={uid}>{title}</label>}
       <Input
         type="checkbox"
         className={classes.input}
