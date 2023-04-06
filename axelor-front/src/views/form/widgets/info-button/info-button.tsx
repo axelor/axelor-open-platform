400658d29b63ffb8e4871522c3a792d08814bcd4
Mayank Mahadevwala <m.mahadevwala@axelor.com>
2023-04-06 17:42:03 +0530

    Implement info-button form widget

---
+++ axelor-front/src/views/form/widgets/info-button/info-button.tsx
@@ -0,0 +1,71 @@
+import { dialogs } from "@/components/dialogs";
+import clsx from "clsx";
+import { useAtomValue } from "jotai";
+import { focusAtom } from "jotai-optics";
+import { useCallback, useMemo } from "react";
+
+import { Box, Button } from "@axelor/ui";
+
+import { legacyClassNames } from "@/styles/legacy";
+
+import { FieldContainer, WidgetProps } from "../../builder";
+import { useFormScope } from "../../builder/scope";
+
+import styles from "./info-button.module.scss";
+
+export function InfoButton({ schema, readonly, formAtom }: WidgetProps) {
+  const { title, icon, iconHover, widgetAttrs } = schema;
+  const { field } = widgetAttrs || {};
+
+  const { actionExecutor } = useFormScope();
+  const value = useAtomValue(
+    useMemo(
+      () => focusAtom(formAtom, (o) => o.prop("record").prop(field!)),
+      [formAtom, field]
+    )
+  );
+
+  const handleClick = useCallback(async () => {
+    const { prompt, onClick } = schema;
+    if (prompt) {
+      const confirmed = await dialogs.confirm({
+        content: prompt,
+      });
+      if (!confirmed) return;
+    }
+    actionExecutor.execute(onClick);
+  }, [actionExecutor, schema]);
+
+  return (
+    <FieldContainer readonly={readonly}>
+      <Button
+        title={title}
+        variant="primary"
+        className={clsx(styles.button)}
+        onClick={handleClick}
+      >
+        <Box
+          as="i"
+          className={clsx(styles.icon, legacyClassNames("fa", icon), {
+            [styles.hideOnHover]: iconHover,
+          })}
+        />
+        {iconHover && (
+          <Box
+            as="i"
+            className={clsx(
+              styles.iconHover,
+              styles.showOnHover,
+              legacyClassNames("fa", iconHover)
+            )}
+          />
+        )}
+
+        <Box className={styles.data}>
+          {value && <div className={styles.value}>{value}</div>}
+          <div className={styles.title}>{title}</div>
+        </Box>
+      </Button>
+    </FieldContainer>
+  );
+}
