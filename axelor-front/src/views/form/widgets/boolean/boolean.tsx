788986318bb079738015d2f8df46b238b5d3a5aa
Mayank Mahadevwala <m.mahadevwala@axelor.com>
2023-04-10 19:05:44 +0530

    Fix inline-checkbox widget styles

--- axelor-front/src/views/form/widgets/boolean/boolean.tsx
+++ axelor-front/src/views/form/widgets/boolean/boolean.tsx
@@ -1,26 +1,39 @@
-import { Input } from "@axelor/ui";
-import { FieldContainer, FieldProps } from "../../builder";
+import { Box, Input, InputLabel } from "@axelor/ui";
 import { useAtom } from "jotai";
-import { toKebabCase } from "@/utils/names";
+import { FieldContainer, FieldProps } from "../../builder";
 import classes from "./boolean.module.scss";
 
 export function Boolean({ schema, readonly, valueAtom }: FieldProps<boolean>) {
   const { uid, name, title } = schema;
   const [value, setValue] = useAtom(valueAtom);
-  const showTitle = toKebabCase(schema.widget) !== "inline-checkbox";
+  const inline = schema.widget === "inline-checkbox";
   return (
     <FieldContainer readonly={readonly}>
-      {showTitle && <label htmlFor={uid}>{title}</label>}
-      <Input
-        type="checkbox"
-        className={classes.input}
-        checked={!!value}
-        onChange={() => {
-          setValue(!value, true);
-        }}
-        value={name}
-        disabled={readonly}
-      />
+      <Box
+        d="flex"
+        g={2}
+        flexDirection={inline ? "row-reverse" : "column"}
+        {...(inline && {
+          alignItems: "center",
+          justifyContent: "flex-end",
+        })}
+      >
+        <InputLabel htmlFor={uid} m={0}>
+          {title}
+        </InputLabel>
+        <Input
+          m={0}
+          id={uid}
+          type="checkbox"
+          className={classes.input}
+          checked={!!value}
+          onChange={() => {
+            setValue(!value, true);
+          }}
+          value={name}
+          disabled={readonly}
+        />
+      </Box>
     </FieldContainer>
   );
 }
