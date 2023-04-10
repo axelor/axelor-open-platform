1ac341de9dd0f98649152b5793768f685eb5e054
Amit Mendapara <amit@axelor.com>
2023-04-10 06:34:41 +0530

    Implement useInput hook for input fields
    This hook can be used with input fields to handle value change.

--- axelor-front/src/views/form/builder/hooks.ts
+++ axelor-front/src/views/form/builder/hooks.ts
@@ -1,5 +1,9 @@
+import { useAtomValue, useSetAtom } from "jotai";
+import React, { useCallback, useState } from "react";
+
 import { useAsync } from "@/hooks/use-async";
 import { toCamelCase, toKebabCase } from "@/utils/names";
+import { ValueAtom } from "./types";
 
 export function useWidgetComp(type: string) {
   return useAsync(async () => {
@@ -8,4 +12,64 @@ export function useWidgetComp(type: string) {
     const { [name]: Comp } = await import(`../widgets/${module}/index.ts`);
     return Comp as React.ElementType;
   }, [type]);
+}
+
+/**
+ * This hook can be used with input fields to handle value updates.
+ *
+ * @param valueAtom the field value atom
+ * @param options additional options
+ * @returns
+ */
+export function useInput<T>(
+  valueAtom: ValueAtom<T>,
+  options: {
+    /**
+     * The default value to use when the atom value is null or undefined.
+     */
+    defaultValue: T;
+
+    /**
+     * When to fire the onChange event.
+     */
+    onChangeTrigger?: "blur" | "change";
+  }
+) {
+  const { onChangeTrigger = "blur", defaultValue } = options;
+  const value = useAtomValue(valueAtom) || defaultValue;
+  const setValue = useSetAtom(valueAtom);
+  const [changed, setChanged] = useState(false);
+
+  const update = useCallback(
+    (text: string, fireOnChange = false) => {
+      const value = text as T;
+      setValue(value, fireOnChange);
+    },
+    [setValue]
+  );
+
+  const onChange = useCallback<React.ChangeEventHandler<HTMLInputElement>>(
+    (e) => {
+      update(e.target.value, onChangeTrigger === "change");
+      setChanged(true);
+    },
+    [onChangeTrigger, update]
+  );
+
+  const onBlur = useCallback<React.FocusEventHandler<HTMLInputElement>>(
+    (e) => {
+      if (changed && onChangeTrigger === "blur") {
+        setChanged(false);
+        update(e.target.value, true);
+      }
+    },
+    [changed, onChangeTrigger, update]
+  );
+
+  return {
+    value,
+    setValue,
+    onChange,
+    onBlur,
+  } as const;
 }
--- axelor-front/src/views/form/widgets/string/string.tsx
+++ axelor-front/src/views/form/widgets/string/string.tsx
@@ -1,9 +1,9 @@
-import { useAtom, useAtomValue } from "jotai";
-import { useCallback, useState } from "react";
+import { useAtomValue } from "jotai";
 
 import { Input } from "@axelor/ui";
 
 import { FieldContainer, FieldProps } from "../../builder";
+import { useInput } from "../../builder/hooks";
 
 export function String({
   schema,
@@ -16,26 +16,9 @@ export function String({
   const { attrs } = useAtomValue(widgetAtom);
   const { required } = attrs;
 
-  const [value, setValue] = useAtom(valueAtom);
-  const [changed, setChanged] = useState(false);
-
-  const handleChange = useCallback<React.ChangeEventHandler<HTMLInputElement>>(
-    (e) => {
-      setValue(e.target.value);
-      setChanged(true);
-    },
-    [setValue]
-  );
-
-  const handleBlur = useCallback<React.FocusEventHandler<HTMLInputElement>>(
-    (e) => {
-      if (changed) {
-        setChanged(false);
-        setValue(e.target.value, true);
-      }
-    },
-    [changed, setValue]
-  );
+  const { value, onChange, onBlur } = useInput(valueAtom, {
+    defaultValue: "",
+  });
 
   return (
     <FieldContainer readonly={readonly}>
@@ -56,8 +39,8 @@ export function String({
           id={uid}
           value={value || ""}
           required={required}
-          onChange={handleChange}
-          onBlur={handleBlur}
+          onChange={onChange}
+          onBlur={onBlur}
         />
       )}
     </FieldContainer>
