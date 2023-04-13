bffac6e28b76cd9b39dcc30014a2e1ecfb020380
Mayank Mahadevwala <m.mahadevwala@axelor.com>
2023-04-13 19:06:49 +0530

    Implement phone form widget

---
+++ axelor-front/src/views/form/widgets/phone/phone.tsx
@@ -0,0 +1,30 @@
+import { useAtomValue } from "jotai";
+import { Box } from "@axelor/ui";
+import { String } from "../string";
+import { FieldContainer, FieldProps } from "../../builder";
+
+export function Phone(props: FieldProps<string>) {
+  const {
+    schema: { uid, showTitle = true },
+    readonly,
+    widgetAtom,
+    valueAtom,
+  } = props;
+  const value = useAtomValue(valueAtom);
+  const {
+    attrs: { title },
+  } = useAtomValue(widgetAtom);
+  if (readonly) {
+    return (
+      value && (
+        <FieldContainer readonly={readonly}>
+          {showTitle && <label htmlFor={uid}>{title}</label>}
+          <Box as="a" target="_blank" href={`tel:${value}`}>
+            {value}
+          </Box>
+        </FieldContainer>
+      )
+    );
+  }
+  return <String {...props} inputProps={{ type: "tel" }} />;
+}
