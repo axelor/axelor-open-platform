e95509b6d6777f594b740b88bc3a7d6b5eb0697b
Amit Mendapara <amit@axelor.com>
2023-04-13 19:33:55 +0530

    Improve GridLayout
    Add support for colWidths
    Add support for gap

--- axelor-front/src/views/form/builder/form-layouts.tsx
+++ axelor-front/src/views/form/builder/form-layouts.tsx
@@ -2,10 +2,38 @@ import clsx from "clsx";
 
 import { Schema } from "@/services/client/meta.types";
 
-import { FormLayout } from "./types";
 import { FormWidget } from "./form-widget";
+import { FormLayout } from "./types";
 
+import { useMemo } from "react";
 import styles from "./form-layouts.module.scss";
+
+function computeCols(cols: number, colWidths?: string | (string | number)[]) {
+  const widths: string[] = [];
+  if (typeof colWidths === "string") colWidths.split(",");
+  if (Array.isArray(colWidths)) {
+    for (const width of colWidths) {
+      const w = String(width).trim();
+      if (w === "*") {
+        widths.push("1fr");
+      } else if (/^(\d+(.\d+)?)$/.test(w)) {
+        widths.push(`${w}px`);
+      } else {
+        widths.push(w);
+      }
+    }
+  }
+
+  if (widths.length === 0) {
+    return undefined;
+  }
+
+  while (widths.length < cols) {
+    widths.push("1fr");
+  }
+
+  return widths.join(" ");
+}
 
 export const GridLayout: FormLayout = ({
   schema,
@@ -13,9 +41,22 @@ export const GridLayout: FormLayout = ({
   className,
   readonly,
 }) => {
-  const { cols, items = [] } = schema;
+  const { cols, colWidths, gap, items = [] } = schema;
+  const widths = useMemo(() => computeCols(cols, colWidths), [cols, colWidths]);
+  const style = {
+    "--grid-cols": widths,
+    "--grid-gap": gap,
+  } as React.CSSProperties;
+
   return (
-    <div className={clsx(className, styles.grid)} data-cols={cols}>
+    <div
+      className={clsx(className, styles.grid, {
+        [styles.table]: schema.layout === "table",
+      })}
+      data-cols={widths ? undefined : cols}
+      data-col-widths={widths}
+      style={style}
+    >
       {items.map((item) => (
         <GridItem key={item.uid} schema={item}>
           <FormWidget schema={item} formAtom={formAtom} readonly={readonly} />
