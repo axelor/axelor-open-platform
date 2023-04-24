d3aa03042eb01305ad0cbf234b9052a544727a59
Mayank Mahadevwala <m.mahadevwala@axelor.com>
2023-04-24 15:50:32 +0530

    Fix check context showRecord in form view

--- axelor-front/src/views/form/form.tsx
+++ axelor-front/src/views/form/form.tsx
@@ -74,7 +74,7 @@ export function Form(props: ViewProps<FormView>) {
 
   const readonly =
     action.params?.forceReadonly ?? viewProps.readonly ?? Boolean(id);
-  const recordId = id ?? action.context?._showRecord;
+  const recordId = id || action.context?._showRecord;
   const popupRecord = action.params?.["_popup-record"];
 
   const { state, data: record = {} } = useAsync(async () => {
