057c603a46eeffcfaea539ff93a92f38ac5791b1
Amit Mendapara <amit@axelor.com>
2023-04-24 14:59:29 +0530

    Add support to show/hide tabs of panel-tabs

--- axelor-front/src/views/form/widgets/panel-tabs/panel-tabs.tsx
+++ axelor-front/src/views/form/widgets/panel-tabs/panel-tabs.tsx
@@ -1,6 +1,11 @@
-import { Box, NavItemProps, NavTabs as Tabs } from "@axelor/ui";
+import { useAtomValue } from "jotai";
 import { useCallback, useEffect, useMemo, useState } from "react";
-import { FormWidget, WidgetProps } from "../../builder";
+
+import { Box, NavItemProps, NavTabs as Tabs } from "@axelor/ui";
+
+import { Schema } from "@/services/client/meta.types";
+import { FormAtom, FormWidget, WidgetProps } from "../../builder";
+import { createWidgetAtom } from "../../builder/atoms";
 
 function TabContent({
   schema,
@@ -21,9 +26,32 @@ function TabContent({
   );
 }
 
+function Tab(
+  props: NavItemProps & {
+    schema?: Schema;
+    formAtom?: FormAtom;
+  }
+) {
+  const schema = props.schema!;
+  const formAtom = props.formAtom!;
+
+  const widgetAtom = useMemo(
+    () => createWidgetAtom({ schema, formAtom }),
+    [formAtom, schema]
+  );
+  const { attrs } = useAtomValue(widgetAtom);
+  const { title, hidden } = attrs;
+
+  if (hidden) {
+    return null;
+  }
+
+  return <div>{title}</div>;
+}
+
 export function PanelTabs(props: WidgetProps) {
   const { schema, formAtom, readonly } = props;
-  const { items } = schema;
+  const { items = [] } = schema;
   const [activeTab, setActiveTab] = useState<string | null>(null);
 
   const handleChange = useCallback(function handleChange(
@@ -35,11 +63,13 @@ export function PanelTabs(props: WidgetProps) {
   []);
 
   const tabItems = useMemo(() => {
-    return items!.map((item, ind) => ({
+    return items.map((item) => ({
       ...item,
-      id: item.name || `tab_item_${ind}`,
+      schema: item,
+      formAtom,
+      id: item.uid,
     })) as NavItemProps[];
-  }, [items]);
+  }, [formAtom, items]);
 
   useEffect(() => {
     const [firstItem] = tabItems;
@@ -48,12 +78,17 @@ export function PanelTabs(props: WidgetProps) {
 
   return (
     <Box d="flex" flexDirection="column">
-      <Tabs items={tabItems!} value={activeTab!} onChange={handleChange} />
-      {tabItems.map((item, index) => {
+      <Tabs
+        items={tabItems}
+        value={activeTab ?? undefined}
+        onChange={handleChange}
+        onItemRender={Tab}
+      />
+      {tabItems.map((item) => {
         const active = activeTab === item.id;
         return (
           <TabContent
-            key={item.id || index}
+            key={item.id}
             schema={item}
             active={active}
             formAtom={formAtom}
