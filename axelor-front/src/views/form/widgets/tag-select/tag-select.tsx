f1a90d0d9f8e5f2e61722c83de47953118476caa
Mayank Mahadevwala <m.mahadevwala@axelor.com>
2023-04-29 23:41:54 +0530

    Implement create option in tag-select widget
    - Add create... option to open new record form
    - Add create and select option to dynamic create record
    - Add create with input to show record form with pre-defined values

--- axelor-front/src/views/form/widgets/tag-select/tag-select.tsx
+++ axelor-front/src/views/form/widgets/tag-select/tag-select.tsx
@@ -1,13 +1,15 @@
 import { useAtom, useAtomValue } from "jotai";
-import { MouseEvent, useCallback, useMemo } from "react";
+import { MouseEvent, useCallback, useMemo, useState } from "react";
 import { Box, Select, SelectProps } from "@axelor/ui";
 
-import { useCompletion, useEditor } from "@/hooks/use-relation";
+import { EditorOptions, useCompletion, useEditor } from "@/hooks/use-relation";
 import { DataRecord } from "@/services/client/data.types";
 
 import { FieldContainer, FieldProps } from "../../builder";
 import { Chip } from "../selection";
 import { Schema } from "@/services/client/meta.types";
+import { i18n } from "@/services/client/i18n";
+import { DataStore } from "@/services/client/data-store";
 
 export function TagSelectComponent({
   schema,
@@ -43,6 +45,12 @@ export function TagSelectComponent({
   );
 }
 
+const ADD_ON_OPTIONS = {
+  Create: -1,
+  CreateInput: -2,
+  CreateAndSelectInput: -3,
+} as const;
+
 export function TagSelect(props: FieldProps<DataRecord[]>) {
   const { schema, valueAtom, widgetAtom, readonly } = props;
   const {
@@ -54,12 +62,19 @@ export function TagSelect(props: FieldProps<DataRecord[]>) {
     showTitle = true,
   } = schema;
 
-  const {
-    attrs: { title },
-  } = useAtomValue(widgetAtom);
-  const [value, setValue] = useAtom(valueAtom);
+  const { attrs } = useAtomValue(widgetAtom);
 
+  const [value, setValue] = useAtom(valueAtom);
+  const [inputText, setInputText] = useState("");
   const showEditor = useEditor();
+
+  const { title } = attrs;
+  const canNew = schema.canNew !== false;
+  const optionLabel = targetName;
+  const createNames = useMemo<string[]>(
+    () => (schema.create ?? "")?.split(/\s*,\s*/),
+    [schema.create]
+  );
 
   const search = useCompletion({
     target,
@@ -67,17 +82,18 @@ export function TagSelect(props: FieldProps<DataRecord[]>) {
     targetSearch,
   });
 
-  const handleChange = (value: any) => {
-    setValue(value);
-  };
-
   const handleEdit = useCallback(
-    async (value: DataRecord, readonly = false) => {
-      showEditor({
+    async (
+      value: DataRecord,
+      readonly = false,
+      onSelect?: EditorOptions["onSelect"]
+    ) => {
+      return showEditor({
         title: title ?? "",
         model: target,
         record: value,
         readonly,
+        onSelect,
       });
     },
     [showEditor, target, title]
@@ -99,6 +115,61 @@ export function TagSelect(props: FieldProps<DataRecord[]>) {
     },
     [search]
   );
+
+  const handleChange = useCallback(
+    async (value: any[]) => {
+      const addOnOption = value?.find?.((v) =>
+        Object.values(ADD_ON_OPTIONS).includes(v?.id)
+      );
+      if (addOnOption) {
+        const record: DataRecord = {};
+        const updateValue = (record: DataRecord) =>
+          setValue(value.map((v) => (v === addOnOption ? record : v)));
+        if (
+          [
+            ADD_ON_OPTIONS.CreateInput,
+            ADD_ON_OPTIONS.CreateAndSelectInput,
+          ].includes(addOnOption.id)
+        ) {
+          createNames?.forEach((name) => {
+            record[name] = addOnOption._text;
+          });
+        }
+
+        if (addOnOption.id === ADD_ON_OPTIONS.CreateAndSelectInput) {
+          const ds = new DataStore(target);
+          const newRecord = await ds.save(record);
+          newRecord && updateValue(newRecord);
+        } else {
+          await handleEdit(record, false, updateValue);
+        }
+      } else {
+        setValue(value);
+      }
+    },
+    [target, createNames, handleEdit, setValue]
+  );
+
+  const addOnOptions = useMemo(() => {
+    if (!canNew) return [];
+    if (inputText && createNames.length) {
+      return [
+        {
+          id: ADD_ON_OPTIONS.CreateInput,
+          _text: inputText,
+          [optionLabel]: i18n.get('Create "{0}"...', inputText),
+        },
+        {
+          id: ADD_ON_OPTIONS.CreateAndSelectInput,
+          _text: inputText,
+          [optionLabel]: i18n.get('Create "{0}" and select...', inputText),
+        },
+      ];
+    }
+    return [
+      { id: ADD_ON_OPTIONS.Create, [optionLabel]: i18n.get("Create...") },
+    ];
+  }, [canNew, optionLabel, createNames, inputText]);
 
   return (
     <FieldContainer>
@@ -124,6 +195,12 @@ export function TagSelect(props: FieldProps<DataRecord[]>) {
           value={value}
           fetchOptions={handleCompletion}
           placeholder={placeholder}
+          {...(canNew && {
+            addOnOptions,
+          })}
+          {...(createNames.length > 0 && {
+            onInputChange: setInputText,
+          })}
         />
       )}
     </FieldContainer>
