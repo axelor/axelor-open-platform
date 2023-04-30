a4ca8fd11eb014b2037acc7ceeab5f650b94b717
Amit Mendapara <amit@axelor.com>
2023-04-30 06:30:22 +0530

    Restructure legacy styles

--- axelor-front/src/styles/legacy.ts
+++ axelor-front/src/styles/legacy.ts
@@ -1,23 +1,5 @@
-import { clsx, type ClassValue } from "clsx";
+import { cssx, type ClassValue } from "@axelor/ui";
 import styles from "./legacy.module.scss";
 
-const clean = (names: string[]) =>
-  names.flatMap((n) => n.trim().split(/\s+/)).filter(Boolean);
-
-const names = (item: ClassValue): string[] => {
-  if (Array.isArray(item)) return item.flatMap(names);
-  if (item && typeof item === "object") {
-    let items: string[] = [];
-    for (let k in item) {
-      if (item[k]) {
-        items.push(k);
-      }
-    }
-    return clean(items);
-  }
-  return item ? clean([item.toString()]) : [];
-};
-
-export const legacyClassNames = (...input: ClassValue[]): string => {
-  return clsx(names(input).map((name) => styles[name] ?? name));
-};
+export const legacyClassNames = (...input: ClassValue[]) =>
+  cssx(styles, ...input);
