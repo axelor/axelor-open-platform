90ea0df2f965bf8a9321a47beb3246354446f3f2
Amit Mendapara <amit@axelor.com>
2023-04-30 09:07:16 +0530

    Responsive sidebar

--- axelor-front/src/layout/nav-drawer/hook.ts
+++ axelor-front/src/layout/nav-drawer/hook.ts
@@ -1,9 +1,29 @@
+import { useMediaQuery } from "@/hooks/use-media-query";
 import { atom, useAtom } from "jotai";
+import { useCallback } from "react";
 
-const sidebarAtom = atom<boolean>(true);
+const sidebarAtom = atom<{ small?: boolean; active?: boolean }>({});
 
 export function useSidebar() {
-  const [sidebar, setSidebar] = useAtom(sidebarAtom);
+  const small = useMediaQuery("(max-width: 768px)");
+  const [state, setState] = useAtom(sidebarAtom);
+
+  const setSidebar = useCallback(
+    (active: boolean) => {
+      setState({ small, active });
+    },
+    [setState, small]
+  );
+
+  let sidebar = state.active ?? !small;
+  if (small && state.small) {
+    sidebar = state?.active ?? false;
+  } else if (state.small) {
+    sidebar = true;
+  } else if (small) {
+    sidebar = false;
+  }
+
   return {
     sidebar,
     setSidebar,
