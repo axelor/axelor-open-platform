678361db5ca93bef73865df6969627693be041c5
Amit Mendapara <amit@axelor.com>
2023-05-09 22:42:24 +0530

    Fix static widget

--- axelor-front/src/views/form/widgets/static/static.tsx
+++ axelor-front/src/views/form/widgets/static/static.tsx
@@ -1,11 +1,9 @@
 import { useTemplate } from "@/hooks/use-parser";
 import { useAtomValue } from "jotai";
-import { useMemo } from "react";
 import { FieldProps } from "../../builder";
 
-export function Static({ formAtom, valueAtom }: FieldProps<string>) {
-  const value = useAtomValue(valueAtom);
-  const text = useMemo(() => value || "", [value]);
+export function Static({ schema, formAtom }: FieldProps<string>) {
+  const { text } = schema;
   const { record } = useAtomValue(formAtom);
   const Template = useTemplate(text);
   return (
