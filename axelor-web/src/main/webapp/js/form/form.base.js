b8a669ea40b9c749b836afbb6c39b1fa45c627dd
Pierre Belloy <p.belloy@axelor.com>
2023-06-12 16:54:24 +0200

    Fix onChange on Enter key in simple fields
    Fixes RM-65153
    
    Signed-off-by: Pierre Belloy <p.belloy@axelor.com>

--- axelor-web/src/main/webapp/js/form/form.base.js
+++ axelor-web/src/main/webapp/js/form/form.base.js
@@ -711,6 +711,10 @@ var FormInput = {
         }
       });
 
+      scope.$on("on:edit", function() {
+        oldValue = getInputValue();
+      });
+
       function getInputValue() {
         var childInput = element.find('input:first');
         var valInput = childInput.length && childInput || element;
