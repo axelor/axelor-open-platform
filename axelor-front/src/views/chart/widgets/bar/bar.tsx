757f41d79c295c69ecd58440b6cb19d9e0c3727a
Mayank Mahadevwala <m.mahadevwala@axelor.com>
2023-06-12 19:02:23 +0530

    Remove labels from bar in bar charts

--- axelor-front/src/views/chart/widgets/bar/bar.tsx
+++ axelor-front/src/views/chart/widgets/bar/bar.tsx
@@ -28,22 +28,12 @@ export function Bar({ data, ...rest }: ChartProps) {
 
   useEffect(() => {
     const { types: dimensions, data: source, formatter } = PlusData(data);
-    const isVertical = (source || []).some((s) => s.raw && s.raw.length > 1);
     setOptions(
       produce((draft: any) => {
         applyTitles(draft, data);
         draft.series = dimensions.map((key) => ({
           type: "bar",
           stack: type === "stack" ? "all" : `${key}`,
-          ...(isVertical && type === "stack"
-            ? {}
-            : {
-                label: {
-                  show: true,
-                  position: "top",
-                  formatter: (v: any) => formatter(v.data[v.seriesName]),
-                },
-              }),
         }));
         draft.tooltip.valueFormatter = formatter;
         draft.dataset.dimensions = ["x", ...dimensions];
