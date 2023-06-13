b88550c16974a6219a0c24ac4eba676451a0e5e9
Mayank Mahadevwala <m.mahadevwala@axelor.com>
2023-06-13 19:22:35 +0530

    Fix viewer link styling
    Fixes #124

--- axelor-front/src/views/form/widgets/string/viewer.tsx
+++ axelor-front/src/views/form/widgets/string/viewer.tsx
@@ -41,8 +41,9 @@ export function ViewerLink({
   return (
     <Button
       variant="link"
-      alignSelf="start"
+      textAlign="start"
       textDecoration="none"
+      overflow="hidden"
       onClick={onClick}
     >
       {children}
