587fe45f255c5001bae9b6a2c917397a6a8f4093
Amit Mendapara <amit@axelor.com>
2023-06-27 23:50:22 +0200

    Don't merge light theme with dark

--- axelor-front/src/hooks/use-app-theme/use-app-theme-options.ts
+++ axelor-front/src/hooks/use-app-theme/use-app-theme-options.ts
@@ -1,20 +1,16 @@
+import { ThemeOptions } from "@axelor/ui/core/styles/theme/types";
+
 import { request } from "@/services/client/client";
 import { LoadingCache } from "@/utils/cache";
-import { ThemeOptions } from "@axelor/ui/core/styles/theme/types";
-import merge from "lodash/merge";
-import { useAppTheme } from ".";
+
 import { useAsync } from "../use-async";
+import { useAppTheme } from "./use-app-theme";
 
 const cache = new LoadingCache<Promise<ThemeOptions>>();
 
 const load = async (theme: "light" | "dark") => {
-  const files =
-    theme === "dark"
-      ? [`theme/light.json`, `theme/dark.json`]
-      : [`theme/light.json`];
-  return Promise.all(
-    files.map((url) => request({ url }).then((resp) => resp.json()))
-  ).then((themes) => merge({}, ...themes));
+  const url = theme === "dark" ? `theme/dark.json` : `theme/light.json`;
+  return request({ url }).then((resp) => resp.json());
 };
 
 export function useAppThemeOption() {
