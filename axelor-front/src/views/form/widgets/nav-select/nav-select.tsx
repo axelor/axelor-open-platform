343bfc98f04fa3b1e3be6bc1502396c64b205df8
Amit Mendapara <amit@axelor.com>
2023-07-05 20:23:53 +0530

    Improve NavSelect widget
    - use OverflowList component
    - always show selected item if possible

--- axelor-front/src/views/form/widgets/nav-select/nav-select.tsx
+++ axelor-front/src/views/form/widgets/nav-select/nav-select.tsx
@@ -1,15 +1,11 @@
 import { useAtom } from "jotai";
-import { forwardRef, useCallback, useMemo, useState } from "react";
+import { useCallback, useMemo } from "react";
 
 import {
-  Menu,
-  MenuItem,
-  Overflow,
-  OverflowItem,
+  OverflowList,
+  OverflowListItemProps,
+  OverflowMenuTriggerProps,
   clsx,
-  useIsOverflowItemVisible,
-  useOverflowMenu,
-  useRefs,
 } from "@axelor/ui";
 
 import { Selection } from "@/services/client/meta.types";
@@ -24,90 +20,34 @@ type SelectItem = {
   selection: Selection;
 };
 
-type ItemProps = {
-  item: SelectItem;
-  onClick: (item: SelectItem) => void;
-  className?: string;
-};
+type ItemProps = OverflowListItemProps<SelectItem>;
 
-type ItemsProps = {
-  items: SelectItem[];
-  onClick: (item: SelectItem) => void;
-};
-
-const Item = forwardRef<HTMLDivElement, ItemProps>((props, ref) => {
-  const { item, onClick, className } = props;
+function Item(props: ItemProps) {
+  const { item, active } = props;
   const { selection } = item;
-  const handleClick = useCallback(() => onClick(item), [item, onClick]);
   return (
-    <div
-      ref={ref}
-      className={clsx(styles.item, className)}
-      onClick={handleClick}
-    >
+    <div className={clsx(styles.item, [{ [styles.active]: active }])}>
       <div className={styles.text}>{selection.title}</div>
     </div>
   );
-});
-
-function OverflowMenuItem({ item, onClick }: ItemProps) {
-  const { id, selection } = item;
-  const isVisible = useIsOverflowItemVisible(id);
-  const handleClick = useCallback(() => onClick(item), [item, onClick]);
-
-  if (isVisible) {
-    return null;
-  }
-
-  return <MenuItem onClick={handleClick}>{selection.title}</MenuItem>;
 }
 
-function OverflowMenu({ items, onClick }: ItemsProps) {
-  const { ref, isOverflowing, overflowCount } =
-    useOverflowMenu<HTMLDivElement>();
-
-  const [show, setShow] = useState(false);
-  const [target, setTarget] = useState<HTMLDivElement | null>(null);
-
-  const iconRef = useRefs(ref, (el: HTMLDivElement | null) => setTarget(el));
-
-  const showMenu = useCallback(() => setShow(true), []);
-  const hideMenu = useCallback(() => setShow(false), []);
-
-  const handleClick = useCallback(
-    (item: SelectItem) => {
-      onClick(item);
-      setShow(false);
-    },
-    [onClick]
-  );
-
-  if (!isOverflowing) {
-    return null;
-  }
-
+function MenuTrigger({ count, open }: OverflowMenuTriggerProps) {
   return (
-    <div className={styles.itemWrapper}>
-      <Item
-        ref={iconRef}
-        onClick={() => showMenu()}
-        className={clsx(styles.more, {
-          [styles.open]: show,
-        })}
-        item={{
-          id: "",
-          selection: {
-            title: `+${overflowCount}`,
-          },
-        }}
-      ></Item>
-      <Menu target={target} show={show} onHide={hideMenu} navigation>
-        {items.map((item) => (
-          <OverflowMenuItem key={item.id} item={item} onClick={handleClick} />
-        ))}
-      </Menu>
+    <div className={clsx(styles.item, [{ [styles.open]: open }])}>
+      <div className={styles.text}>+{count}</div>
     </div>
   );
+}
+
+function MenuItem({ item }: ItemProps) {
+  const { selection } = item;
+  return <>{selection.title}</>;
+}
+
+function isSelected(selection: Selection, value: any) {
+  const val = selection.value;
+  return val === value || String(val) === String(value);
 }
 
 export function NavSelect(props: FieldProps<string | number>) {
@@ -117,47 +57,38 @@ export function NavSelect(props: FieldProps<string | number>) {
   const selection = useSelectionList({ value, widgetAtom, schema });
   const items: SelectItem[] = useMemo(() => {
     return selection.map((selection, i) => {
-      return { id: String(selection.title ?? i), selection };
+      return {
+        id: String(selection.title ?? i),
+        selection,
+        priority: isSelected(selection, value) ? 1 : 0,
+      };
     });
-  }, [selection]);
+  }, [selection, value]);
 
   const onItemClick = useCallback(
-    (item: SelectItem) => {
+    ({ selection }: SelectItem) => {
       if (readonly) return;
-      const { selection } = item;
       setValue(selection.value, true);
     },
     [readonly, setValue]
   );
 
-  const isActive = useCallback(
-    (selection: Selection) => {
-      const val = selection.value;
-      return val === value || String(val) === String(value);
-    },
+  const isItemActive = useCallback(
+    ({ selection }: SelectItem) => isSelected(selection, value),
     [value]
   );
 
   return (
     <FieldControl {...props}>
-      <Overflow>
-        <div className={styles.container}>
-          {items.map((item) => (
-            <OverflowItem key={item.id} id={item.id}>
-              <div className={styles.itemWrapper}>
-                <Item
-                  item={item}
-                  onClick={onItemClick}
-                  className={clsx({
-                    [styles.active]: isActive(item.selection),
-                  })}
-                />
-              </div>
-            </OverflowItem>
-          ))}
-          <OverflowMenu items={items} onClick={onItemClick} />
-        </div>
-      </Overflow>
+      <OverflowList
+        className={styles.container}
+        items={items}
+        isItemActive={isItemActive}
+        onItemClick={onItemClick}
+        renderItem={Item}
+        renderMenuTrigger={MenuTrigger}
+        renderMenuItem={MenuItem}
+      />
     </FieldControl>
   );
 }
