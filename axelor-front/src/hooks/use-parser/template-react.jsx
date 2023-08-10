3bb90fb46ffc9a265ff38722f74e8e91387da260
Amit Mendapara <amit@axelor.com>
2023-08-10 16:57:59 +0530

    Add support for UI components in react templates
    Following components are supported:
    
    - `Box`
    - `Badge`
    - `Alert`
    - `AlertHeader`
    - `AlertLink`
    - `Button`
    - `ButtonGroup`
    - `Divider`
    - `Image`
    - `Link`
    - `List`
    - `ListItem`
    - `Panel`
    - `Popper`
    - `Scrollable`
    - `Table`
    - `TableBody`
    - `TableHead`
    - `TableFoot`
    - `TableRow`
    - `TableCell`
    - `TableCaption`

--- axelor-front/src/hooks/use-parser/template-react.jsx
+++ axelor-front/src/hooks/use-parser/template-react.jsx
@@ -1,9 +1,72 @@
+import {
+  Alert,
+  AlertHeader,
+  AlertLink,
+  Badge,
+  Box,
+  Button,
+  ButtonGroup,
+  Divider,
+  Image,
+  Link,
+  List,
+  ListItem,
+  Panel,
+  Popper,
+  Scrollable,
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFoot,
+  TableHead,
+  TableRow,
+} from "@axelor/ui";
+
+import { useMemo } from "react";
 import { parseSafe } from "./parser";
+
+const COMPONENTS = {
+  Box,
+  Badge,
+  Alert,
+  AlertHeader,
+  AlertLink,
+  Button,
+  ButtonGroup,
+  Divider,
+  Image,
+  Link,
+  List,
+  ListItem,
+  Panel,
+  Popper,
+  Scrollable,
+  Table,
+  TableBody,
+  TableHead,
+  TableFoot,
+  TableRow,
+  TableCell,
+  TableCaption,
+};
+
+function createContext(context) {
+  return new Proxy(context, {
+    get(target, p, receiver) {
+      return p in COMPONENTS ? COMPONENTS[p] : Reflect.get(target, p, receiver);
+    },
+    set(target, p, newValue, receiver) {
+      Reflect.set(target, p, newValue);
+    },
+  });
+}
 
 export function processReactTemplate(template) {
   const render = parseSafe(template);
   const ReactComponent = ({ context }) => {
-    return render(context);
+    const ctx = useMemo(() => createContext(context), [context]);
+    return render(ctx);
   };
   return ReactComponent;
 }
