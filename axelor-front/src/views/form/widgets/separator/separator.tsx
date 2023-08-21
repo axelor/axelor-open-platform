9f0e2a77b9691f45fa823518ffe1a2c73b69610a
Mayank Mahadevwala <m.mahadevwala@axelor.com>
2023-08-21 17:59:36 +0530

    Fix remove showTitle check for separator widget
    Fixes #293

--- axelor-front/src/views/form/widgets/separator/separator.tsx
+++ axelor-front/src/views/form/widgets/separator/separator.tsx
@@ -5,10 +5,9 @@ import { WidgetProps } from "../../builder";
 export function Separator({ schema, widgetAtom }: WidgetProps) {
   const { attrs } = useAtomValue(widgetAtom);
   const { title } = attrs;
-  const showTitle = title && schema.showTitle !== false;
   return (
     <Box>
-      {showTitle && <Box fontWeight="bold">{title}</Box>}
+      {title && <Box fontWeight="bold">{title}</Box>}
       <Box as="hr" my={2} />
     </Box>
   );
