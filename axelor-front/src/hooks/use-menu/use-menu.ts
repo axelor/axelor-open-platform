366c57b4e3a67fd69ce6c8db78975ea88580dd39
Mayank Mahadevwala <m.mahadevwala@axelor.com>
2023-08-25 13:36:54 +0530

    Improve use menu hook
    Fix multiple fetch menus request issue caused by multiple usage of useMenu hook

--- axelor-front/src/hooks/use-menu/use-menu.ts
+++ axelor-front/src/hooks/use-menu/use-menu.ts
@@ -1,28 +1,40 @@
+import { atom, useAtomValue } from "jotai";
+import { useAtomCallback } from "jotai/utils";
+import { useCallback } from "react";
+
 import * as meta from "@/services/client/meta";
 import { MenuItem } from "@/services/client/meta.types";
-import { atom, useAtom } from "jotai";
-import { useState } from "react";
 import { useAsyncEffect } from "../use-async-effect";
 
 const menusAtom = atom<MenuItem[]>([]);
+const loadingAtom = atom<boolean | null>(null);
 
 export function useMenu() {
-  const [menus, setMenus] = useAtom(menusAtom);
-  const [loading, setLoading] = useState(false);
+  const menus = useAtomValue(menusAtom);
+  const loading = useAtomValue(loadingAtom);
+
+  const fetchMenus = useAtomCallback(
+    useCallback(async (get, set) => {
+      const loading = get(loadingAtom);
+      const menus = get(menusAtom);
+      if (loading || menus.length) return;
+
+      set(loadingAtom, true);
+      try {
+        const res = await meta.menus("all");
+        set(menusAtom, res ?? []);
+      } finally {
+        set(loadingAtom, false);
+      }
+    }, [])
+  );
 
   useAsyncEffect(async () => {
-    if (loading || menus.length) return;
-    setLoading(true);
-    try {
-      const res = await meta.menus("all");
-      setMenus(res ?? []);
-    } finally {
-      setLoading(false);
-    }
-  }, []);
+    await fetchMenus();
+  }, [fetchMenus]);
 
   return {
     menus,
-    loading,
+    loading: loading ?? true,
   };
 }
