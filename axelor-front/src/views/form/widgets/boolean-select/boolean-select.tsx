eec31291b18f622d2f2370e726a9bdec39dc6f22
Amit Mendapara <amit@axelor.com>
2023-09-09 07:11:41 +0530

    Re-implement BooleanSelect widget

--- axelor-front/src/views/form/widgets/boolean-select/boolean-select.tsx
+++ axelor-front/src/views/form/widgets/boolean-select/boolean-select.tsx
@@ -1,8 +1,7 @@
 import { useAtom, useAtomValue } from "jotai";
 import { useCallback, useMemo } from "react";
 
-import { Input, Select } from "@axelor/ui";
-
+import { Select, SelectOptionType } from "@/components/select";
 import { i18n } from "@/services/client/i18n";
 
 import { FieldControl, FieldProps } from "../../builder";
@@ -17,20 +16,21 @@ export function BooleanSelect(props: FieldProps<boolean | null>) {
   const { widgetAttrs, nullable } = schema;
   const {
     nullText = "\u00A0",
-    falseText = /*$$(*/ "No" /*)*/,
-    trueText = /*$$(*/ "Yes" /*)*/,
+    falseText = i18n.get("No"),
+    trueText = i18n.get("Yes"),
   } = widgetAttrs || {};
+
   const [value = nullable ? null : undefined, setValue] = useAtom(valueAtom);
 
   const {
-    attrs: { focus },
+    attrs: { focus, required },
   } = useAtomValue(widgetAtom);
 
   const handleOnChange = useCallback(
-    (option: SelectOption) => {
-      setValue(nullable && option.value === null ? null : option.value, true);
+    (option: SelectOptionType<SelectOption, false>) => {
+      setValue(nullable && option?.value === null ? null : option?.value, true);
     },
-    [nullable, setValue]
+    [nullable, setValue],
   );
 
   const options = useMemo<SelectOption[]>(
@@ -39,39 +39,27 @@ export function BooleanSelect(props: FieldProps<boolean | null>) {
       { title: i18n.get(trueText), value: true },
       { title: i18n.get(falseText), value: false },
     ],
-    [nullable, nullText, trueText, falseText]
+    [nullable, nullText, trueText, falseText],
   );
 
   const selected = options.find((option) => option.value === value);
 
   return (
     <FieldControl {...props}>
-      {readonly ? (
-        <Input
-          px={0}
-          type="text"
-          defaultValue={(selected?.value === null ? "" : selected?.title) || ""}
-          disabled
-          readOnly
-          bg="body"
-          border={false}
-        />
-      ) : (
-        <Select
-          {...(focus && { key: "focused" })}
-          autoFocus={focus}
-          invalid={invalid}
-          value={selected}
-          onChange={handleOnChange}
-          options={options}
-          optionLabel="title"
-          optionValue="value"
-          isSearchable={false}
-          isClearable={false}
-          isClearOnDelete={false}
-          icons={[{ icon: "arrow_drop_down" }]}
-        />
-      )}
+      <Select
+        autoComplete={false}
+        autoFocus={focus}
+        required={required}
+        readOnly={readonly}
+        invalid={invalid}
+        value={selected}
+        onChange={handleOnChange}
+        options={options}
+        optionKey={(x) => x.title}
+        optionLabel={(x) => x.title}
+        optionEqual={(x, y) => x.value === y.value}
+        clearIcon={false}
+      />
     </FieldControl>
   );
 }
