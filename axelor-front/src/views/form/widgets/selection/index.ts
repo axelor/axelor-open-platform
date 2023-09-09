3e5e45aa5f2fd188b791ebbc644ed351baefc247
Amit Mendapara <amit@axelor.com>
2023-09-09 07:11:13 +0530

    Re-implement Selection widget
    The Selection widget has been re-implemented using new Select
    component.

--- axelor-front/src/views/form/widgets/selection/index.ts
+++ axelor-front/src/views/form/widgets/selection/index.ts
@@ -1,5 +1,3 @@
 export * from "./selection";
 export * from "./selection-tag";
 export * from "./selection-text";
-export { Selection as Select } from "./selection";
-
--- axelor-front/src/views/form/widgets/selection/selection.tsx
+++ axelor-front/src/views/form/widgets/selection/selection.tsx
@@ -1,142 +1,95 @@
+import clsx from "clsx";
 import { useAtom, useAtomValue } from "jotai";
-import { FunctionComponent, useCallback, useMemo } from "react";
+import { useCallback, useMemo } from "react";
 
-import { Box, Select, SelectComponents, SelectProps } from "@axelor/ui";
-
-import { Schema } from "@/services/client/meta.types";
+import { Select, SelectOptionType, SelectProps } from "@/components/select";
+import { Selection as SelectionType } from "@/services/client/meta.types";
 
 import { FieldControl, FieldProps } from "../../builder";
 import { useSelectionList } from "./hooks";
-import { SelectionTag } from "./selection-tag";
-import { SelectionText } from "./selection-text";
 
-export function Selection({
-  selectComponents = [],
-  selectProps,
-  ...props
-}: FieldProps<string | number | null> & {
-  selectProps?: Partial<SelectProps>;
-  selectComponents?: SelectComponent[];
-}) {
-  const { schema, readonly, invalid, widgetAtom, valueAtom } = props;
-  const { placeholder } = schema;
-  const { isMulti } = selectProps || {};
+import styles from "./selection.module.scss";
+
+const optionKey = (item: SelectionType) => item.value!;
+const optionLabel = (item: SelectionType) => item.title!;
+const optionEqual = (a: SelectionType, b: SelectionType) => a.value === b.value;
+
+export type SelectionProps<Multiple extends boolean> = FieldProps<
+  string | number | null
+> &
+  Pick<
+    SelectProps<SelectionType, Multiple>,
+    "multiple" | "autoComplete" | "renderOption" | "renderValue"
+  >;
+
+export function Selection<Multiple extends boolean>(
+  props: SelectionProps<Multiple>,
+) {
   const {
-    attrs: { focus },
-  } = useAtomValue(widgetAtom);
+    schema,
+    readonly,
+    invalid,
+    multiple,
+    autoComplete = false,
+    renderOption,
+    renderValue,
+    widgetAtom,
+    valueAtom,
+  } = props;
+
   const [value, setValue] = useAtom(valueAtom);
+  const {
+    attrs: { required, focus },
+  } = useAtomValue(widgetAtom);
+
   const selectionList = useSelectionList({ schema, widgetAtom, value });
-
-  const hasValue = (value ?? null) !== null;
-  const selectValue = hasValue
-    ? (() => {
-        if (isMulti) {
-          const values = String(value ?? "").split(",");
-          return selectionList.filter((item) =>
-            values.includes(String(item.value)),
-          );
-        }
-        return selectionList.find(
-          (item) => String(item.value) === String(value),
-        );
-      })()
-    : null;
-
-  const handleClear = useCallback(() => {
-    setValue(null);
-  }, [setValue]);
+  const selectionValue = useMemo(() => {
+    if (multiple) {
+      const values = String(value ?? "").split(",");
+      return selectionList.filter((item) =>
+        values.includes(String(item.value)),
+      );
+    }
+    return selectionList.find((item) => String(item.value) === String(value));
+  }, [multiple, selectionList, value]) as SelectOptionType<
+    SelectionType,
+    Multiple
+  >;
 
   const handleChange = useCallback(
-    (e: any) => {
-      const value = isMulti
-        ? Array.isArray(e)
-          ? e.map((e) => e?.value).join(",")
-          : e
-        : e && e.value;
-      setValue(value, true);
+    (value: SelectOptionType<SelectionType, Multiple>) => {
+      let next: string | null = null;
+      if (value) {
+        next = Array.isArray(value)
+          ? value.map((x) => String(x.value)).join(",")
+          : value.value ?? null;
+      }
+      setValue(next, true);
     },
-    [isMulti, setValue],
+    [setValue],
   );
-
-  const components = useComponents(schema, ["Option", ...selectComponents]);
 
   return (
     <FieldControl {...props}>
-      {readonly && <SelectionText schema={schema} value={value} />}
-      {readonly || (
-        <Select
-          {...(focus && { key: "focused" })}
-          autoFocus={focus}
-          invalid={invalid}
-          value={selectValue ?? null}
-          onChange={handleChange}
-          options={selectionList}
-          optionLabel="title"
-          optionValue="value"
-          placeholder={placeholder}
-          icons={[
-            {
-              icon: "close",
-              hidden: !hasValue,
-              onClick: handleClear,
-            },
-            { icon: "arrow_drop_down" },
-          ]}
-          {...selectProps}
-          components={components}
-        />
-      )}
+      <Select
+        className={clsx({
+          [styles.readonly]: readonly,
+        })}
+        autoFocus={focus}
+        autoComplete={autoComplete}
+        multiple={multiple}
+        readOnly={readonly}
+        required={required}
+        invalid={invalid}
+        options={selectionList}
+        optionKey={optionKey}
+        optionLabel={optionLabel}
+        optionEqual={optionEqual}
+        value={selectionValue}
+        onChange={handleChange}
+        renderOption={renderOption}
+        renderValue={renderValue}
+      />
     </FieldControl>
   );
-}
-
-type SelectComponent = "Option" | "SingleValue" | "MultiValue";
-
-function useComponents(schema: Schema, componentList: SelectComponent[]) {
-  const { colorField = "color" } = schema;
-  const componentListAsStr = componentList.join(",");
-
-  return useMemo(() => {
-    const components: Record<SelectComponent, FunctionComponent<any>> = {
-      MultiValue: (props: any) => {
-        const { data, removeProps } = props;
-        return (
-          <Box me={1}>
-            <SelectionTag
-              color={data?.[colorField]}
-              title={data?.title}
-              onRemove={removeProps.onClick}
-            />
-          </Box>
-        );
-      },
-      SingleValue: (props: any) => {
-        const { data } = props;
-        return (
-          <SelectComponents.SingleValue {...props}>
-            <SelectionTag color={data?.[colorField]} title={data?.title} />
-          </SelectComponents.SingleValue>
-        );
-      },
-      Option: (props: any) => {
-        const { data } = props;
-        const color = data?.[colorField];
-        if (!color) {
-          return <SelectComponents.Option {...props} />;
-        }
-        return (
-          <SelectComponents.Option {...props}>
-            <SelectionTag color={color} title={data?.title} />
-          </SelectComponents.Option>
-        );
-      },
-    };
-    return componentListAsStr.split(",").reduce(
-      (obj, key) => ({
-        ...obj,
-        [key]: components[key as SelectComponent],
-      }),
-      {},
-    );
-  }, [colorField, componentListAsStr]);
 }
