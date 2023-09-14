636631543c1afbed7244e97f5f637ae6467c58f3
Phi-Long Do <p.do@axelor.com>
2023-09-14 16:43:47 +0200

    Fix static and label template eval

--- axelor-front/src/views/form/widgets/label/label.tsx
+++ axelor-front/src/views/form/widgets/label/label.tsx
@@ -1,15 +1,24 @@
 import { useTemplate } from "@/hooks/use-parser";
 import { useAtomValue } from "jotai";
 import { WidgetProps } from "../../builder";
+import { useFormField } from "../../builder/scope";
 
 export function Label({ formAtom, widgetAtom }: WidgetProps) {
   const { attrs } = useAtomValue(widgetAtom);
   const { title = "" } = attrs;
   const { record } = useAtomValue(formAtom);
   const Template = useTemplate(title);
+  const $getField = useFormField(formAtom);
   return (
     <label>
-      <Template context={record} />
+      <Template
+        context={record}
+        options={{
+          helpers: {
+            $getField,
+          },
+        }}
+      />
     </label>
   );
 }
--- axelor-front/src/views/form/widgets/static/static.tsx
+++ axelor-front/src/views/form/widgets/static/static.tsx
@@ -1,14 +1,23 @@
 import { useTemplate } from "@/hooks/use-parser";
 import { useAtomValue } from "jotai";
 import { FieldProps } from "../../builder";
+import { useFormField } from "../../builder/scope";
 
 export function Static({ schema, formAtom }: FieldProps<string>) {
   const { text } = schema;
   const { record } = useAtomValue(formAtom);
   const Template = useTemplate(text);
+  const $getField = useFormField(formAtom);
   return (
     <div>
-      <Template context={record} />
+      <Template
+        context={record}
+        options={{
+          helpers: {
+            $getField,
+          },
+        }}
+      />
     </div>
   );
 }
