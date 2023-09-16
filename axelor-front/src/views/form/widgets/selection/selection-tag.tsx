002feb53ce24dc09d4d7da186e2d9a4f3cccd3f0
Amit Mendapara <amit@axelor.com>
2023-09-16 16:00:42 +0200

    Improve selection tag
    Now uses badge variant for the given color if possible.

--- axelor-front/src/views/form/widgets/selection/selection-tag.tsx
+++ axelor-front/src/views/form/widgets/selection/selection-tag.tsx
@@ -1,10 +1,27 @@
-import { Badge, Box, clsx } from "@axelor/ui";
+import React from "react";
+
+import { Badge, Box, TVariant, clsx } from "@axelor/ui";
 import { MaterialIcon } from "@axelor/ui/icons/material-icon";
 
 import { legacyClassNames } from "@/styles/legacy";
 
 import styles from "./selection.module.scss";
-import React from "react";
+
+const VARIANTS: TVariant[] = [
+  "primary",
+  "secondary",
+  "success",
+  "danger",
+  "warning",
+  "info",
+  "light",
+  "dark",
+];
+
+const COLOR_MAPS: Record<string, TVariant> = {
+  white: "light",
+  black: "dark",
+};
 
 export function SelectionTag({
   title,
@@ -17,33 +34,39 @@ export function SelectionTag({
   className?: string;
   onRemove?: () => void;
 }) {
-  return (title && (
-    <Badge
-      px={2}
-      className={clsx(
-        styles["tag"],
-        className,
-        legacyClassNames(`hilite-${color}`),
-      )}
-    >
-      <Box as="span" className={styles["tag-text"]}>
-        {title}
-      </Box>
-      {onRemove && (
-        <Box
-          as="span"
-          className={styles["tag-remove"]}
-          onClick={(e) => {
-            if (onRemove) {
-              e.preventDefault();
-              e.stopPropagation();
-              onRemove();
-            }
-          }}
-        >
-          <MaterialIcon icon="close" fontSize="1rem" />
+  const variant = COLOR_MAPS[color!] ?? (color as TVariant);
+  const isVariant = variant && VARIANTS.includes(variant);
+  const colorProps = isVariant ? { variant } : undefined;
+  const colorClass = isVariant
+    ? undefined
+    : legacyClassNames(`hilite-${color}`);
+
+  if (title) {
+    return (
+      <Badge
+        {...colorProps}
+        px={2}
+        className={clsx(styles["tag"], className, colorClass)}
+      >
+        <Box as="span" className={styles["tag-text"]}>
+          {title}
         </Box>
-      )}
-    </Badge>
-  )) as React.ReactElement;
+        {onRemove && (
+          <Box
+            as="span"
+            className={styles["tag-remove"]}
+            onClick={(e) => {
+              if (onRemove) {
+                e.preventDefault();
+                e.stopPropagation();
+                onRemove();
+              }
+            }}
+          >
+            <MaterialIcon icon="close" fontSize="1rem" />
+          </Box>
+        )}
+      </Badge>
+    );
+  }
 }
