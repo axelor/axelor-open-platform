0e0e4409196c109c156db0cc323851a733d72fe2
Amit Mendapara <amit@axelor.com>
2023-09-16 15:14:47 +0200

    Update eslint config
    Add unused vars ignore pattern.

--- axelor-front/.eslintrc.cjs
+++ axelor-front/.eslintrc.cjs
@@ -15,7 +15,13 @@ module.exports = {
       { allowConstantExport: true },
     ],
     "@typescript-eslint/no-explicit-any": "warn",
-    "@typescript-eslint/no-unused-vars": "warn",
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      {
+        argsIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+      },
+    ],
     "@typescript-eslint/ban-types": "warn",
     "prefer-const": "warn",
   },
