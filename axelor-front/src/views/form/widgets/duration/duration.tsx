f852396f36da32fa3f8243d918b659ec45a252ae
Amit Mendapara <amit@axelor.com>
2023-09-25 16:51:32 +0200

    Fix duration widget validation issue
    Fixes ##373

--- axelor-front/src/views/form/widgets/duration/duration.tsx
+++ axelor-front/src/views/form/widgets/duration/duration.tsx
@@ -1,18 +1,19 @@
-import { useAtom, useAtomValue } from "jotai";
-import { useCallback, useMemo, useState } from "react";
+import { useAtomValue } from "jotai";
 import padStart from "lodash/padStart";
-import isString from "lodash/isString";
+import { useMemo } from "react";
 
-import { FieldControl, FieldProps } from "../../builder";
-import { ViewerInput } from "../string/viewer";
-import { MaskedInput } from "../date/mask-input";
-import { toKebabCase } from "@/utils/names";
 import { moment } from "@/services/client/l10n";
+import { toKebabCase } from "@/utils/names";
+import { FieldControl, FieldProps } from "../../builder";
+import { useInput } from "../../builder/hooks";
+import { MaskedInput } from "../date/mask-input";
+import { ViewerInput } from "../string/viewer";
 
-function getValue(
-  value: number | string,
-  { big = false, seconds = false } = {}
+function toText(
+  value?: number | string | null,
+  { big = false, seconds = false } = {},
 ) {
+  if (value === null || value === undefined) return "";
   const timestamp = parseInt(String(value));
   let [secs, mins, hrs] = [
     timestamp % 60,
@@ -21,7 +22,7 @@ function getValue(
   ];
 
   try {
-    if (value && isString(value)) {
+    if (typeof value === "string") {
       const d = moment(value);
       if (d.isValid()) {
         mins = d.minute();
@@ -29,7 +30,9 @@ function getValue(
         hrs = d.hour();
       }
     }
-  } catch {}
+  } catch {
+    // ignore
+  }
 
   return timestamp >= 0
     ? `${padStart(`${hrs}`, big ? 3 : 2, "0")}:${padStart(`${mins}`, 2, "0")}${
@@ -38,19 +41,18 @@ function getValue(
     : "";
 }
 
+const toNumber = (val: string | number) =>
+  (isNaN(val as number) ? 0 : val) as number;
+
 function toValue(value: string) {
-  const toNumber = (val: string | number) =>
-    (isNaN(val as number) ? 0 : val) as number;
   const [hr, min, secs = 0] = value
     .replace(/_/g, "0")
     .split(":")
     .map((x) => toNumber(parseInt(x)));
-
   return hr * 60 * 60 + min * 60 + secs;
 }
 
-const isValid = (value: string | number | null) =>
-  !String(value)?.includes("_");
+const isValid = (value: string) => !value.includes("_");
 
 export function Duration(props: FieldProps<string | number>) {
   const { schema, readonly, widgetAtom, valueAtom, invalid } = props;
@@ -59,49 +61,23 @@ export function Duration(props: FieldProps<string | number>) {
 
   const { attrs } = useAtomValue(widgetAtom);
   const { focus, required } = attrs;
-  const [changed, setChanged] = useState(false);
-  const [_value = "", setValue] = useAtom(valueAtom);
+
   const isTime = toKebabCase(schema.widget!) === "time";
 
-  const value = useMemo(
-    () =>
-      (isValid(_value) && !isTime
-        ? getValue(_value ?? "", { big, seconds })
-        : _value) as string,
-    [_value, isTime, big, seconds]
-  );
+  const { value, text, onChange, onBlur } = useInput(valueAtom, {
+    validate: isValid,
+    format: toText,
+    parse: toValue,
+  });
 
-  const update = useCallback(
-    (value: string, fireOnChange = false) => {
-      setValue(
-        isValid(value) && !isTime ? toValue(value) : value,
-        fireOnChange
-      );
-    },
-    [isTime, setValue]
-  );
-
-  const onChange = useCallback<React.ChangeEventHandler<HTMLInputElement>>(
-    (e) => {
-      update(e.target.value);
-      setChanged(true);
-    },
-    [update]
-  );
-
-  const onBlur = useCallback<React.FocusEventHandler<HTMLInputElement>>(
-    (e) => {
-      if (changed) {
-        update(e.target.value, true);
-        setChanged(false);
-      }
-    },
-    [changed, update]
+  const displayText = useMemo(
+    () => (isTime ? value : toText(value, { big, seconds })) ?? "",
+    [big, isTime, seconds, value],
   );
 
   return (
     <FieldControl {...props}>
-      {readonly && <ViewerInput value={value} />}
+      {readonly && <ViewerInput value={displayText} />}
       {readonly || (
         <MaskedInput
           {...(focus && { key: "focused" })}
@@ -110,7 +86,7 @@ export function Duration(props: FieldProps<string | number>) {
           id={uid}
           autoFocus={focus}
           placeholder={placeholder}
-          value={value}
+          value={text}
           invalid={invalid}
           required={required}
           onChange={onChange}
