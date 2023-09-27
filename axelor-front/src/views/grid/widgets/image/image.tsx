e08de9d6bce5d62e70c07eeeb6f3efb61af7a827
Phi-Long Do <p.do@axelor.com>
2023-09-27 13:46:54 +0200

    Fix image permission on grid
    Fixes #357

--- axelor-front/src/views/grid/widgets/image/image.tsx
+++ axelor-front/src/views/grid/widgets/image/image.tsx
@@ -11,12 +11,13 @@ export function Image(props: GridCellProps) {
   const { view, data, record } = props;
   const value = record?.[data?.name];
   const isMetaFile = (data as Field).target === META_FILE_MODEL;
-  const parent = isMetaFile
-    ? null
-    : {
-        ...record,
-        _model: view?.model,
-      };
+  const parent =
+    isMetaFile && !record
+      ? null
+      : {
+          ...record,
+          _model: view?.model,
+        };
   const url = makeImageURL(isMetaFile ? value : record, parent, data);
   return url && <img src={url} className={styles.image} />;
 }
