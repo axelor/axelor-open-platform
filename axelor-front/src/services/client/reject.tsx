29482f052d4c44308139ed35e8d661f97858cd3c
Amit Mendapara <amit@axelor.com>
2023-09-27 11:53:01 +0200

    Revert "Improve show dialog on server errors"
    This reverts commit c09b4a2bebc29d2b97e09622e8f948174c68d80d.

--- axelor-front/src/services/client/reject.tsx
+++ axelor-front/src/services/client/reject.tsx
@@ -1,18 +1,13 @@
-import { useCallback, useMemo, useState } from "react";
-import { Box, NavTabItem, NavTabs } from "@axelor/ui";
-
 import { alerts } from "@/components/alerts";
 import { dialogs } from "@/components/dialogs";
 
 import { i18n } from "./i18n";
+import { Box } from "@axelor/ui";
 
 export function reject(report: any) {
   let message;
   let exception;
   let stacktrace;
-  let cause;
-
-  const isDev = process.env.NODE_ENV !== "production";
 
   if (report.popup) {
     message =
@@ -37,7 +32,6 @@ export function reject(report: any) {
     }
 
     stacktrace = report.stacktrace;
-    cause = report.cause;
   } else if (report.message) {
     message = "<p>" + report.message.replace("\n", "<br>") + "</p>";
     alerts.error({ message });
@@ -54,66 +48,15 @@ export function reject(report: any) {
     size: "lg",
     content: (
       <Box d="flex" flexDirection="column" style={{ minWidth: 0 }}>
-        <p dangerouslySetInnerHTML={{ __html: message }} />
-        {stacktrace &&
-          (isDev ? (
-            <ErrorTabs cause={cause} stacktrace={stacktrace} />
-          ) : (
-            <Box flex={1} overflow="auto">
-              <ErrorText error={stacktrace} />
-            </Box>
-          ))}
+        <p>{message}</p>
+        {stacktrace && (
+          <Box flex={1} overflow="auto">
+            <pre>{stacktrace}</pre>
+          </Box>
+        )}
       </Box>
     ),
   });
 
   return Promise.reject(500);
-}
-
-function ErrorText({ error }: { error?: string }) {
-  return (
-    error && (
-      <Box
-        as="pre"
-        style={{ whiteSpace: "normal" }}
-        dangerouslySetInnerHTML={{
-          __html: error,
-        }}
-      />
-    )
-  );
-}
-
-function ErrorTabs({
-  stacktrace,
-  cause,
-}: {
-  stacktrace?: string;
-  cause?: string;
-}) {
-  const items = useMemo(
-    () => [
-      {
-        id: "1",
-        title: i18n.get("Stacktrace"),
-      },
-      { id: "2", title: i18n.get("Cause") },
-    ],
-    [],
-  );
-  const [active, setActive] = useState("1");
-  const handleChange = useCallback(
-    (item: NavTabItem) => setActive(item.id),
-    [],
-  );
-
-  const error = active === "1" ? stacktrace : cause;
-  return (
-    <>
-      <NavTabs items={items} active={active} onItemClick={handleChange} />
-      <Box flex={1} p={3} overflow="auto">
-        <ErrorText error={error} />
-      </Box>
-    </>
-  );
 }
