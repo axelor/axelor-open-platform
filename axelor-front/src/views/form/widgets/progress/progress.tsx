6c221f7a848b2ffed23a119bc269321307a876ed
Amit Mendapara <amit@axelor.com>
2023-09-29 18:19:02 +0200

    Improve Progress widget
    - show Integer or Decimal input in editor mode
    - show progress bar in readonly mode
    - changed the progress color scales
    
    Fixes #377

--- axelor-front/src/views/form/widgets/progress/progress.tsx
+++ axelor-front/src/views/form/widgets/progress/progress.tsx
@@ -6,6 +6,7 @@ import { Box, LinearProgress } from "@axelor/ui";
 import { Schema } from "@/services/client/meta.types";
 
 import { FieldControl, FieldProps } from "../../builder";
+import { Integer as IntegerWidget } from "../integer";
 
 import styles from "./progress.module.scss";
 
@@ -15,48 +16,43 @@ type TransformColor = {
   max: number;
 };
 
-const transformColors = (colors: string) =>
+const transformColors = (colors: string): TransformColor[] =>
   colors
     .split(/,/)
     .map((c) => c.split(/:/))
-    .map(
-      (c) =>
-        ({
-          code: c[0],
-          max: Number(c[1]),
-        } as TransformColor)
-    )
+    .map((c) => ({
+      code: c[0],
+      max: Number(c[1]),
+    }))
     .sort((a, b) => a.max - b.max);
 
 export function ProgressComponent({
   value,
   schema,
 }: {
-  value: number;
+  value: number | string;
   schema: Schema;
 }) {
-  const progressProps = useMemo(() => {
+  const props = useMemo(() => {
     const { min = 0, max = 100, colors = "r:24,y:49,b:74,g:100" } = schema;
-
-    const $value = Math.min(Math.round((+value * 100) / (max - min)), 100);
-    const { code } = transformColors(colors).find((c) => $value <= c.max) || {
-      code: "",
-    };
-
-    return { value: $value, className: styles[code] };
+    const num = Math.min(Math.round((+value * 100) / (max - min)), 100);
+    const code = transformColors(colors).find((c) => num <= c.max)?.code ?? "";
+    return { value: num, className: styles[code] };
   }, [value, schema]);
-
-  return <LinearProgress flex={1} {...progressProps} striped animated />;
+  return <LinearProgress flex={1} {...props} striped animated />;
 }
 
-export function Progress(props: FieldProps<number>) {
-  const { schema, valueAtom } = props;
+export function Progress(props: FieldProps<number | string>) {
+  const { schema, readonly, valueAtom } = props;
   const value = useAtomValue(valueAtom);
-  return (
-    <FieldControl {...props}>
-      <Box className={styles.progress}>
-        <ProgressComponent value={value || 0} schema={schema} />
-      </Box>
-    </FieldControl>
-  );
+  if (readonly) {
+    return (
+      <FieldControl {...props}>
+        <Box className={styles.progress}>
+          <ProgressComponent value={value || 0} schema={schema} />
+        </Box>
+      </FieldControl>
+    );
+  }
+  return <IntegerWidget {...props} />;
 }
