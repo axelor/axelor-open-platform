9a92d2a39a2e7ce8b3c228bbeb5b6508a7a46283
Phi-Long Do <p.do@axelor.com>
2023-10-03 17:34:34 +0200

    Add label legacy styles
    Addresses #337

--- axelor-front/src/views/form/widgets/label/label.tsx
+++ axelor-front/src/views/form/widgets/label/label.tsx
@@ -1,14 +1,16 @@
-import { useTemplate } from "@/hooks/use-parser";
 import { useAtomValue } from "jotai";
+
+import { useTemplate } from "@/hooks/use-parser";
+import { legacyClassNames } from "@/styles/legacy";
 import { WidgetProps } from "../../builder";
 
 export function Label({ formAtom, widgetAtom }: WidgetProps) {
   const { attrs } = useAtomValue(widgetAtom);
-  const { title = "" } = attrs;
+  const { title = "", css } = attrs;
   const { record } = useAtomValue(formAtom);
   const Template = useTemplate(title);
   return (
-    <label>
+    <label className={legacyClassNames(css)}>
       <Template context={record} />
     </label>
   );
