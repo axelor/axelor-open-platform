f030b587441ecfaaa33f29e7d7f29b1874da0e06
Amit Mendapara <amit@axelor.com>
2023-10-03 17:06:09 +0200

    Fix NaN issue in grid view progress widget
    The grid cell renderer will get formatted value causing progress widget
    to fail converting it to a proper number.
    
    Fixes #413

--- axelor-front/src/views/grid/widgets/progress/progress.tsx
+++ axelor-front/src/views/grid/widgets/progress/progress.tsx
@@ -1,12 +1,14 @@
 import { Box } from "@axelor/ui";
-import { ProgressComponent } from "@/views/form/widgets/progress";
 import { GridColumnProps } from "@axelor/ui/grid/grid-column";
 
+import { ProgressComponent } from "@/views/form/widgets/progress";
+
 export function Progress(props: GridColumnProps) {
-  const { data, value } = props;
+  const { data, value, record } = props;
+  const rawValue = record?.[data.name] ?? value;
   return (
     <Box d="flex" flex={1} alignItems="center">
-      <ProgressComponent value={value} schema={data} />
+      <ProgressComponent value={rawValue ?? 0} schema={data} />
     </Box>
   );
 }
