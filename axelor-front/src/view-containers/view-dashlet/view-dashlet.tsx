4048858d1cb9109cb0e566c61b7f7f05b55908d0
Amit Mendapara <amit@axelor.com>
2023-10-11 15:45:39 +0200

    Fix dashlet handlers lost because of scope change

--- axelor-front/src/view-containers/view-dashlet/view-dashlet.tsx
+++ axelor-front/src/view-containers/view-dashlet/view-dashlet.tsx
@@ -1,10 +1,12 @@
-import { ReactElement } from "react";
 import { ScopeProvider } from "jotai-molecules";
+import { ReactElement, useMemo } from "react";
+
 import { DashletScope } from "./handler";
 
 export function DashletView({ children }: { children?: ReactElement }) {
+  const scope = useMemo(() => ({}), []);
   return (
-    <ScopeProvider scope={DashletScope} value={{}}>
+    <ScopeProvider scope={DashletScope} value={scope}>
       {children}
     </ScopeProvider>
   );
