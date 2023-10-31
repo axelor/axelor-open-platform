31a5051b41af41920da5d62a3c88345d97ec80e5
Mayank Mahadevwala <m.mahadevwala@axelor.com>
2023-10-31 18:07:49 +0530

    Revert "Fix _parent support in form action context"
    Commit : 62fd43e6da689ce1d7e46cad192e9aeaf91d14f9

--- axelor-front/src/hooks/use-relation/use-editor.tsx
+++ axelor-front/src/hooks/use-relation/use-editor.tsx
@@ -111,12 +111,7 @@ export function useEditor() {
         popup: true,
         forceEdit: !readonly,
         "show-toolbar": false,
-        "_popup-record": context?._parent
-          ? {
-              ...record,
-              _parent: context._parent,
-            }
-          : record,
+        "_popup-record": record,
       },
       context: {
         _showRecord: record?.id,
