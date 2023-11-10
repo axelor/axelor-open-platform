a025c2c3cad42a794a60907b7997fc12564de78b
Pierre Belloy <p.belloy@axelor.com>
2023-11-10 15:47:41 +0100

    Add support for icon in tree image-select widget
    Fixes #581
    
    Signed-off-by: Pierre Belloy <p.belloy@axelor.com>

--- axelor-front/src/views/tree/widgets/image-select/image-select.tsx
+++ axelor-front/src/views/tree/widgets/image-select/image-select.tsx
@@ -1,21 +1,33 @@
 import { WidgetProps } from "../../types";
+import { Icon } from "@/components/icon";
+import { Box } from "@axelor/ui";
 
 export function ImageSelect({ field, record }: WidgetProps) {
   const { name, selectionList, labels } = field;
   const value = record[name];
   const selectValue = selectionList?.find(
-    (item) => String(item.value) === String(value)
+    (item) => String(item.value) === String(value),
   );
 
   if (selectValue?.icon) {
     return (
       <>
-        <img
-          style={{ maxHeight: 24 }}
-          src={selectValue.icon || selectValue.value}
-          alt="select icon"
-        />
-        {labels !== false && <span>{selectValue.title}</span>}
+        <Box gap={6} alignItems="center">
+          {selectValue.icon && !selectValue.icon.includes(".") ? (
+            <Icon icon={selectValue.icon} />
+          ) : (
+            <img
+              style={
+                labels === false
+                  ? { maxHeight: 24 }
+                  : { maxWidth: 18, height: "fit-content" }
+              }
+              src={selectValue.icon}
+              alt={selectValue.title}
+            />
+          )}
+          {labels !== false && <span>{selectValue.title}</span>}
+        </Box>
       </>
     );
   }
