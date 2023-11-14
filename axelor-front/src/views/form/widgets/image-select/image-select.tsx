eb591593e82c14f12c4f9f77f632994403e3430b
Pierre Belloy <p.belloy@axelor.com>
2023-11-14 13:37:34 +0100

    Fix ImageSelect value render in form
    Render the ImageSelect value if the option exist (case of default
    value in number field).
    
    Fixes #607
    
    Signed-off-by: Pierre Belloy <p.belloy@axelor.com>

--- axelor-front/src/views/form/widgets/image-select/image-select.tsx
+++ axelor-front/src/views/form/widgets/image-select/image-select.tsx
@@ -1,17 +1,39 @@
+import { useCallback, useMemo } from "react";
+import { useAtom } from "jotai/index";
+
 import { FieldProps } from "../../builder";
 import { Selection } from "../selection";
 import { ImageSelectValue } from "./image-select-value";
 
+import { SelectOptionProps } from "@/components/select";
+import { Selection as SelectionType } from "@/services/client/meta.types";
+
 export function ImageSelect(props: FieldProps<string | number | null>) {
-  const { schema } = props;
+  const { schema, valueAtom } = props;
   const { labels } = schema;
+  const selectionList = schema?.selectionList as SelectionType[];
+  const [value, setValue] = useAtom(valueAtom);
+
+  const selectedOption = useMemo(
+    () => selectionList?.find((x) => String(x.value) === String(value)),
+    [selectionList, value],
+  );
+
+  const renderValue = useCallback(
+    ({ option }: SelectOptionProps<SelectionType>) => {
+      if (selectedOption) {
+        return <ImageSelectValue option={option} showLabel={labels} />;
+      }
+      return null;
+    },
+    [labels, selectedOption],
+  );
+
   return (
     <Selection
       {...props}
       autoComplete={false}
-      renderValue={({ option }) => (
-        <ImageSelectValue option={option} showLabel={labels} />
-      )}
+      renderValue={renderValue}
       renderOption={({ option }) => (
         <ImageSelectValue option={option} showLabel={labels} />
       )}
