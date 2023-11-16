5409b51a8eda69cc809c7ab7cb1acd2a8bece22e
Pierre Belloy <p.belloy@axelor.com>
2023-11-16 23:40:40 +0100

    Fix button support in tree view
    Fixes #422
    
    Signed-off-by: Pierre Belloy <p.belloy@axelor.com>

--- axelor-front/src/views/tree/widgets/button/button.tsx
+++ axelor-front/src/views/tree/widgets/button/button.tsx
@@ -1,30 +1,41 @@
 import { Icon } from "@/components/icon";
-import { Link } from "@axelor/ui";
+import { Box, Link } from "@axelor/ui";
 import { WidgetProps } from "../../types";
 import styles from "./button.module.scss";
+import { useCallback } from "react";
 
 export function Button({ field, node, record, actionExecutor }: WidgetProps) {
-  const { name, icon, onClick } = field;
+  const { name, icon, onClick, title, help } = field;
+
+  const handleClick = useCallback(
+    async (event: React.MouseEvent<HTMLElement>) => {
+      event.preventDefault();
+      event.stopPropagation();
+      if (onClick && actionExecutor) {
+        await actionExecutor.waitFor();
+        await actionExecutor.execute(onClick, {
+          context: {
+            ...record,
+            _signal: name,
+            _model: node?.model,
+          },
+        });
+      }
+    },
+    [onClick, actionExecutor, record, name, node],
+  );
+
   return (
     <Link
-      className={styles.link}
-      onClick={(e) => {
-        e.preventDefault();
-        e.stopPropagation();
-        onClick &&
-          actionExecutor?.execute(onClick, {
-            context: {
-              ...record,
-              _signal: name,
-              _model: node?.model,
-            },
-          });
-      }}
+      d="inline-flex"
+      onClick={handleClick}
+      title={help}
+      className={styles.action}
     >
-      {icon && icon.includes?.("fa") ? (
+      {icon && !icon.includes(".") ? (
         <Icon icon={icon} />
       ) : (
-        <img width={16} alt="Tree Icon" src={icon} />
+        <img style={{ maxHeight: 24, width: "100%"}} alt={title} title={help} src={icon} />
       )}
     </Link>
   );
