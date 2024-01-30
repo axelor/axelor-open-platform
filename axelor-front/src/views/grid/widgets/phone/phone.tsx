00b325969d7f872e5bd6e5171a7343130388055f
Phi-Long Do <p.do@axelor.com>
2024-01-30 18:11:16 +0100

    Implement phone widget on grid
    Addresses #304

--- axelor-front/src/views/grid/widgets/phone/phone.tsx
+++ axelor-front/src/views/grid/widgets/phone/phone.tsx
@@ -1,11 +1,31 @@
-import { Box } from "@axelor/ui";
 import { GridColumnProps } from "@axelor/ui/grid";
+import { FlagImage, usePhoneInput } from "react-international-phone";
+
+import { Box } from "@axelor/ui";
+import "react-international-phone/style.css";
+import styles from "./phone.module.scss";
 
 export function Phone(props: GridColumnProps) {
-  const { rawValue } = props;
+  const { rawValue: value } = props;
+
+  const { inputValue, phone, country } = usePhoneInput({
+    value: value ?? "",
+  });
+
   return (
-    <Box as="a" href={`tel:${rawValue}`}>
-      {rawValue}
-    </Box>
+    value && (
+      <>
+        <FlagImage iso2={country.iso2} className={styles.country} />
+        <Box
+          as="a"
+          target="_blank"
+          href={`tel:${phone}`}
+          className={styles.link}
+          onClick={(e) => e.stopPropagation()} // Prevent going into edit when clicking on link.
+        >
+          {inputValue}
+        </Box>
+      </>
+    )
   );
 }
