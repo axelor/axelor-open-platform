axelor/axelor-open-platform

commit c273f5df36a8515d6a16229d6204b0f865ad394a

Fix custom field change should mark form dirty
Set dirty state before value set, because in json editor, state.dirty of formAtom is used.

Fixes #748

axelor-front/src/views/form/builder/atoms.ts
@@ -169,12 +169,12 @@ export function createValueAtom({
         Boolean(name && !isCleanDummy(name)) &&
         canDirty(widgetSchema);
 
-      set(lensAtom, next);
-
       if (dirty) {
         set(formAtom, (prev) => (prev.dirty ? prev : { ...prev, dirty }));
         set(dirtyAtom, true);
       }
+
+      set(lensAtom, next);
 
       return fireOnChange && triggerOnChange();
     },

commit 30613311bbe133cfdecb8c56af417af5eef00dac

Fix form dirty update for for form editors
When custom field is used independently in form view, changes in one custom field is reset values of other custom fields.

Regression by commit c273f5df36a8515d6a16229d6204b0f865ad394a.

axelor-front/src/views/form/builder/atoms.ts
@@ -103,6 +103,10 @@ export function createWidgetAtom(props: {
   return widgetAtom;
 }
 
+export function formDirtyUpdater(prev: FormState) {
+  return prev.dirty ? prev : { ...prev, dirty: true };
+}
+
 export function createValueAtom({
   schema,
   formAtom,
@@ -170,7 +174,7 @@ export function createValueAtom({
         canDirty(widgetSchema);
 
       if (dirty) {
-        set(formAtom, (prev) => (prev.dirty ? prev : { ...prev, dirty }));
+        set(formAtom, formDirtyUpdater);
         set(dirtyAtom, true);
       }
 

axelor-front/src/views/form/builder/form-editors.tsx
@@ -27,14 +27,13 @@ import { toKebabCase } from "@/utils/names.ts";
 import { MetaScope, useViewTab } from "@/view-containers/views/scope";
 
 import { Layout as FormViewLayout, useGetErrors } from "../form";
-import { createFormAtom } from "./atoms";
+import { createFormAtom, formDirtyUpdater } from "./atoms";
 import { Form, useFormHandlers, usePermission } from "./form";
 import { FieldControl } from "./form-field";
 import { GridLayout } from "./form-layouts";
 import { useAfterActions, useFormScope } from "./scope";
 import {
   FieldProps,
-  FormAtom,
   FormLayout,
   FormState,
   ValueAtom,
@@ -895,6 +894,11 @@ const RecordEditor = memo(function RecordEditor({
         }
 
         set(editorFormAtom, state);
+
+        // the update is intended for dirty state changed
+        // no value changes occurs through this update
+        if (update === formDirtyUpdater) return;
+
         set(
           valueAtom,
           isEqual(record, EMPTY_RECORD) ? null : record,