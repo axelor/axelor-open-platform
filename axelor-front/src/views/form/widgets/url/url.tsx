71554f75def7c80b1eb1b48edd0054222afd9027
Tom Boulesteix <t.boulesteix@axelor.com>
2024-03-04 17:30:35 +0100

    Allow open urls in edit mode
    Fixes #820

--- axelor-front/src/views/form/widgets/url/url.tsx
+++ axelor-front/src/views/form/widgets/url/url.tsx
@@ -1,14 +1,41 @@
+import { useCallback, useEffect, useMemo, useState } from "react";
 import { useAtomValue } from "jotai";
 
-import { Box } from "@axelor/ui";
+import { Box, Button } from "@axelor/ui";
+import { BootstrapIcon } from "@axelor/ui/icons/bootstrap-icon";
+import { i18n } from "@/services/client/i18n";
 
 import { String } from "../string";
-
 import { FieldControl, FieldProps } from "../../builder";
+
+import styles from "./url.module.scss";
 
 export function Url(props: FieldProps<string>) {
   const { readonly, valueAtom } = props;
   const value = useAtomValue(valueAtom);
+  const [url, setUrl] = useState(value);
+
+  const isValidUrl = useMemo(() => {
+    try {
+      new URL(url ?? "");
+      return true;
+    } catch (err) {
+      return false;
+    }
+  }, [url]);
+
+  const onChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    setUrl(e.target.value);
+  }, []);
+
+  const handleOpenUrl = useCallback(() => {
+    window.open(url as string, "_blank");
+  }, [url]);
+
+  useEffect(() => {
+    setUrl(value);
+  }, [value]);
+
   if (readonly) {
     return (
       <FieldControl {...props}>
@@ -20,5 +47,21 @@ export function Url(props: FieldProps<string>) {
       </FieldControl>
     );
   }
-  return <String {...props} inputProps={{ type: "url" }} />;
+  return (
+    <String
+      {...props}
+      inputProps={{ type: "url" }}
+      onChange={onChange}
+      inputEndAdornment={
+        <Button
+          onClick={handleOpenUrl}
+          className={styles.inputIcon}
+          disabled={!isValidUrl}
+          title={i18n.get("Open URL")}
+        >
+          <BootstrapIcon icon="globe" />
+        </Button>
+      }
+    />
+  );
 }
