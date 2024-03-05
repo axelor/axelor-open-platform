85c1f91554eabc186e9afae5bab7e059f478e318
Tom Boulesteix <t.boulesteix@axelor.com>
2024-03-05 17:18:50 +0100

    Allow send emails in edit mode
    Addresses #819

--- axelor-front/src/views/form/widgets/email/email.tsx
+++ axelor-front/src/views/form/widgets/email/email.tsx
@@ -1,6 +1,9 @@
+import { useCallback, useEffect, useMemo, useState } from "react";
 import { useAtomValue } from "jotai";
 
-import { Box } from "@axelor/ui";
+import { Box, Button } from "@axelor/ui";
+import { BootstrapIcon } from "@axelor/ui/icons/bootstrap-icon";
+import { i18n } from "@/services/client/i18n";
 
 import { FieldControl, FieldProps } from "../../builder";
 import { String } from "../string";
@@ -10,6 +13,24 @@ import styles from "./email.module.scss";
 export function Email(props: FieldProps<string>) {
   const { readonly, valueAtom } = props;
   const value = useAtomValue(valueAtom);
+  const [email, setEmail] = useState(value);
+
+  const isValidEmail = useMemo(() => {
+    return !!email?.match(/^[\w-.]+@([\w-]+\.)+[\w-]{2,}$/g);
+  }, [email]);
+
+  const onChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  }, []);
+
+  const handleOpenEmailLink = useCallback(() => {
+    window.location.href = `mailto:${email}`;
+  }, [email]);
+
+  useEffect(() => {
+    setEmail(value);
+  }, [value]);
+
   if (readonly) {
     return (
       <FieldControl {...props}>
@@ -26,5 +47,21 @@ export function Email(props: FieldProps<string>) {
       </FieldControl>
     );
   }
-  return <String {...props} inputProps={{ type: "email" }} />;
+  return (
+    <String
+      {...props}
+      inputProps={{ type: "email" }}
+      onChange={onChange}
+      inputEndAdornment={
+        <Button
+          onClick={handleOpenEmailLink}
+          disabled={!isValidEmail}
+          className={styles.inputIcon}
+          title={i18n.get("Send email")}
+        >
+          <BootstrapIcon icon={"envelope"} />
+        </Button>
+      }
+    />
+  );
 }
