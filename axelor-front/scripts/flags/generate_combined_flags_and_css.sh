3fc9546ed789e570f2264d35551129e234eb10c0
Phi-Long Do <p.do@axelor.com>
2024-03-07 17:23:44 +0100

    Add filename extensions to flags scripts
    Addresses #304

---
+++ axelor-front/scripts/flags/generate_combined_flags_and_css.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+IMAGE_FILE=flags.svg
+CSS_FILE=flags.module.scss
+
+WORK_DIR=${TMPDIR:-${TEMP:-${TMP:-/tmp}}}
+
+./extract_country_codes.py | ./to_twemoji_url.py | ./download_image.py -d $WORK_DIR/twemoji
+./rename_svg.py $WORK_DIR/twemoji $WORK_DIR/flags
+./combine_svg.py $WORK_DIR/flags -o ../../src/assets/$IMAGE_FILE
+./generate_css.py $WORK_DIR/flags -o ../../src/views/form/widgets/phone/$CSS_FILE
+
+rm -rf $WORK_DIR/flags
