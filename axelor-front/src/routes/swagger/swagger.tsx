84bfcc9bc7a30377bd8a3041630075a108b0e7c7
Phi-Long Do <p.do@axelor.com>
2024-03-19 17:55:49 +0100

    Refactor swagger component

--- axelor-front/src/routes/swagger/swagger.tsx
+++ axelor-front/src/routes/swagger/swagger.tsx
@@ -1,51 +1,48 @@
 import SwaggerUI from "swagger-ui-react";
 
-import { session } from "@/services/client/session";
-
 import {
   CSRF_COOKIE_NAME,
   CSRF_HEADER_NAME,
   readCookie,
 } from "@/services/client/client";
+import { session } from "@/services/client/session";
 
 import "swagger-ui-react/swagger-ui.css";
 import styles from "./swagger.module.scss";
 
-export function Swagger() {
-  const openapiUrl = "ws/openapi";
+const OPENAPI_URL = "ws/openapi";
 
-  const { info } = session;
-  const { allowTryItOut } = info?.application?.swaggerUI ?? {};
-
-  const DisableTryItOutPlugin = function () {
-    return {
-      statePlugins: {
-        spec: {
-          wrapSelectors: {
-            allowTryItOutFor: () => () => false,
-          },
-        },
+const DisableTryItOutPlugin = () => ({
+  statePlugins: {
+    spec: {
+      wrapSelectors: {
+        allowTryItOutFor: () => () => false,
       },
-    };
-  };
+    },
+  },
+});
 
-  const RequestInterceptor = function (req: {
-    headers?: Record<string, string>;
-  }) {
-    const token = readCookie(CSRF_COOKIE_NAME);
-    if (req.headers && token) {
-      req.headers[CSRF_HEADER_NAME] = token;
-    }
-    return req;
-  };
+const RequestInterceptor = (req: { headers?: Record<string, string> }) => ({
+  ...req,
+  headers: {
+    ...req.headers,
+    [CSRF_HEADER_NAME]: readCookie(CSRF_COOKIE_NAME),
+  },
+});
+
+export function Swagger() {
+  const { info } = session;
+  const { enabled, allowTryItOut } = info?.application?.swaggerUI ?? {};
 
   return (
-    <div className={styles.page}>
-      <SwaggerUI
-        url={openapiUrl}
-        plugins={allowTryItOut ? undefined : [DisableTryItOutPlugin]}
-        requestInterceptor={RequestInterceptor}
-      />
-    </div>
+    enabled && (
+      <div className={styles.page}>
+        <SwaggerUI
+          url={OPENAPI_URL}
+          plugins={allowTryItOut ? undefined : [DisableTryItOutPlugin]}
+          requestInterceptor={RequestInterceptor}
+        />
+      </div>
+    )
   );
 }
