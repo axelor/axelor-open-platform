57998d1dfdbfc44ff93d0966bbc6e1a1a57ed446
Phi-Long Do <p.do@axelor.com>
2024-04-03 12:04:39 +0200

    Fix setting form dirty after grid button action
    Action triggered from grid button in non-edit mode did not set main form record as dirty.
    
    Addresses #887
    
    Signed-off-by: Pierre Belloy <p.belloy@axelor.com>

--- axelor-front/src/views/grid/widgets/button/button.tsx
+++ axelor-front/src/views/grid/widgets/button/button.tsx
@@ -1,16 +1,17 @@
-import { legacyClassNames } from "@/styles/legacy";
-import { Box } from "@axelor/ui";
 import clsx from "clsx";
 import { useMemo } from "react";
+import { Box } from "@axelor/ui";
 
+import { dialogs } from "@/components/dialogs";
 import { Icon } from "@/components/icon";
+import { createScriptContext } from "@/hooks/use-parser/context";
 import { parseExpression } from "@/hooks/use-parser/utils";
 import { Button as ButtonField, Field } from "@/services/client/meta.types";
-import { dialogs } from "@/components/dialogs";
+import { legacyClassNames } from "@/styles/legacy";
 import { useViewAction } from "@/view-containers/views/scope";
-import { GridCellProps } from "../../builder/types";
+import { useCanDirty } from "@/views/form/builder/scope";
 import { processContextValues } from "@/views/form/builder/utils";
-import { createScriptContext } from "@/hooks/use-parser/context";
+import { GridCellProps } from "../../builder/types";
 
 import styles from "./button.module.scss";
 
@@ -20,6 +21,8 @@ export function Button(props: GridCellProps) {
   const { title, name, icon, css, help: _help } = field as Field;
   const help = _help || title;
   const { context } = useViewAction();
+
+  const canDirty = useCanDirty();
 
   const { hidden, readonly } = useMemo(() => {
     const { showIf, hideIf, readonlyIf } = field as Field;
@@ -72,7 +75,14 @@ export function Button(props: GridCellProps) {
         }),
         {},
       );
-      Object.keys(values || {}).length && onUpdate?.({ ...record, ...values });
+
+      const fieldNames = Object.keys(values || {});
+
+      if (fieldNames.length) {
+        const _dirty =
+          record._dirty || fieldNames.some((name) => canDirty(name));
+        onUpdate?.({ ...record, ...values, _dirty });
+      }
     }
   }
 
