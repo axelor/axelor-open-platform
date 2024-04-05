b6d34584073ee409b968c39b1366d475fe4ffae3
Phi-Long Do <p.do@axelor.com>
2024-04-05 16:52:39 +0200

    Commit editable widgets when clicking on grid button
    This fixes editable grid row pending changes being lost if we click on another grid row button.
    
    Addresses #887

--- axelor-front/src/views/grid/widgets/button/button.tsx
+++ axelor-front/src/views/grid/widgets/button/button.tsx
@@ -8,7 +8,7 @@ import { parseExpression } from "@/hooks/use-parser/utils";
 import { Button as ButtonField, Field } from "@/services/client/meta.types";
 import { legacyClassNames } from "@/styles/legacy";
 import { useViewAction } from "@/view-containers/views/scope";
-import { useCanDirty } from "@/views/form/builder/scope";
+import { useCanDirty, useFormEditableScope } from "@/views/form/builder/scope";
 import { processContextValues } from "@/views/form/builder/utils";
 import { GridCellProps } from "../../builder/types";
 
@@ -39,6 +39,8 @@ export function Button(props: GridCellProps) {
     return { hidden, readonly };
   }, [field, record, context]);
 
+  const { commit: commitEditableWidgets } = useFormEditableScope();
+
   if (!icon || hidden) return null;
 
   const className = clsx({
@@ -54,6 +56,7 @@ export function Button(props: GridCellProps) {
         if (!confirmed) return;
       }
       if (!onClick) return;
+      await commitEditableWidgets();
       await actionExecutor.waitFor();
       const res = await actionExecutor.execute(onClick, {
         context: {
