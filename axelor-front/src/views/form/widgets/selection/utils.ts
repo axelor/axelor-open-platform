1261e032ada5a16809c7c61bc15e092442a75975
Pierre Belloy <p.belloy@axelor.com>
2024-11-04 11:44:52 +0100

    Fix handling 0 valued in selection widgets
    Fixes #1227
    
    Signed-off-by: Pierre Belloy <p.belloy@axelor.com>

--- axelor-front/src/views/form/widgets/selection/utils.ts
+++ axelor-front/src/views/form/widgets/selection/utils.ts
@@ -1,5 +1,5 @@
 export function getMultiValues(value?: null | number | string) {
-  return value ? String(value).split(/\s*,\s*/) : [];
+  return value || value === 0 ? String(value).split(/\s*,\s*/) : [];
 }
 
 export function joinMultiValues(values: string[]) {
