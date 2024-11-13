ff070c4179e090a7ac78f9d3a823270ad0570f8d
Phi-Long Do <p.do@axelor.com>
2024-11-13 16:49:58 +0100

    Fix instructions for InfinispanTest

--- axelor-core/src/test/java/com/axelor/cache/InfinispanTest.java
+++ axelor-core/src/test/java/com/axelor/cache/InfinispanTest.java
@@ -25,10 +25,10 @@ import org.hibernate.cfg.Environment;
 import org.junit.jupiter.api.Disabled;
 
 /**
- * `AbstractBaseCache#shouldHitQueryCache` fail with Infinispan : cache isn't hit Moreover,
- * Infinispan 13.0.21.Final depends on Caffeine 2.8.4. We use Caffeine 3+, but it isn't compatible
- * with Infinispan. To run the test, manually uncomment caffeine dependency load in `implementation`
- * configuration.
+ * `AbstractBaseCache#shouldHitQueryCache` fail with Infinispan: cache isn't hit. Moreover,
+ * Infinispan 13.x depends on Caffeine 2.8.4. We use Caffeine 3+, but it isn't compatible with
+ * Infinispan. To run the test, manually comment caffeine dependency load in
+ * `implementation`configuration.
  */
 @Disabled
 @GuiceModules(InfinispanTest.InfinispanTestModule.class)
