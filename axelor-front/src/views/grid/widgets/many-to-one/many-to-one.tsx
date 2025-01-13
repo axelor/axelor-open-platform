32f19359121b0779417462cb32a6f08b3487e189
Mayank Mahadevwala <m.mahadevwala@axelor.com>
2025-01-13 13:32:16 +0530

    Fix relational field target-name support in grid-view
    When any custom target-name is used instead of default nameColumn then
    it should display its value in grid cell.
    
    Fixes #1326

--- axelor-front/src/views/grid/widgets/many-to-one/many-to-one.tsx
+++ axelor-front/src/views/grid/widgets/many-to-one/many-to-one.tsx
@@ -1,11 +1,28 @@
-import { RelationalValue } from "@/views/form/widgets";
+import { useMemo } from "react";
+
 import { Box } from "@axelor/ui";
 import { GridColumnProps } from "@axelor/ui/grid";
 
-export function ManyToOne({ data: column, rawValue }: GridColumnProps) {
+import { RelationalValue } from "@/views/form/widgets";
+import { Field } from "@/services/client/meta.types";
+
+export function ManyToOne({ data: column, value, rawValue }: GridColumnProps) {
+  const field = column as Field;
+
+  const $value = useMemo(
+    () =>
+      rawValue && field.targetName
+        ? {
+            ...rawValue,
+            [field.targetName]: value,
+          }
+        : rawValue,
+    [value, rawValue, field],
+  );
+
   return (
     <Box d="inline-flex">
-      <RelationalValue schema={column} value={rawValue} />
+      <RelationalValue schema={column} value={$value} />
     </Box>
   );
 }
