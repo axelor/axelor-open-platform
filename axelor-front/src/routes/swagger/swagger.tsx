214aec3d0d11a0cbbed0763f768586715468af00
Mayank Mahadevwala <m.mahadevwala@axelor.com>
2025-03-18 13:50:48 +0530

    Fix swagger ui styles for dark base themes
    Fixes #1374

--- axelor-front/src/routes/swagger/swagger.tsx
+++ axelor-front/src/routes/swagger/swagger.tsx
@@ -1,12 +1,14 @@
 import SwaggerUI from "swagger-ui-react";
+import { clsx } from "@axelor/ui/core";
 
+import { useAppSettings } from "@/hooks/use-app-settings";
 import {
   CSRF_COOKIE_NAME,
   CSRF_HEADER_NAME,
   readCookie,
 } from "@/services/client/client";
-import { session } from "@/services/client/session";
 
+import { session } from "@/services/client/session";
 import "swagger-ui-react/swagger-ui.css";
 import styles from "./swagger.module.scss";
 
@@ -33,10 +35,11 @@ const RequestInterceptor = (req: { headers?: Record<string, string> }) => ({
 export function Swagger() {
   const { info } = session;
   const { enabled, allowTryItOut } = info?.application?.swaggerUI ?? {};
+  const { themeMode } = useAppSettings();
 
   return (
     enabled && (
-      <div className={styles.page}>
+      <div className={clsx(styles.page, `swagger-theme-${themeMode}`)}>
         <SwaggerUI
           url={OPENAPI_URL}
           plugins={allowTryItOut ? undefined : [DisableTryItOutPlugin]}
