53fd6a3ed94eb842049eb1628525cd3334a3376c
Pierre Belloy <p.belloy@axelor.com>
2025-06-30 14:47:10 +0200

    Improve M2O widget in grid view
    Signed-off-by: Pierre Belloy <p.belloy@axelor.com>

--- axelor-front/src/views/grid/widgets/many-to-one/many-to-one.tsx
+++ axelor-front/src/views/grid/widgets/many-to-one/many-to-one.tsx
@@ -1,28 +1,40 @@
 import { useMemo } from "react";
+import set from "lodash/set";
 
 import { Box } from "@axelor/ui";
 import { GridColumnProps } from "@axelor/ui/grid";
+import { RelationalValue, Tag } from "@/views/form/widgets";
+import { Field, Schema } from "@/services/client/meta.types";
 
-import { RelationalValue } from "@/views/form/widgets";
-import { Field } from "@/services/client/meta.types";
+export function ManyToOne(props: GridColumnProps) {
+  const { rawValue, record, data: schema } = props;
+  const isTagSelect = (schema as Field).widget === "tag-select";
 
-export function ManyToOne({ data: column, value, rawValue }: GridColumnProps) {
-  const field = column as Field;
-
-  const $value = useMemo(
-    () =>
-      rawValue && field.targetName
-        ? {
-            ...rawValue,
-            [field.targetName]: value,
-          }
-        : rawValue,
-    [value, rawValue, field],
-  );
+  const value = useMemo(() => {
+    if (!rawValue) {
+      return rawValue;
+    }
+    return Object.keys(record).reduce(
+      (_value, key) => {
+        const prefix = `${schema.name}.`;
+        // To merge dotted field value from record
+        if (key.startsWith(prefix)) {
+          const fieldName = key.slice(prefix.length);
+          const fieldValue = record[key];
+          return set(_value, fieldName, fieldValue);
+        }
+        return _value;
+      },
+      { ...rawValue },
+    );
+  }, [schema.name, rawValue, record]);
 
   return (
     <Box d="inline-flex">
-      <RelationalValue schema={column} value={$value} />
+      {!isTagSelect && (
+        <RelationalValue schema={schema as Schema} value={value} />
+      )}
+      {isTagSelect && value && <Tag schema={schema as Schema} record={value} />}
     </Box>
   );
 }
--- axelor-front/src/views/grid/widgets/tag-select/tag-select.tsx
+++ axelor-front/src/views/grid/widgets/tag-select/tag-select.tsx
@@ -1,68 +1,35 @@
 import { useCallback, useMemo } from "react";
 import { Box, Overflow, OverflowItem } from "@axelor/ui";
 import { GridColumnProps } from "@axelor/ui/grid";
-import set from "lodash/set";
 
 import { OverflowMenu } from "@/components/overflow-menu/overflow-menu";
-import { RelationalValue, SelectionTag, Tag } from "@/views/form/widgets";
+import { Tag } from "@/views/form/widgets";
 import { DataRecord } from "@/services/client/data.types";
-import { Field, Schema } from "@/services/client/meta.types";
+import { Schema } from "@/services/client/meta.types";
 import { toKebabCase } from "@/utils/names";
-import formTagSelectStyles from "@/views/form/widgets/tag-select/tag-select.module.scss";
+import { ManyToOne } from "@/views/grid/widgets";
 
 const getItemKey = (record: DataRecord) => record.id!;
 
 export function TagSelect(props: GridColumnProps) {
-  const { rawValue: value, data } = props;
+  const { data } = props;
   const schema = data as Schema;
   const hasToOne = toKebabCase(schema.serverType || schema.widget).endsWith(
     "-to-one",
   );
 
   if (hasToOne) {
-    return value && <SingleTagSelect {...props} />;
+    return <ManyToOne {...props} />;
   }
 
   return <MultipleTagSelect {...props} />;
-}
-
-function SingleTagSelect(props: GridColumnProps) {
-  const { rawValue, record, data: schema } = props;
-
-  const value = useMemo(() => {
-    return Object.keys(record).reduce(
-      (_value, key) => {
-        const prefix = `${schema.name}.`;
-        // To merge dotted field value from record
-        // Like for colorField, as it's value is fetched as dotted field in grid search
-        // ex: record: { "product": { "id": 1 }, "product.color": "red"} to { "product": { "id": 1, "color": "red"}}
-        if (key.startsWith(prefix)) {
-          const fieldName = key.slice(prefix.length);
-          const fieldValue = record[key];
-          return set(_value, fieldName, fieldValue);
-        }
-        return _value;
-      },
-      { ...rawValue },
-    );
-  }, [schema.name, rawValue, record]);
-
-  return <Tag schema={schema} record={value} />;
 }
 
 function MultipleTagSelect(props: GridColumnProps) {
   const { rawValue, data: schema } = props;
 
   const renderItem = useCallback(
-    (item: DataRecord) => (
-      <SelectionTag
-        {...((schema as Field).imageField && {
-          className: formTagSelectStyles.imageTag,
-        })}
-        title={<RelationalValue schema={schema} value={item} />}
-        color={"primary"}
-      />
-    ),
+    (item: DataRecord) => <Tag schema={schema as Schema} record={item} />,
     [schema],
   );
 
