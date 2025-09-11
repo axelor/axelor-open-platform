e5bf6ab3e4786aa438cc0a38f61be5cdebcc0596
Pierre Belloy <p.belloy@axelor.com>
2025-09-11 12:56:48 +0200

    Add documentation on GenerateCode class
    Signed-off-by: Pierre Belloy <p.belloy@axelor.com>

--- axelor-gradle/src/main/java/com/axelor/gradle/tasks/GenerateCode.java
+++ axelor-gradle/src/main/java/com/axelor/gradle/tasks/GenerateCode.java
@@ -49,49 +49,113 @@ public class GenerateCode extends DefaultTask {
       "Generate code for domain models from xml definitions.";
   public static final String TASK_GROUP = AxelorPlugin.AXELOR_BUILD_GROUP;
 
+  // Input directory of the domain models
+
   private static final String DIR_INPUT_MAIN = "src/main/resources/domains";
   private static final String DIR_INPUT_TEST = "src/test/resources/domains";
+
+  // Output directories of the domain models
 
   private static final String DIR_OUTPUT_MAIN_JAVA = "src-gen/main/java";
   private static final String DIR_OUTPUT_TEST_JAVA = "src-gen/test/java";
 
+  // Output directory for the axelor-module.properties file
+
   private static final String DIR_OUTPUT_MAIN_RESOURCES = "src-gen/main/resources";
 
+  /** Indicates whether test domain models should be used in the code generation process. */
   private Boolean useTestSources = Boolean.FALSE;
 
+  /**
+   * Indicates whether test domain models should be used in the code generation process.
+   *
+   * @return true if test domain models should be generated
+   */
   @Input
   public Boolean getUseTestSources() {
     return useTestSources;
   }
 
+  /**
+   * Sets whether test domain models should be used in the code generation process.
+   *
+   * @param useTestSources true if test domain models should be generated
+   */
   public void setUseTestSources(Boolean useTestSources) {
     this.useTestSources = useTestSources;
   }
 
+  /**
+   * Retrieves the directory containing the main domain models for the given project.
+   *
+   * @param project the project
+   * @return the directory containing the main domain models as a {@link File}
+   */
   public static File getInputMainDir(Project project) {
     return new File(project.getProjectDir(), DIR_INPUT_MAIN);
   }
 
+  /**
+   * Retrieves the directory containing the test domain models for the given project.
+   *
+   * @param project the project
+   * @return the directory containing the test domain models as a {@link File}
+   */
   public static File getInputTestDir(Project project) {
     return new File(project.getProjectDir(), DIR_INPUT_TEST);
   }
 
+  /**
+   * Retrieves the build directory of the given project
+   *
+   * @param project the project
+   * @return the build directory as a {@link File}
+   */
   private static File getOutputBase(Project project) {
     return project.getLayout().getBuildDirectory().get().getAsFile();
   }
 
+  /**
+   * Retrieves the output directory of the main domain models for the given project.
+   *
+   * @param project the project
+   * @return the output directory of the main domain models as a {@link File}
+   */
   public static File getMainJavaOutputDir(Project project) {
     return new File(getOutputBase(project), DIR_OUTPUT_MAIN_JAVA);
   }
 
+  /**
+   * Retrieves the output directory of the test domain models for the given project.
+   *
+   * @param project the project
+   * @return the output directory of the test domain models as a {@link File}
+   */
   public static File getTestJavaOutputDir(Project project) {
     return new File(getOutputBase(project), DIR_OUTPUT_TEST_JAVA);
   }
 
+  /**
+   * Retrieves the output directory of the main resources for the given project.
+   *
+   * @param project the project
+   * @return the output directory of the main resources as a {@link File}
+   */
   public static File getMainResourceOutputDir(Project project) {
     return new File(getOutputBase(project), DIR_OUTPUT_MAIN_RESOURCES);
   }
 
+  /**
+   * Retrieves a list of lookup files that are used during the code generation process. For test, an
+   * empty list is returned as it is not allowed to enhance test domain models from other modules or
+   * current project main domain models; otherwise, it collects the output directory of the main
+   * domain models for all axelor projects associated with the current project.
+   *
+   * <p>Important: Gradle uses this method for incremental builds and configuration cache. It is not
+   * explicitly used in the code.
+   *
+   * @return a list of {@link File} objects representing the lookup files
+   */
   @InputFiles
   public List<File> getLookupFiles() {
     if (useTestSources) {
@@ -102,13 +166,27 @@ public class GenerateCode extends DefaultTask {
         .collect(Collectors.toList());
   }
 
+  /**
+   * Retrieves the input directory used for the code generation process.
+   *
+   * <p>Important: Gradle uses this method for incremental builds and configuration cache. It is not
+   * explicitly used in the code.
+   *
+   * @return a {@link File} object representing the input directory
+   */
   @InputFiles
-  public List<File> getInputDirectories() {
-    return useTestSources
-        ? List.of(getInputMainDir(getProject()), getInputTestDir(getProject()))
-        : List.of(getInputMainDir(getProject()));
+  public File getInputDirectory() {
+    return useTestSources ? getInputTestDir(getProject()) : getInputMainDir(getProject());
   }
 
+  /**
+   * Retrieves a list of output directories for the code generation process.
+   *
+   * <p>Important: Gradle uses this method for incremental builds and configuration cache. It is not
+   * explicitly used in the code.
+   *
+   * @return a list of {@link File} objects representing the output directories
+   */
   @OutputDirectories
   public List<File> getOutputDirectories() {
     return useTestSources
@@ -116,6 +194,14 @@ public class GenerateCode extends DefaultTask {
         : List.of(getMainJavaOutputDir(getProject()), getMainResourceOutputDir(getProject()));
   }
 
+  /**
+   * Generates the "axelor-module.properties" file for the given project.
+   *
+   * @param extension an instance of {@link AxelorExtension} containing module-specific metadata
+   * @param artifacts a list of {@link ResolvedArtifact} objects representing the project's resolved
+   *     dependencies
+   * @throws IOException if an error occurs while writing to the output file
+   */
   private void generateInfo(AxelorExtension extension, List<ResolvedArtifact> artifacts)
       throws IOException {
     final Project project = getProject();
@@ -187,18 +273,37 @@ public class GenerateCode extends DefaultTask {
     Files.asCharSink(outputPath, StandardCharsets.UTF_8).write(text);
   }
 
+  /**
+   * Builds and returns an {@link EntityGenerator} for the main domain models of the given project.
+   *
+   * @param project the project
+   * @return an instance of {@link EntityGenerator} configured for the main domain models of the
+   *     project
+   */
   private EntityGenerator buildMainGenerator(Project project) {
     final File domainPath = getInputMainDir(project);
     final File targetPath = getMainJavaOutputDir(project);
     return new EntityGenerator(domainPath, targetPath);
   }
 
+  /**
+   * Builds and returns an {@link EntityGenerator} for the test domain models of the given project.
+   *
+   * @param project the project
+   * @return an instance of {@link EntityGenerator} configured for the test domain models of the
+   *     project
+   */
   private EntityGenerator buildTestGenerator(Project project) {
     final File domainPath = getInputTestDir(project);
     final File targetPath = getTestJavaOutputDir(project);
     return new EntityGenerator(domainPath, targetPath);
   }
 
+  /**
+   * Executes the code generation task.
+   *
+   * @throws IOException if an error occurs during file I/O operations
+   */
   @TaskAction
   public void generate() throws IOException {
     final Project project = getProject();
@@ -218,6 +323,14 @@ public class GenerateCode extends DefaultTask {
     generateCodeForSourceSet(project, axelorArtifacts);
   }
 
+  /**
+   * Generates code for the given source set of the given project.
+   *
+   * @param project the project for which code generation is being performed
+   * @param axelorArtifacts a list of resolved artifacts representing dependencies used for code
+   *     generation
+   * @throws IOException if an I/O error occurs during the code generation process
+   */
   private void generateCodeForSourceSet(Project project, List<ResolvedArtifact> axelorArtifacts)
       throws IOException {
     final File inputDir = useTestSources ? getInputTestDir(project) : getInputMainDir(project);
@@ -233,7 +346,7 @@ public class GenerateCode extends DefaultTask {
         !useTestSources ? buildMainGenerator(project) : buildTestGenerator(project);
 
     if (!useTestSources) {
-      // Add lookup generators from dependencies. Allow to extend domains from others modules
+      // Add lookup generators from dependencies. Allow extending domain models from other modules
       for (ResolvedArtifact artifact : axelorArtifacts) {
         final Project sub = AxelorUtils.findProject(project, artifact);
         if (sub == null) {
