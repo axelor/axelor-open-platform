ce923343417a57b59431034c3199298ff58f2bd9
Pierre Belloy <p.belloy@axelor.com>
2025-09-16 10:20:02 +0200

    Apply new SPDX license headers format
    Signed-off-by: Pierre Belloy <p.belloy@axelor.com>

--- buildSrc/src/main/java/com/axelor/gradle/task/CentralPortalUploadTask.java
+++ buildSrc/src/main/java/com/axelor/gradle/task/CentralPortalUploadTask.java
@@ -1,3 +1,7 @@
+/*
+ * SPDX-FileCopyrightText: Axelor <https://axelor.com>
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
 package com.axelor.gradle.task;
 
 import java.io.ByteArrayOutputStream;
