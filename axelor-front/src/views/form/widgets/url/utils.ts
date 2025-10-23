3773f1990c0c497adfc55ccf60ce9f164bcf130e
Phi-Long Do <p.do@axelor.com>
2025-10-23 15:21:36 +0200

    Suppress ESLint errors for JavaScript protocol regex
    The JavaScript protocol regex is taken verbatim from React.

--- axelor-front/src/views/form/widgets/url/utils.ts
+++ axelor-front/src/views/form/widgets/url/utils.ts
@@ -1,4 +1,5 @@
 const isJavaScriptProtocol =
+  // eslint-disable-next-line no-control-regex, no-useless-escape
   /^[\u0000-\u001F ]*j[\r\n\t]*a[\r\n\t]*v[\r\n\t]*a[\r\n\t]*s[\r\n\t]*c[\r\n\t]*r[\r\n\t]*i[\r\n\t]*p[\r\n\t]*t[\r\n\t]*\:/i;
 
 export function isValidUrl(url?: string | null) {
