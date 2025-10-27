0c4e3c116da4a19affa4ba1c4a833e8260d149dd
Phi-Long Do <p.do@axelor.com>
2025-10-27 17:54:21 +0100

    Fix time series with line chart
    Use xAxis type "time" for `<category type="date|time" />`
    Line chart was always using "category" type (equal spacing),
    which is incorrect when dealing with time.

--- axelor-front/src/views/chart/widgets/line/line.tsx
+++ axelor-front/src/views/chart/widgets/line/line.tsx
@@ -1,6 +1,7 @@
 import { produce } from "immer";
 import { useState, useEffect } from "react";
 
+import { Formatters } from "@/utils/format";
 import { PlotData, applyTitles } from "../../builder/utils";
 import { ChartProps, ECharts } from "../../builder";
 
@@ -26,16 +27,45 @@ export function Line({ data, type, ...rest }: ChartProps) {
     const { data: series, types, formatter } = PlotData(data);
     setOptions(
       produce((draft: any) => {
+        const xType = ["date", "time"].includes(data?.xType ?? "")
+          ? "time"
+          : "category";
         applyTitles(draft, data);
         draft.series = series.map((line: any) => ({
           name: line.key,
           type: "line",
-          data: line.values.map(({ y }: any) => y),
+          data: line.values.map(
+            xType === "time"
+              ? ({ sort, y }: any) => [sort, y]
+              : ({ y }: any) => y,
+          ),
           ...(type === "area" ? { areaStyle: {} } : {}),
         }));
+        draft.xAxis.type = xType;
         draft.xAxis.data = types;
-        draft.legend.data = series.map((x: any) => x.key).filter(x => x);
+        draft.legend.data = series.map((x: any) => x.key).filter((x) => x);
         draft.tooltip.valueFormatter = formatter;
+        if (xType === "time") {
+          draft.tooltip = {
+            ...draft.tooltip,
+            axisPointer: {
+              label: {
+                formatter: (params: any) => {
+                  const labelFormatter =
+                    params.seriesData?.[0].value?.[0]?.includes("T")
+                      ? Formatters.datetime
+                      : Formatters.date;
+                  return labelFormatter(params.value);
+                },
+              },
+            },
+          };
+        } else {
+          draft.tooltip = {
+            ...draft.tooltip,
+            axisPointer: {},
+          };
+        }
       }),
     );
   }, [type, data]);
