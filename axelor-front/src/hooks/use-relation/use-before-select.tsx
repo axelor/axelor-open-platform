739d806159e1fcd75dbe538972924fc9f7988903
Phi-Long Do <p.do@axelor.com>
2025-11-05 12:37:54 +0100

    Fix concurrent select searches not waiting for onSelect
    See #1583

--- axelor-front/src/hooks/use-relation/use-before-select.tsx
+++ axelor-front/src/hooks/use-relation/use-before-select.tsx
@@ -2,6 +2,7 @@ import deepEqual from "lodash/isEqual";
 import { useCallback, useRef } from "react";
 
 import { DataContext } from "@/services/client/data.types";
+import type { ActionResult } from "@/services/client/meta";
 import { Schema } from "@/services/client/meta.types";
 import { useFormScope } from "@/views/form/builder/scope";
 
@@ -19,20 +20,30 @@ export function useBeforeSelect(
   const { actionExecutor } = useFormScope();
 
   const lastCtxRef = useRef<DataContext | undefined>(undefined);
+  const actionPromiseRef = useRef<Promise<void | ActionResult[]>>();
 
   const handleBeforeSelect = useCallback(
     async (currentDomain?: string, force = false) => {
       const ctx = getContext?.();
       if (onSelectAction && (force || !deepEqual(lastCtxRef.current, ctx))) {
-        lastCtxRef.current = ctx;
         const opts = ctx ? { context: ctx } : undefined;
-        const res = await actionExecutor.execute(onSelectAction, opts);
-        if (res && res.length > 0) {
-          const attrs = res[0].attrs || {};
-          const domain = attrs[schema.name!]?.domain;
-          if (domain !== undefined) {
-            return domain;
+        actionPromiseRef.current ??= actionExecutor.execute(
+          onSelectAction,
+          opts,
+        );
+
+        try {
+          const res = await actionPromiseRef.current;
+          if (res && res.length > 0) {
+            const attrs = res[0].attrs || {};
+            const domain = attrs[schema.name!]?.domain;
+            if (domain !== undefined) {
+              return domain;
+            }
           }
+        } finally {
+          actionPromiseRef.current = undefined;
+          lastCtxRef.current = ctx;
         }
       }
       return currentDomain;
