2aea5d1cd8a1a70a9fe872e3bc015f4a5b9910fe
Amit Mendapara <amit@axelor.com>
2025-11-14 14:26:57 +0530

    Add AccessibileButton
    This component can be used where elements other than `button` is
    used with click handler.

---
+++ axelor-front/src/components/accessible-button/accessible-button.tsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+
+import { Box, useRefs } from "@axelor/ui";
+import { withStyled } from "@axelor/ui/core/styled";
+
+export const AccessibleButton = withStyled(Box)((props, ref) => {
+  const [element, setElement] = useState<HTMLElement | null>(null);
+  const mergedRef = useRefs(ref, setElement);
+
+  useEffect(() => {
+    if (!element) return;
+
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === "Enter" || event.key === " ") {
+        event.preventDefault();
+        element.click();
+      }
+    };
+
+    element.addEventListener("keydown", handleKeyDown);
+    return () => {
+      element.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [element]);
+
+  return <Box ref={mergedRef} rounded focusRing role="button" {...props} />;
+});
