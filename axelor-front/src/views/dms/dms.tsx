452ad72de86c6feec1404febc762fca39dd7a845
Mayank Mahadevwala <m.mahadevwala@axelor.com>
2026-01-07 14:05:51 +0530

    Fix dms grid auto scroll issue on cell selection
    Fixes #1659

--- axelor-front/src/views/dms/dms.tsx
+++ axelor-front/src/views/dms/dms.tsx
@@ -1022,7 +1022,6 @@ function DMSGridRow(props: GridRowProps) {
     <>
       <Box
         ref={ref}
-        position="relative"
         {...{
           style,
           className: legacyClassNames(className, gridRowStyles.row, {
