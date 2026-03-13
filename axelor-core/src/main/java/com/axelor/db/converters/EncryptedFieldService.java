ea0402a406ab7e1d11737c677fcca405816220d1
Pierre Belloy <p.belloy@axelor.com>
2026-03-13 09:32:53 +0100

    Fix non-deterministic pagination in EncryptedFieldService
    Without ORDER BY, offset-based pagination is non-deterministic:
    already-migrated records can reappear at shifted offsets and fail
    decryption with the old encryptor
    
    Fixes #1713
    
    Signed-off-by: Pierre Belloy <p.belloy@axelor.com>

--- axelor-core/src/main/java/com/axelor/db/converters/EncryptedFieldService.java
+++ axelor-core/src/main/java/com/axelor/db/converters/EncryptedFieldService.java
@@ -85,7 +85,7 @@ public class EncryptedFieldService {
                 names.stream().map(n -> "self." + n + " as " + n).collect(Collectors.joining(", ")))
             .append(") FROM ")
             .append(model.getSimpleName())
-            .append(" self");
+            .append(" self ORDER BY self.id");
 
     TypedQuery<Map> selectQuery = JPA.em().createQuery(sb.toString(), Map.class);
     TypedQuery<Long> countQuery =
