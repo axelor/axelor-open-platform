199cf3a1a5199de30394c8dc856033ead2821aca
Pierre Belloy <p.belloy@axelor.com>
2026-04-08 21:21:05 +0200

    Clean logout filter
    subject logout and session stop already done in base filter
    
    Signed-off-by: Pierre Belloy <p.belloy@axelor.com>

--- axelor-core/src/main/java/com/axelor/auth/pac4j/AxelorLogoutFilter.java
+++ axelor-core/src/main/java/com/axelor/auth/pac4j/AxelorLogoutFilter.java
@@ -6,19 +6,10 @@ package com.axelor.auth.pac4j;
 
 import com.axelor.app.AppSettings;
 import com.axelor.app.AvailableAppSettings;
-import com.axelor.auth.AuthUtils;
 import com.axelor.auth.pac4j.config.LogoutConfig;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
-import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
-import java.io.IOException;
-import java.util.Optional;
-import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.session.Session;
-import org.apache.shiro.subject.Subject;
 import org.pac4j.jee.filter.LogoutFilter;
 
 @Singleton
@@ -36,24 +27,5 @@ public class AxelorLogoutFilter extends LogoutFilter {
     setLogoutUrlPattern(logoutUrlPattern);
 
     init(filterConfig);
-  }
-
-  @Override
-  public void doFilter(
-      ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
-      throws IOException, ServletException {
-
-    super.doFilter(servletRequest, servletResponse, filterChain);
-
-    if (!Boolean.FALSE.equals(getLocalLogout())) {
-      // Log out subject.
-      Optional.ofNullable(AuthUtils.getSubject()).ifPresent(Subject::logout);
-
-      // Destroy web session.
-      final Session session = SecurityUtils.getSubject().getSession(false);
-      if (session != null) {
-        session.stop();
-      }
-    }
   }
 }
